aaae283720cd675b858826f8f235d9bcb397aee0
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-11-21 16:06:17 -0500

    Adding the Runner concept

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,55 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Because yardoc's scope imperative does not appear to work, I'm pushing the
+  # comments into the class definition
+  class << self
+    # @!attribute [rw] runner_container
+    #   So you can specify where you will be finding an action's Hydramata::Runner
+    #   class.
+    #
+    #   @see ApplicationController#run
+  end
+  class_attribute :runner_container
+
+  # So you can more easily decouple the controller's command behavior and
+  # response behavior.
+  #
+  # @example
+  #   def index
+  #     run(specific_params) do |on|
+  #       on.success { |collection|
+  #         @collection = collection
+  #         respond_with(@collection)
+  #       }
+  #     end
+  #   end
+  #
+  # @see ApplicationController.runner_container for customization
+  def run(*args, &block)
+    runner_name = action_name.classify
+    if runner_container.const_defined?(runner_name)
+      runner = runner_container.const_get(runner_name)
+      runner.run(self, *args, &block)
+    else
+      fail RunnerNotFoundError.new(runner_container, runner_name)
+    end
+  end
+
+  # Raised when a Runner is not found
+  class RunnerNotFoundError < RuntimeError # :nodoc:
+    def initialize(runner_container, runner_name)
+      super("Unable to find #{runner_name} in #{runner_container}")
+    end
+  end
+
+  # So you can easily invoke the public repository of Hydramata.
+  # It is these repository that indicate what the application can and is doing.
+  #
+  # @see Cur8Nd::Repository for the default methods
+  def repository
+    @repository = Sip::Repository.new
+  end
+  helper_method :repository
 end
