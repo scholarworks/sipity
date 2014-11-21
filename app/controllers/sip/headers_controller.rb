60d7cd099a628f42a151044ddd521677573f3d97
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-11-21 08:59:40 -0500

    Prefering explicit decoration
    Instead of the controller imperative, prefer explicit decoration.
    There is a performance hit but that is acceptable compared to the
    improved expressiveness.

--- app/controllers/sip/headers_controller.rb
+++ app/controllers/sip/headers_controller.rb
@@ -1,28 +1,31 @@
 module Sip
   # The controller for creating headers.
   class HeadersController < ApplicationController
-    decorates_assigned :model
     respond_to :html, :json
 
     def new
-      @model = Header.new
+      @model = decorate(Header.new)
       respond_with(@model)
     end
 
     def create
-      @model = Header.new(create_params)
+      @model = decorate(Header.new(create_params))
       @model.save
       respond_with(@model)
     end
 
     def show
-      @model = Header.find(params[:id])
+      @model = decorate(Header.find(params[:id]))
     end
 
     private
 
     def create_params
       params.require(:sip_header).permit(:title, :work_publication_strategy)
+    end
+
+    def decorate(model)
+      HeaderDecorator.new(model)
     end
   end
 end
