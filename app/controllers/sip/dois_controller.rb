2214ea562368ee3703798d123e35ad80cecb6a33
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-11-24 12:49:38 -0500

    Breaking apart a larger method

--- app/controllers/sip/dois_controller.rb
+++ app/controllers/sip/dois_controller.rb
@@ -11,16 +11,25 @@ module Sip
     def show
       run(header_id: header_id) do |on|
         on.doi_not_assigned do |header|
-          # TODO: Really I shouldn't be doing this; The header that is
-          # return should be decorated
-          @model = HeaderDoi.new(header: header)
-          respond_with(@model) do |wants|
-            flash.now.alert = t(:doi_not_assigned, title: header.title, scope: SIP_MESSAGE_SCOPE)
-            wants.html { render action: 'doi_not_assigned' }
-          end
+          doi_not_assigned_response(header)
+        end
+        on.doi_already_assigned do |header|
+          flash[:notice] = t(:doi_already_assigned, title: header.title, scope: SIP_MESSAGE_SCOPE)
+          redirect_to sip_header_path(header)
         end
       end
     end
+
+    def doi_not_assigned_response(header)
+      # TODO: Really I shouldn't be doing this; The header that is
+      # return should be decorated
+      @model = HeaderDoi.new(header: header)
+      respond_with(@model) do |wants|
+        flash.now.alert = t(:doi_not_assigned, title: header.title, scope: SIP_MESSAGE_SCOPE)
+        wants.html { render action: 'doi_not_assigned' }
+      end
+    end
+    private :doi_not_assigned_response
 
     def assign
       run(header_id: header_id, identifier: doi) do |on|
