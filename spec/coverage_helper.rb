3d34c9dc73594d2c5169acbfc38da850491c6e91
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-11-24 15:11:21 -0500

    Switching around coverage helper
    The coveralls job was not invoking, as the logic was mangled.

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -1,13 +1,13 @@
 if ENV['COV'] || ENV['COVERAGE'] || ENV['TRAVIS']
-  if ENV['COV'] || ENV['COVERAGE']
-    require 'simplecov'
-    SimpleCov.start do
-      load_profile 'rails'
-    end
-  elsif ENV['TRAVIS']
+  if ENV['TRAVIS']
     require 'coveralls'
     require 'simplecov'
     SimpleCov.formatter = Coveralls::SimpleCov::Formatter
     Coveralls.wear!('rails')
+  elsif ENV['COV'] || ENV['COVERAGE']
+    require 'simplecov'
+    SimpleCov.start do
+      load_profile 'rails'
+    end
   end
 end
