dc4fbdfacff426e2126f5dd076d8ac1e58711d27
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-11-25 08:44:04 -0500

    Improving code coverage to 100%

---
+++ spec/forms/sip/header_doi_request_form_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+module Sip
+  RSpec.describe HeaderDoiRequestForm do
+    let(:header) { double }
+    subject { described_class.new(header: header) }
+
+    it 'has a default decorator that implements #decorate' do
+      # I want the #decorator method to remain private
+      # This example was to test a default path.
+      expect(subject.send(:decorator)).to respond_to(:decorate)
+    end
+  end
+end
