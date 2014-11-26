4cd3e7515c13ef6acb7f98e3f22320b0f16831f4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-11-26 13:46:47 -0500

    Adding a CreateHeaderForm
    This is a class that can be used to replace the current Header pseudo-
    form.

---
+++ spec/forms/sip/create_header_form_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+module Sip
+  RSpec.describe CreateHeaderForm do
+    subject { described_class.new }
+
+    it 'will have a model name like Header' do
+      expect(described_class.model_name).to be_a(ActiveModel::Name)
+    end
+
+    context 'validations for' do
+      context '#contributors' do
+        it '#role must be present' do
+          # Adhearing to the ActiveRecord::Base.accepts_nested_attributes_for
+          # interface
+          subject.collaborators_attributes = { '0' => { name: 'Hello', role: '' } }
+          expect(subject.valid?).to be_falsey
+          expect(subject.collaborators[0].errors[:role]).to be_present
+        end
+        it '#role must be on the approved list' do
+          # The enum enforcement is aggressive throwing an exception
+          expect do
+            subject.collaborators_attributes = { '0' => { name: 'Hello', role: '__missing__' } }
+          end.to raise_error(ArgumentError)
+        end
+      end
+      context '#title' do
+        it 'must be present' do
+          subject.valid?
+          expect(subject.errors[:title]).to be_present
+        end
+      end
+      context '#work_publication_strategy' do
+        it 'must be present' do
+          subject.valid?
+          expect(subject.errors[:work_publication_strategy]).to be_present
+        end
+        it 'must be from the approved list' do
+          subject.work_publication_strategy = '__missing__'
+          subject.valid?
+          expect(subject.errors[:work_publication_strategy]).to be_present
+        end
+      end
+      context '#publication_date' do
+        it 'must be present when it was :already_published' do
+          subject.work_publication_strategy = 'already_published'
+          subject.valid?
+          expect(subject.errors[:publication_date]).to be_present
+        end
+        it 'need not be present otherwise' do
+          subject.valid?
+          expect(subject.errors[:publication_date]).to_not be_present
+        end
+      end
+    end
+  end
+end
