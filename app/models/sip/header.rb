3e86bbb4d5cf5a1c64a96bb8ea87982337589377
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-03 21:10:09 -0500

    Removing model level validations

--- app/models/sip/header.rb
+++ app/models/sip/header.rb
@@ -13,11 +13,6 @@ module Sip
       reject_if: ->(collaborator_attributes) { collaborator_attributes['name'].blank? }
     )
 
-    validates :title, presence: true
-    validates :work_publication_strategy,
-      presence: true,
-      inclusion: { in: ->(obj) { obj.class.work_publication_strategies } }
-
     # While this make look ridiculous, if I use an Array, the enum declaration
     # insists on persisting the value as the index instead of the key. While
     # this might make more sense from a storage standpoint, it is not as clear
