3b1387a60d8616471fd256d66e8ada2226bdeeae
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-05 13:06:24 -0500

    Adding documentation regarding Controllers
    [skip ci]

---
+++ app/controllers/sipity/controllers.rb
@@ -0,0 +1,21 @@
+module Sipity
+  # Controllers are the HTTP end-points for the underlying router. In other
+  # words, when an HTTP request comes in, the router will map the request to
+  # a Controller's action. A controller represents a logical grouping of those
+  # actions.
+  #
+  # @see Sipity::Runners::BaseRunner for reference to how individual actions
+  #   can be written so as to detach the Action from the controller context, and
+  #   thus make the underlying Action much more portable and reusable.
+  #
+  # @note Providing a namespace to group Sipity related controllers. Why go
+  #   through this extra effort? Because without the Controllers namespace, the
+  #   generated documentation -- via `$ yard` -- has a lot more objects in the
+  #   same namespace.
+  #
+  # @note When you add controllers to this namespace, be mindful of the
+  #   #controller_path method. Rails will use the module namespace to define
+  #   where views can be found.
+  module Controllers
+  end
+end
