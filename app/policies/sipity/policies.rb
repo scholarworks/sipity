b8eea674a96bd0c91a202a5f979a114aef29b42a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-09 16:06:43 -0500

    Adding a Sipity::Policies module
    [skip ci]

---
+++ app/policies/sipity/policies.rb
@@ -0,0 +1,11 @@
+module Sipity
+  # Contains the various Policies associated with Sipity.
+  #
+  # A Policy is an object responsible for answering questions about actions
+  # the given user is attempting to take on a given object.
+  #
+  # @see [Elabs' Pundit gem](http://github.com/elabs/pundit) for
+  #   further explanation of Policy and Scope objects.
+  module Policies
+  end
+end
