91b13b4b5e839800be400ce4906bb26be191adca
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-10 14:05:23 -0500

    Removing vestigial methods

--- app/policies/sipity/policies.rb
+++ app/policies/sipity/policies.rb
@@ -7,29 +7,5 @@ module Sipity
   # @see [Elabs' Pundit gem](http://github.com/elabs/pundit) for
   #   further explanation of Policy and Scope objects.
   module Policies
-    module_function
-
-    def with_authorization_enforcement(user:, policy_question:, entity:)
-      if policy_authorized_for?(user: user, policy_question: policy_question, entity: entity)
-        yield
-      else
-        fail Exceptions::AuthorizationFailureError, user: user, policy_question: policy_question, entity: entity
-      end
-    end
-
-    def policy_authorized_for?(user:, policy_question:, entity:)
-      policy_enforcer = find_policy_enforcer_for(entity)
-      policy_enforcer.call(user: user, entity: entity, policy_question: policy_question)
-    end
-
-    def find_policy_enforcer_for(entity)
-      if entity.respond_to?(:policy_enforcer) && entity.policy_enforcer.present?
-        entity.policy_enforcer
-      else
-        # Yowza! This could cause lots of problems; Maybe I should be very
-        # specific about this?
-        Policies.const_get("#{entity.class.to_s.demodulize}Policy")
-      end
-    end
   end
 end
--- app/services/sipity/repository.rb
+++ app/services/sipity/repository.rb
@@ -5,9 +5,5 @@ module Sipity
     include Sipity::Repo::HeaderMethods
     include Sipity::Repo::CitationMethods
     include Sipity::Repo::DoiMethods
-
-    def policy_authorized_for?(user:, policy_question:, entity:)
-      Policies.policy_authorized_for?(user: user, policy_question: policy_question, entity: entity)
-    end
   end
 end
