2d4fc986aa6fecf790da93d88dd79bb27c5b2e2d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-10 14:34:23 -0500

    Adding README for policy_enforcement

--- app/services/sipity/policy_enforcer.rb
+++ app/services/sipity/policy_enforcer.rb
@@ -8,8 +8,23 @@ module Sipity
     attr_reader :user, :context
     private :user, :context
 
-    def enforce!(questions_and_entities = {})
-      questions_and_entities.each do |policy_question, entity|
+    # Responsible for enforcing policies on the :policy_questions_and_entity_pairs.
+    #
+    # @param policy_questions_and_entity_pairs [Hash<Symbol,Object>, #each] Yield two elements a
+    #   :policy_question and an :entity
+    #
+    # @yield Returns control to the caller if all :policy_questions_and_entity_pairs
+    #   are authorized.
+    #
+    # @raises [Exceptions::AuthorizationFailureError] if one of the
+    #   policy_question/entity pairs fail to authorize.
+    #
+    # @note If the context implements #callbacks, that will be called.
+    #
+    # @todo Would it be helpful to include in the exception the policy_enforcer
+    #   that was found?
+    def enforce!(policy_questions_and_entity_pairs = {})
+      policy_questions_and_entity_pairs.each do |policy_question, entity|
         next if policy_authorized_for?(user: user, policy_question: policy_question, entity: entity)
         context.callback(:unauthorized) if context.respond_to?(:callback)
         fail Exceptions::AuthorizationFailureError, user: user, policy_question: policy_question, entity: entity
