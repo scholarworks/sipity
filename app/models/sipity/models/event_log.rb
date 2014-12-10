56669935ad8e95d9b3633aeb3b484be17ef29c6d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-10 13:09:26 -0500

    Adding rudimentary tests of EventLog

--- app/models/sipity/models/event_log.rb
+++ app/models/sipity/models/event_log.rb
@@ -3,6 +3,8 @@ module Sipity
   module Models
     # A place to record the named event for the given subject that was trigger by
     # a user's action.
+    #
+    # @note Where are the validations? They are enforced on the database.
     class EventLog < ActiveRecord::Base
       self.table_name = 'sipity_event_logs'
       belongs_to :user
--- spec/models/sipity/models/event_log_spec.rb
+++ spec/models/sipity/models/event_log_spec.rb
@@ -3,7 +3,22 @@ require 'rails_helper'
 module Sipity
   module Models
     RSpec.describe EventLog, type: :model do
-      pending "add some examples to (or delete) #{__FILE__}"
+      it 'belongs to a :user' do
+        expect(described_class.reflect_on_association(:user)).
+          to be_a(ActiveRecord::Reflection::AssociationReflection)
+      end
+
+      it 'belongs to a :subject' do
+        expect(described_class.reflect_on_association(:subject)).
+          to be_a(ActiveRecord::Reflection::AssociationReflection)
+      end
+
+      it 'relies on the database to enforce the requirement of an :event_name' do
+        user = User.new(id: 1)
+        entity = Models::Header.new(id: 1)
+        expect { EventLog.create!(user: user, subject: entity) }.
+          to raise_error(ActiveRecord::StatementInvalid, /event_name may not be NULL/)
+      end
     end
   end
 end
