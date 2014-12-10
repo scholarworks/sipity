12327fc57b750a21962fa3297e43dc4d3c2e9fe4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-10 15:01:23 -0500

    Adding specs for Permission

--- spec/models/sipity/models/permission_spec.rb
+++ spec/models/sipity/models/permission_spec.rb
@@ -3,7 +3,22 @@ require 'rails_helper'
 module Sipity
   module Models
     RSpec.describe Permission, type: :model do
-      pending "add some examples to (or delete) #{__FILE__}"
+      it 'belongs to a :user' do
+        expect(described_class.reflect_on_association(:user)).
+          to be_a(ActiveRecord::Reflection::AssociationReflection)
+      end
+
+      it 'belongs to a :subject' do
+        expect(described_class.reflect_on_association(:subject)).
+          to be_a(ActiveRecord::Reflection::AssociationReflection)
+      end
+
+      it 'relies on the database to enforce the requirement of an :role' do
+        user = User.new(id: 1)
+        entity = Models::Header.new(id: 1)
+        expect { Permission.create!(user: user, subject: entity) }.
+          to raise_error(ActiveRecord::StatementInvalid, /role may not be NULL/)
+      end
     end
   end
 end
