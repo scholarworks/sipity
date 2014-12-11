86d1f345b10040aaaaeb9cbe82cf3c8629b3457d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-10 19:16:36 -0500

    Constantizing DoiCreationRequest#states

--- app/models/sipity/models/doi_creation_request.rb
+++ app/models/sipity/models/doi_creation_request.rb
@@ -6,13 +6,17 @@ module Sipity
       self.table_name = 'sipity_doi_creation_requests'
       belongs_to :header, foreign_key: 'sipity_header_id'
       REQUEST_NOT_YET_SUBMITTED = 'request_not_yet_submitted'.freeze
+      REQUEST_SUBMITTED = 'request_submitted'.freeze
+      REQUEST_COMPLETED = 'request_completed'.freeze
+      REQUEST_FAILED = 'request_failed'.freeze
+
       enum(
         state:
         {
           REQUEST_NOT_YET_SUBMITTED => REQUEST_NOT_YET_SUBMITTED,
-          'request_submitted' => 'request_submitted',
-          'request_completed' => 'request_completed',
-          'request_failed' => 'request_failed'
+          REQUEST_SUBMITTED => REQUEST_SUBMITTED,
+          REQUEST_COMPLETED => REQUEST_COMPLETED,
+          REQUEST_FAILED => REQUEST_FAILED
         }
       )
     end
