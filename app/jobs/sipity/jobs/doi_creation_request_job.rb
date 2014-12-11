4f5804ad80eded84d094f3dd2dd1604e24694777
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-10 19:15:17 -0500

    Expanding on the DOI Creation Request Job

--- app/jobs/sipity/jobs/doi_creation_request_job.rb
+++ app/jobs/sipity/jobs/doi_creation_request_job.rb
@@ -1,19 +1,37 @@
 module Sipity
   module Jobs
+    # Responsible for processing a remote request for minting a DOI.
     class DoiCreationRequestJob
       def initialize(doi_creation_request_id)
-        @doi_creation_request_id = doi_creation_request_id
+        # Find the DOI Creation Request
+        @doi_creation_request = Models::DoiCreationRequest.find(doi_creation_request_id)
       end
-      attr_reader :doi_creation_request_id
-      private :doi_creation_request_id
+      attr_reader :doi_creation_request
+      private :doi_creation_request
 
       def work
-        # Find the DOI Creation Request
         # Guard REQUEST_NOT_YET_SUBMITTED? Maybe?
+        guard_doi_creation_request_state!
         # Update request to REQUEST_SUBMITTED
+        transition_doi_creation_request_to_submitted!
         # Submit remote request
-        # Handle the response by assigning the DOI
-        # Update DOI Creation Request state
+        submit_remote_request!
+      end
+
+      private
+
+      def guard_doi_creation_request_state!
+        return true if doi_creation_request.request_failed?
+        return true if doi_creation_request.request_not_yet_submitted?
+        # TODO: Need a better exception
+        fail RuntimeError
+      end
+
+      def transition_doi_creation_request_to_submitted!
+        doi_creation_request.update(state: doi_creation_request.class::REQUEST_SUBMITTED)
+      end
+
+      def submit_remote_request!
       end
     end
   end
--- spec/jobs/sipity/jobs/doi_creation_request_job_spec.rb
+++ spec/jobs/sipity/jobs/doi_creation_request_job_spec.rb
@@ -3,6 +3,26 @@ require 'spec_helper'
 module Sipity
   module Jobs
     RSpec.describe DoiCreationRequestJob do
+      let(:doi_creation_request) { Models::DoiCreationRequest.new(id: 123) }
+      subject { described_class.new(doi_creation_request.id) }
+      before do
+        allow(Models::DoiCreationRequest).to receive(:find).with(doi_creation_request.id).and_return doi_creation_request
+      end
+
+      context '#work' do
+        before do
+        end
+
+        it 'will guard against incorrect state' do
+          expect { subject.work }.to raise_error(RuntimeError)
+        end
+
+        it 'will transition the state to ' do
+          doi_creation_request.state = doi_creation_request.class::REQUEST_NOT_YET_SUBMITTED
+          allow(doi_creation_request).to receive(:update).with(state: doi_creation_request.class::REQUEST_SUBMITTED)
+          subject.work
+        end
+      end
     end
   end
 end
