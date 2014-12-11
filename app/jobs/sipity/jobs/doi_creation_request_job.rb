a5930bb066ad239882593edaeee68c21fec5bd34
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-10 19:33:26 -0500

    Dependency injection for DoiCreationRequest

--- app/jobs/sipity/jobs/doi_creation_request_job.rb
+++ app/jobs/sipity/jobs/doi_creation_request_job.rb
@@ -2,12 +2,14 @@ module Sipity
   module Jobs
     # Responsible for processing a remote request for minting a DOI.
     class DoiCreationRequestJob
-      def initialize(doi_creation_request_id)
+      def initialize(doi_creation_request_id, options = {} )
         # Find the DOI Creation Request
         @doi_creation_request = Models::DoiCreationRequest.find(doi_creation_request_id)
+        @minter = options.fetch(:minter) { default_minter }
+        @minter_exceptions = Array.wrap(options.fetch(:minter_exceptions) { default_minter_exceptions })
       end
-      attr_reader :doi_creation_request
-      private :doi_creation_request
+      attr_reader :doi_creation_request, :minter, :minter_exceptions
+      private :doi_creation_request, :minter, :minter_exceptions
 
       def work
         # Guard REQUEST_NOT_YET_SUBMITTED? Maybe?
@@ -32,6 +34,29 @@ module Sipity
       end
 
       def submit_remote_request!
+        begin
+          identifier = minter.call(metadata)
+        rescue *minter_exceptions => e
+          doi_creation_request.update(state: doi_creation_request.class::REQUEST_FAILED)
+        end
+      end
+
+      def metadata
+        {
+          '_target' => '',
+          'datacite.creator' => '',
+          'datacite.title' => '',
+          'datacite.publisher' => '',
+          'datacite.publicationyear' => ''
+        }
+      end
+
+      def default_minter
+        ->(metadata) { Ezid::Identifier.create(metadata: metadata) }
+      end
+
+      def default_minter_exceptions
+        Ezid::Error
       end
     end
   end
--- spec/jobs/sipity/jobs/doi_creation_request_job_spec.rb
+++ spec/jobs/sipity/jobs/doi_creation_request_job_spec.rb
@@ -4,7 +4,8 @@ module Sipity
   module Jobs
     RSpec.describe DoiCreationRequestJob do
       let(:doi_creation_request) { Models::DoiCreationRequest.new(id: 123) }
-      subject { described_class.new(doi_creation_request.id) }
+      let(:minter) { double(call: true) }
+      subject { described_class.new(doi_creation_request.id, minter: minter) }
       before do
         allow(Models::DoiCreationRequest).to receive(:find).with(doi_creation_request.id).and_return doi_creation_request
       end
@@ -19,7 +20,8 @@ module Sipity
 
         it 'will transition the state to ' do
           doi_creation_request.state = doi_creation_request.class::REQUEST_NOT_YET_SUBMITTED
-          allow(doi_creation_request).to receive(:update).with(state: doi_creation_request.class::REQUEST_SUBMITTED)
+          allow(doi_creation_request).to receive(:update).with(state: doi_creation_request.class::REQUEST_SUBMITTED).ordered
+          allow(doi_creation_request).to receive(:update).with(state: doi_creation_request.class::REQUEST_FAILED).ordered
           subject.work
         end
       end
