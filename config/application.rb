ee298fc7517dbe36112f4162b6a29034c98eaa34
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-11 16:21:42 -0500

    Adding autoload_paths

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,10 @@ module Sipity
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
+    %w(conversions forms jobs policies runners services).each do |concept|
+        config.autoload_paths << Rails.root.join("app/#{concept}")
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
