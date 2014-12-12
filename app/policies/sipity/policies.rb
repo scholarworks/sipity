5da116bf0cf40bdffc8016e0367ca579c3125ed7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-12 08:12:34 -0500

    Moving Policy questions to Sipity::Policies

--- app/policies/sipity/policies.rb
+++ app/policies/sipity/policies.rb
@@ -7,5 +7,21 @@ module Sipity
   # @see [Elabs' Pundit gem](http://github.com/elabs/pundit) for
   #   further explanation of Policy and Scope objects.
   module Policies
+    module_function
+
+    def policy_authorized_for?(user:, policy_question:, entity:)
+      policy_enforcer = find_policy_enforcer_for(entity: entity)
+      policy_enforcer.call(user: user, entity: entity, policy_question: policy_question)
+    end
+
+    def find_policy_enforcer_for(entity:)
+      return entity.policy_enforcer if entity.respond_to?(:policy_enforcer) && entity.policy_enforcer.present?
+      policy_name_as_constant = "#{entity.class.to_s.demodulize}Policy"
+      if const_defined?(policy_name_as_constant)
+        const_get(policy_name_as_constant)
+      else
+        fail Exceptions::PolicyNotFoundError, name: policy_name_as_constant, container: self
+      end
+    end
   end
 end
---
+++ spec/policies/sipity/policies_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'sipity/policies'
+
+module Sipity
+  RSpec.describe Policies do
+    subject { described_class }
+    let(:user) { double('User') }
+    let(:policy_enforcer) { double('Policy Enforcer', call: true) }
+    let(:policy_question) { :show? }
+    let(:entity) { double('Entity', policy_enforcer: policy_enforcer) }
+
+    context '#policy_authorized_for?' do
+      it 'will use the found policy_enforcer' do
+        allow(subject).to receive(:find_policy_enforcer_for).with(entity: entity).and_return(policy_enforcer)
+        expect(policy_enforcer).to receive(:call).with(user: user, entity: entity, policy_question: policy_question)
+        subject.policy_authorized_for?(user: user, policy_question: policy_question, entity: entity)
+      end
+    end
+
+    context '#find_policy_enforcer_for' do
+      it "will use an entity's policy_enforcer if one is found" do
+        expect(subject.find_policy_enforcer_for(entity: entity)).to eq(entity.policy_enforcer)
+      end
+
+      it "will lookup one by convention" do
+        header = Models::Header.new
+        expect(subject.find_policy_enforcer_for(entity: header)).to eq(Policies::HeaderPolicy)
+      end
+
+      it "will fail if a policy_enforcer cannot be found" do
+        expect { subject.find_policy_enforcer_for(entity: double) }.to raise_error Exceptions::PolicyNotFoundError
+      end
+    end
+  end
+end
