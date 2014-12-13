2c89b200fa6e7f1185785b3957478b790b8a6b9f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-13 17:07:24 -0500

    Adding Sipity::Services module

---
+++ app/services/sipity/services.rb
@@ -0,0 +1,17 @@
+module Sipity
+  # Service objects are all the rage. Here is the junk drawer for your service
+  # objects.
+  #
+  # This neighborhood of Sipity does not have an obvious home owners
+  # association. That is to say, things are going to be a bit more organic in
+  # their construction.
+  #
+  # You might find objects that fullfil a single purpose (i.e.DoiCreationRequestMetadataGatherer).
+  # Or you may find the ever growing Repository object, with its fingers
+  # reaching deep into the persistence layer concerns.
+  #
+  # Be mindful as you introduce objects. Make sure this area is groomed on
+  # occassion. Organizing and sorting out where something best goes.
+  module Services
+  end
+end
