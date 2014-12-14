5d2d364021b81206a15e206c17e435546c6a3f8d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-14 09:43:55 -0500

    Adding a note about Repository vs. Services
    [skip ci]

--- app/services/sipity/repository.rb
+++ app/services/sipity/repository.rb
@@ -6,6 +6,16 @@ module Sipity
   #   class. Each of the mixins are tested in isolation. It is possible that
   #   there could be method collisions, but see the underlying specs for
   #   additional discussion and verification of method collisions.
+  #
+  # @note Why is the Sipity::Services module and Sipity::Repository at the
+  #   same module level? Right now, I believe that is a mistake. It makes sense
+  #   to me that there will be multiple repository objects; After all some
+  #   objects are going to come from another persistence service (i.e. DB or
+  #   Fedora commons). I suspect there will be a negotiation layer to determine
+  #   which persistence service to retrieve an entity from. That is to say if
+  #   you want to edit an object ingested into Fedora you might request the
+  #   object from Fedora, then request the object from a DB and layer the DB
+  #   values on top of the Fedora values.
   class Repository
     include Sipity::Repo::HeaderMethods
     include Sipity::Repo::CitationMethods
