7d67e8a72943ab773afbaf9dfdbffeecd8c3a431
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-14 13:02:17 -0500

    Wordsmithing
    [skip ci]

--- app/conversions/sipity/conversions.rb
+++ app/conversions/sipity/conversions.rb
@@ -2,16 +2,15 @@ module Sipity
   # Taking a que from Avdi Grimm's "Confident Ruby", the Conversion module
   # is responsible for coercing the inputs to another format.
   #
-  # This is somewhat experimental, though analogous to the Array() method in
-  # base ruby.
-  #
-  # This module is defined here to provide the top-level declaration.
-  #
   # Any Conversion modules should be callable and includable. By calling the
   # module you would perform the singular conversion. By including the module
   # you would gain access to the private conversion method.
   #
+  # @note This module is defined here to provide the top-level documentation and
+  #   declaration.
+  #
   # @see Sipity::Conversions::ConvertToYear
+  # @see http://confidentruby.com Avdi Grimm's "Confident Ruby"
   module Conversions
   end
 end
