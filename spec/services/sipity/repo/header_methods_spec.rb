e5aabf3c164b8ce8125ab25529a7c236c70e4e00
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-15 14:02:38 -0500

    Reworking repository specs
    Including a test_repository

--- spec/services/sipity/repo/header_methods_spec.rb
+++ spec/services/sipity/repo/header_methods_spec.rb
@@ -3,26 +3,18 @@ require 'rails_helper'
 module Sipity
   module Repo
     RSpec.describe HeaderMethods, type: :repository do
-      let!(:repository_class) do
-        class TestRepository
-          include HeaderMethods
-        end
-      end
-      subject { repository_class.new }
-      after { Sipity::Repo.send(:remove_const, :TestRepository) }
-
       it 'will have a permanent URL for a given header' do
-        expect(subject.permanent_uri_for_header_id(123)).to be_a(URI)
+        expect(test_repository.permanent_uri_for_header_id(123)).to be_a(URI)
       end
 
       context '#find_headers_for' do
         let(:user_one) { User.new(id: 1) }
         let(:user_two) { User.new(id: 2) }
-        let(:form) { subject.build_create_header_form(attributes: { title: 'My Title', work_publication_strategy: 'do_not_know' }) }
-        let!(:header_one) { subject.submit_create_header_form(form, requested_by: user_one) }
-        let!(:header_two) { subject.submit_create_header_form(form, requested_by: user_two) }
+        let(:form) { test_repository.build_create_header_form(attributes: { title: 'My Title', work_publication_strategy: 'do_not_know' }) }
+        let!(:header_one) { test_repository.submit_create_header_form(form, requested_by: user_one) }
+        let!(:header_two) { test_repository.submit_create_header_form(form, requested_by: user_two) }
         it 'will include headers that were created by the user' do
-          expect(subject.find_headers_for(user: user_one)).to eq([header_one])
+          expect(test_repository.find_headers_for(user: user_one)).to eq([header_one])
         end
       end
 
@@ -32,18 +24,18 @@ module Sipity
 
       context '#find_header' do
         it 'raises an exception if nothing is found' do
-          expect { subject.find_header('8675309') }.to raise_error
+          expect { test_repository.find_header('8675309') }.to raise_error
         end
         it 'returns the Header when the object is found' do
           allow(Models::Header).to receive(:find).with('8675309').and_return(:found)
-          expect(subject.find_header('8675309')).to eq(:found)
+          expect(test_repository.find_header('8675309')).to eq(:found)
         end
       end
 
       context '#submit_create_header_form' do
         let(:user) { User.new(id: '123') }
         let(:form) do
-          subject.build_create_header_form(
+          test_repository.build_create_header_form(
             attributes: {
               title: 'This is my title',
               work_publication_strategy: 'do_not_know',
@@ -57,12 +49,12 @@ module Sipity
         context 'with invalid data' do
           it 'will not create a a header' do
             allow(form).to receive(:valid?).and_return(false)
-            expect { subject.submit_create_header_form(form, requested_by: user) }.
+            expect { test_repository.submit_create_header_form(form, requested_by: user) }.
               to_not change { Models::Header.count }
           end
           it 'will return false' do
             allow(form).to receive(:valid?).and_return(false)
-            expect(subject.submit_create_header_form(form, requested_by: user)).to eq(false)
+            expect(test_repository.submit_create_header_form(form, requested_by: user)).to eq(false)
           end
         end
         context 'with valid data' do
@@ -70,7 +62,7 @@ module Sipity
           it 'will return the header having created the header, added the attributes,
               assigned collaborators, assigned permission, and loggged the event' do
             allow(form).to receive(:valid?).and_return(true)
-            response = subject.submit_create_header_form(form, requested_by: user)
+            response = test_repository.submit_create_header_form(form, requested_by: user)
 
             expect(response).to be_a(Models::Header)
             expect(Models::Header.count).to eq(1)
@@ -84,7 +76,7 @@ module Sipity
 
       context '#build_create_header_form' do
         it 'will build an object that can be submitted' do
-          expect(subject.build_create_header_form).to respond_to(:submit)
+          expect(test_repository.build_create_header_form).to respond_to(:submit)
         end
       end
 
@@ -92,9 +84,10 @@ module Sipity
         let(:header) { Models::Header.new(title: 'Hello World', id: '123') }
         it 'will raise an exception if the header is not persisted' do
           allow(header).to receive(:persisted?).and_return(false)
-          expect { subject.build_update_header_form(header: header) }.
+          expect { test_repository.build_update_header_form(header: header) }.
             to raise_error(RuntimeError)
         end
+
         context 'with a persisted object will return an object that' do
           before { allow(header).to receive(:persisted?).and_return(true) }
           subject { test_repository.build_update_header_form(header: header) }
@@ -112,17 +105,17 @@ module Sipity
       context '#submit_update_header_form' do
         let(:user) { User.new(id: '123') }
         let(:header) { Models::Header.create(title: 'My Title', work_publication_strategy: 'do_not_know') }
-        let(:form) { subject.build_update_header_form(header: header, attributes: { title: 'My New Title', publisher: 'dance' }) }
+        let(:form) { test_repository.build_update_header_form(header: header, attributes: { title: 'My New Title', publisher: 'dance' }) }
         context 'with invalid data' do
           before do
             allow(header).to receive(:persisted?).and_return(true)
             allow(form).to receive(:valid?).and_return(false)
           end
           it 'will return false' do
-            expect(subject.submit_update_header_form(form, requested_by: user)).to eq(false)
+            expect(test_repository.submit_update_header_form(form, requested_by: user)).to eq(false)
           end
           it 'will NOT update the header' do
-            expect { subject.submit_update_header_form(form, requested_by: user) }.
+            expect { test_repository.submit_update_header_form(form, requested_by: user) }.
               to_not change { header.reload.title }
           end
         end
@@ -133,7 +126,7 @@ module Sipity
             allow(form).to receive(:valid?).and_return(true)
           end
           it 'will return the header after updating the header, additional attributes, and creating an event log entry' do
-            response = subject.submit_update_header_form(form, requested_by: user)
+            response = test_repository.submit_update_header_form(form, requested_by: user)
 
             expect(response).to eq(header)
             expect(header.reload.title).to eq('My New Title')
