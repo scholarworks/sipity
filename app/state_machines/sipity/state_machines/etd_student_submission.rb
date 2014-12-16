42ae59dfbfe20e73eec489156d614a699f84ce33
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-16 13:29:59 -0500

    Renaming state events

--- app/state_machines/sipity/state_machines/etd_student_submission.rb
+++ app/state_machines/sipity/state_machines/etd_student_submission.rb
@@ -12,11 +12,11 @@ module Sipity
 
       def build_state_machine
         state_machine = MicroMachine.new(:new)
-        state_machine.when(:student_submits, new: :under_review)
+        state_machine.when(:submit_for_ingest, new: :under_review)
         state_machine.when(:request_revisions, under_review: :revisions_needed, revisions_needed: :revisions_needed)
-        state_machine.when(:grad_school_approves, under_review: :ready_for_ingest, revisions_needed: :ready_for_ingest)
+        state_machine.when(:approve_for_ingest, under_review: :ready_for_ingest, revisions_needed: :ready_for_ingest)
         state_machine.when(:ingest_completed, ready_for_ingest: :ingested)
-        state_machine.when(:cataloging_completed, ingested: :cataloged)
+        state_machine.when(:finish_cataloging, ingested: :cataloged)
         state_machine.when(:finish, cataloged: :done)
         @state_machine = state_machine
       end
--- spec/state_machines/sipity/state_machines/etd_student_submission_spec.rb
+++ spec/state_machines/sipity/state_machines/etd_student_submission_spec.rb
@@ -11,42 +11,51 @@ module Sipity
           it 'will allow :update? for [:creating_user, :advisor]'
           it 'will allow :delete? for [:creating_user]'
           it 'will allow :show? for [:creating_user, :advisor, :etd_reviewer]'
+          it 'will allow :submit_for_ingest? for [:creating_user]'
         end
 
         context 'for :under_review' do
           it 'will allow :update? for [:etd_reviewer]'
           it 'will allow :delete? for []'
           it 'will allow :show? for [:creating_user, :advisor, :etd_reviewer]'
+          it 'will allow :request_revisions? for [:etd_reviewer]'
+          it 'will allow :approve_for_ingest? for [:etd_reviewer]'
         end
 
         context 'for :revisions_needed' do
           it 'will allow :update? for [:etd_reviewer]'
           it 'will allow :delete? for []'
           it 'will allow :show? for [:creating_user, :advisor, :etd_reviewer]'
+          it 'will allow :request_revisions? for [:etd_reviewer]'
+          it 'will allow :approve_for_ingest? for [:etd_reviewer]'
         end
 
         context 'for :ready_for_ingest' do
           it 'will allow :update? for []'
           it 'will allow :delete? for []'
           it 'will allow :show? for [:creating_user, :advisor, :etd_reviewer]'
+          it 'will allow :ingest? for []'
         end
 
         context 'for :ingested' do
           it 'will allow :update? for []'
           it 'will allow :delete? for []'
           it 'will allow :show? for [:creating_user, :advisor, :etd_reviewer]'
+          it 'will allow :ingest_completed? for []'
         end
 
         context 'for :ready_for_cataloging' do
           it 'will allow :update? for []'
           it 'will allow :delete? for []'
-          it 'will allow :show? for [:creating_user, :advisor, :etd_reviewer]'
+          it 'will allow :show? for [:creating_user, :advisor, :etd_reviewer, :cataloging]'
+          it 'will allow :finish_cataloging? for [:cataloger]'
         end
 
         context 'for :cataloged' do
           it 'will allow :update? for []'
           it 'will allow :delete? for []'
           it 'will allow :show? for [:creating_user, :advisor, :etd_reviewer]'
+          it 'will allow :mark_as_done? for []'
         end
 
         context 'for :done' do
@@ -57,7 +66,7 @@ module Sipity
       end
 
       context 'state transitions when' do
-        context ':student_submits is triggered' do
+        context ':submit_for_ingest is triggered' do
           it 'will send an email notification to the grad school'
           it 'will send an email confirmation to the student with a URL to that item'
           it 'will add permission entries for the etd reviewers for the given ETD'
@@ -71,7 +80,7 @@ module Sipity
           it 'will update the ETDs state to :revisions_needed'
         end
 
-        context ':grad_school_approves is triggered' do
+        context ':approve_for_ingest is triggered' do
           it 'will send an email notification to the student and grad school and any additional emails provided (i.e. ISSA)'
           it 'will record the event for auditing purposes'
           it 'will update the ETDs state to :ready_for_ingest'
@@ -82,6 +91,7 @@ module Sipity
           it 'will submit an ROF job to ingest the ETD; Only ETD reviewers will have rights to the ingested object'
           it 'will record the event for auditing purposes'
           it 'will update the ETDs state to :ingested'
+          it 'will add permission entries for the catalog reviewers of the given ETD'
           it 'will trigger :ingest_completed'
         end
 
@@ -91,7 +101,7 @@ module Sipity
           it 'will update the ETDs state to :ready_for_cataloging'
         end
 
-        context ':cataloging_completed is triggered' do
+        context ':finish_cataloging is triggered' do
           it 'will record the event for auditing purposes'
           it 'will update the ETDs state to :cataloged'
           it 'will trigger the :finish event'
