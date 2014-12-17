5fecf7b5d24a2b96f18060cd1039262586753a52
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-17 15:21:47 -0500

    Teasing out notification methods

---
+++ app/services/sipity/repo/notification_methods.rb
@@ -0,0 +1,23 @@
+module Sipity
+  module Repo
+    # Responsible for coordinating with the notification layer.
+    # What is the notification layer? Don't know yet. But I suspect it will
+    # be us sending various emails to recipients.
+    module NotificationMethods
+      def send_notification_for_entity_trigger(notification:, entity:, to_roles:)
+        # These instance variables are not needed; But to appeas Rubocop I'm
+        # using them.
+        @notification, @entity, @to_roles = notification, entity, to_roles
+        @to = convert_entity_and_roles_to_email_recipients(roles: to_roles, entity: entity)
+      end
+
+      private
+
+      # Responsible for extracting the emails of people with the given role
+      # for the given entity. Note, a role is not a group.
+      def convert_entity_and_roles_to_email_recipients(roles:, entity:)
+        @entity, @to_roles = roles, entity
+      end
+    end
+  end
+end
---
+++ spec/services/sipity/repo/notification_methods_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+module Sipity
+  module Repo
+    RSpec.describe NotificationMethods, type: :repository_methods do
+      context '#send_notification_for_entity_trigger' do
+        let(:notification) { double }
+        let(:entity) { double }
+        let(:to_roles) { double }
+
+        it 'is a placeholder' do
+          test_repository.send_notification_for_entity_trigger(notification: notification, entity: entity, to_roles: to_roles)
+        end
+      end
+    end
+  end
+end
