0d45dc5f09879b31757bc664ecc9bbd3c2b7b271
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-17 20:56:20 -0500

    Adding further explanantion regarding queries
    [skip ci]

--- app/services/sipity/queries.rb
+++ app/services/sipity/queries.rb
@@ -5,6 +5,9 @@ module Sipity
   # In separating them, I hope to expose a Respository::ReadOnly. A longer term
   # goal is to craft custom repository collaborators based on the context.
   #
+  # In keeping the queries separate and the concerns small, it would be feasible
+  # to enrich domain objects with methods from the Queries namespace.
+  #
   # @see http://martinfowler.com/bliki/CommandQuerySeparation.html Martin
   #   Folwer's article on Command/Query separation
   module Queries
