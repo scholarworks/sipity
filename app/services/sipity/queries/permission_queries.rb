e8489419070e372c9367b33f5a12a1a272a4b21e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-18 17:53:03 -0500

    Giving greater meaning to variables

--- app/services/sipity/queries/permission_queries.rb
+++ app/services/sipity/queries/permission_queries.rb
@@ -14,7 +14,7 @@ module Sipity
         perm_table = Models::Permission.arel_table
         memb_table = Models::GroupMembership.arel_table
 
-        group_permission_query = memb_table.project(memb_table[:user_id]).where(
+        subquery_user_relation_to_entity_by_group = memb_table.project(memb_table[:user_id]).where(
           memb_table[:user_id].in(
             perm_table.project(perm_table[:actor_id]).where(
               perm_table[:actor_type].eq(group_class.base_class).
@@ -24,15 +24,15 @@ module Sipity
             )
           )
         )
-        user_permission_query = perm_table.project(perm_table[:actor_id]).where(
+        subqyery_user_relation_entity = perm_table.project(perm_table[:actor_id]).where(
           perm_table[:actor_type].eq(user_class.base_class).
           and(perm_table[:role].in(roles)).
           and(perm_table[:entity_type].eq(entity.class.base_class)).
           and(perm_table[:entity_id].eq(entity.to_key))
         )
         user_class.where(
-          user_table[:id].in(group_permission_query).
-          or(user_table[:id].in(user_permission_query))
+          user_table[:id].in(subquery_user_relation_to_entity_by_group).
+          or(user_table[:id].in(subqyery_user_relation_entity))
         )
       end
       public :find_users_by_entity_and_roles
