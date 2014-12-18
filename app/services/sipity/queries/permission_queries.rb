c1933c5c9a65daaac39abd1b20be8df0cb230ab0
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-18 17:59:06 -0500

    Renaming find to scope
    It is more appropriate to think of them as scopes instead of finders.
    This means that in theory they can be chained.
    
    Also adding documentation.

--- app/services/sipity/queries/permission_queries.rb
+++ app/services/sipity/queries/permission_queries.rb
@@ -5,16 +5,38 @@ module Sipity
       module_function
 
       def emails_for_associated_users(roles:, entity:)
-        find_users_by_entity_and_roles(roles: roles, entity: entity).pluck(:email)
+        scope_users_by_entity_and_roles(roles: roles, entity: entity).pluck(:email)
       end
       public :emails_for_associated_users
 
-      def find_users_by_entity_and_roles(roles:, entity:, user_class: User, group_class: Models::Group)
+      # Responsible for returning a User scope:
+      # That will include all users
+      # That have one or more roles
+      # In which the user has a direction relation to the entity
+      # Or in which a relation to the entity can be inferred by group membership
+      #
+      # If no roles are given, no entities will be returned.
+      #
+      # @param roles [Array<String>]
+      # @param entity [ActiveRecord::Base]
+      #
+      # @return ActiveRecord::Relation
+      #
+      # @note Welcome to the land of AREL.
+      # @see https://github.com/rails/arel AREL - A Relational Algebra
+      def scope_users_by_entity_and_roles(roles:, entity:, user_class: User, group_class: Models::Group)
         user_table = user_class.arel_table
         perm_table = Models::Permission.arel_table
         memb_table = Models::GroupMembership.arel_table
 
-        subquery_user_relation_to_entity_by_group = memb_table.project(memb_table[:user_id]).where(
+        subqyery_user_relation_entity = perm_table.project(perm_table[:actor_id]).where(
+          perm_table[:actor_type].eq(user_class.base_class).
+          and(perm_table[:role].in(roles)).
+          and(perm_table[:entity_type].eq(entity.class.base_class)).
+          and(perm_table[:entity_id].eq(entity.to_key))
+        )
+
+        subquery_user_relation_to_entity_by_group_membership = memb_table.project(memb_table[:user_id]).where(
           memb_table[:user_id].in(
             perm_table.project(perm_table[:actor_id]).where(
               perm_table[:actor_type].eq(group_class.base_class).
@@ -24,20 +46,14 @@ module Sipity
             )
           )
         )
-        subqyery_user_relation_entity = perm_table.project(perm_table[:actor_id]).where(
-          perm_table[:actor_type].eq(user_class.base_class).
-          and(perm_table[:role].in(roles)).
-          and(perm_table[:entity_type].eq(entity.class.base_class)).
-          and(perm_table[:entity_id].eq(entity.to_key))
-        )
         user_class.where(
-          user_table[:id].in(subquery_user_relation_to_entity_by_group).
+          user_table[:id].in(subquery_user_relation_to_entity_by_group_membership).
           or(user_table[:id].in(subqyery_user_relation_entity))
         )
       end
-      public :find_users_by_entity_and_roles
+      public :scope_users_by_entity_and_roles
 
-      # Responsible for returning a list of enitites:
+      # Responsible for returning a scope for the entity type:
       # That match the given :entity_type
       # And for which the given :user has at least one of the given :roles.
       #
@@ -54,23 +70,25 @@ module Sipity
       def scope_permission_resolver(entity_type:, user:, roles:)
         perm_table = Models::Permission.arel_table
         memb_table = Models::GroupMembership.arel_table
+        group_class =  Models::Group
 
-        user_permission_subquery = perm_table.project(perm_table[:entity_id]).where(
+        subquery_entity_relation_to_user = perm_table.project(perm_table[:entity_id]).where(
           perm_table[:actor_id].eq(user.to_key).
           and(perm_table[:actor_type].eq(user.class.base_class)).
           and(perm_table[:role].in(roles)).
           and(perm_table[:entity_type].eq(entity_type.base_class))
         )
 
-        group_permission_subquery = perm_table.project(perm_table[:entity_id]).where(
+        subquery_entity_relation_to_user_by_group_membership = perm_table.project(perm_table[:entity_id]).where(
           perm_table[:role].in(roles).
           and(perm_table[:entity_type].eq(entity_type.base_class)).
-          and(perm_table[:actor_type].eq(Models::Group.base_class)).
+          and(perm_table[:actor_type].eq(group_class.base_class)).
           and(perm_table[:actor_id].in(memb_table.project(memb_table[:group_id]).where(memb_table[:user_id].eq(user.to_key))))
         )
+
         entity_type.where(
-          entity_type.arel_table[:id].in(user_permission_subquery).
-          or(entity_type.arel_table[:id].in(group_permission_subquery))
+          entity_type.arel_table[:id].in(subquery_entity_relation_to_user).
+          or(entity_type.arel_table[:id].in(subquery_entity_relation_to_user_by_group_membership))
         )
       end
       public :scope_permission_resolver
