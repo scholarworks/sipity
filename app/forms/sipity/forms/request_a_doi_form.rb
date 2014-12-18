49ba7937e28bc99bf562bd67ebd854cca242c71f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-17 19:03:55 -0500

    Refactoring away deprecated file

--- app/forms/sipity/forms/request_a_doi_form.rb
+++ app/forms/sipity/forms/request_a_doi_form.rb
@@ -19,7 +19,7 @@ module Sipity
       validates :publication_date, presence: true
 
       def authors(decorator: Decorators::CollaboratorDecorator)
-        RepositoryMethods::CollaboratorMethodss.
+        RepositoryMethods::CollaboratorMethods.
           header_collaborators_for(header: header, role: 'author').
           map { |obj| decorator.decorate(obj) }
       end
--- spec/forms/sipity/forms/request_a_doi_form_spec.rb
+++ spec/forms/sipity/forms/request_a_doi_form_spec.rb
@@ -24,7 +24,8 @@ module Sipity
 
       let(:authors) { [double('Author')] }
       it 'will have #authors' do
-        allow(RepositoryMethods::Support::Collaborators).to receive(:for).with(header: header, role: 'author').and_return(authors)
+        allow(RepositoryMethods::CollaboratorMethods).to receive(:header_collaborators_for).
+          with(header: header, role: 'author').and_return(authors)
         allow(Decorators::CollaboratorDecorator).to receive(:decorate).with(authors[0])
         subject.authors
       end
