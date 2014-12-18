d6e871923119ea0a9e9cbb8024dc24c2b692ce25
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-18 14:33:27 -0500

    Teasing out general grant_permission_for! method

--- app/services/sipity/commands/permission_commands.rb
+++ app/services/sipity/commands/permission_commands.rb
@@ -3,13 +3,20 @@ module Sipity
   module Commands
     # Commands related to the permission model
     module PermissionCommands
+      module_function
+
       def grant_creating_user_permission_for!(entity:, user: nil, group: nil, actor: nil)
         role = Models::Permission::CREATING_USER
-        actors = [user, group, actor].flatten.compact
-        actors.each { |an_actor| Models::Permission.create!(entity: entity, actor: an_actor, role: role) }
+        actors = [user, group, actor]
+        grant_permission_for!(entity: entity, actors: actors, role: role)
       end
-      module_function :grant_creating_user_permission_for!
       public :grant_creating_user_permission_for!
+
+      def grant_permission_for!(entity:, actors:, role:)
+        Array.wrap(actors).flatten.compact.each { |an_actor| Models::Permission.create!(entity: entity, actor: an_actor, role: role) }
+      end
+      private :grant_permission_for!
+      private_class_method :grant_permission_for!
     end
     private_constant :PermissionCommands
   end
