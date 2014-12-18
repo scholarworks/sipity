e1e797c40ab9809784a8e8fc1433e851d59c7d0a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-18 17:24:57 -0500

    Migrating permission query to "correct" module
    I believe this is the more appropriate resting place. I believe there
    is a need for a permission query suite of objects; There is state in
    the class functions, but class functions are hard to refactor on their
    own.

--- app/services/sipity/queries/permission_queries.rb
+++ app/services/sipity/queries/permission_queries.rb
@@ -4,6 +4,39 @@ module Sipity
     module PermissionQueries
       module_function
 
+      def emails_for_associated_users(roles:, entity:)
+        find_users_by_entity_and_roles(roles: roles, entity: entity).pluck(:email)
+      end
+      public :emails_for_associated_users
+
+      def find_users_by_entity_and_roles(roles:, entity:, user_class: User, group_class: Models::Group)
+        user_table = user_class.arel_table
+        perm_table = Models::Permission.arel_table
+        memb_table = Models::GroupMembership.arel_table
+
+        group_permission_query = memb_table.project(memb_table[:user_id]).where(
+          memb_table[:user_id].in(
+            perm_table.project(perm_table[:actor_id]).where(
+              perm_table[:actor_type].eq(group_class.base_class).
+              and(perm_table[:role].in(roles)).
+              and(perm_table[:entity_type].eq(entity.class.base_class)).
+              and(perm_table[:entity_id].eq(entity.to_key))
+            )
+          )
+        )
+        user_permission_query = perm_table.project(perm_table[:actor_id]).where(
+          perm_table[:actor_type].eq(user_class.base_class).
+          and(perm_table[:role].in(roles)).
+          and(perm_table[:entity_type].eq(entity.class.base_class)).
+          and(perm_table[:entity_id].eq(entity.to_key))
+        )
+        user_class.where(
+          user_table[:id].in(group_permission_query).
+          or(user_table[:id].in(user_permission_query))
+        )
+      end
+      public :find_users_by_entity_and_roles
+
       # Responsible for returning a list of enitites:
       # That match the given :entity_type
       # And for which the given :user has at least one of the given :roles.
