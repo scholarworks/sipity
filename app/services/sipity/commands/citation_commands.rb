7e469077bbd992005e6b673d5ea2932576ca4637
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-17 21:22:25 -0500

    Extracting citation commands

---
+++ app/services/sipity/commands/citation_commands.rb
@@ -0,0 +1,19 @@
+module Sipity
+  module Commands
+    # Commands
+    module CitationCommands
+      def submit_assign_a_citation_form(form, requested_by:)
+        form.submit do |f|
+          RepositoryMethods::AdditionalAttributeMethods::Commands.update_header_attribute_values!(
+            header: f.header, key: Models::AdditionalAttribute::CITATION_PREDICATE_NAME, values: f.citation
+          )
+          RepositoryMethods::AdditionalAttributeMethods::Commands.update_header_attribute_values!(
+            header: f.header, key: Models::AdditionalAttribute::CITATION_TYPE_PREDICATE_NAME, values: f.type
+          )
+          RepositoryMethods::EventLogMethods::Commands.log_event!(entity: f.header, user: requested_by, event_name: __method__)
+          f.header
+        end
+      end
+    end
+  end
+end
--- app/services/sipity/repository_methods/citation_methods.rb
+++ app/services/sipity/repository_methods/citation_methods.rb
@@ -6,23 +6,7 @@ module Sipity
       extend ActiveSupport::Concern
       included do |base|
         base.send(:include, Queries::CitationQueries)
-        base.send(:include, Commands)
-      end
-
-      # Commands
-      module Commands
-        def submit_assign_a_citation_form(form, requested_by:)
-          form.submit do |f|
-            AdditionalAttributeMethods::Commands.update_header_attribute_values!(
-              header: f.header, key: Models::AdditionalAttribute::CITATION_PREDICATE_NAME, values: f.citation
-            )
-            AdditionalAttributeMethods::Commands.update_header_attribute_values!(
-              header: f.header, key: Models::AdditionalAttribute::CITATION_TYPE_PREDICATE_NAME, values: f.type
-            )
-            EventLogMethods::Commands.log_event!(entity: f.header, user: requested_by, event_name: __method__)
-            f.header
-          end
-        end
+        base.send(:include, Commands::CitationCommands)
       end
     end
   end
