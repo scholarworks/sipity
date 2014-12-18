e5e678692388a30b6901dda573c74d479a935335
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-18 17:16:34 -0500

    Teasing apart user query methods

--- app/services/sipity/commands/notification_commands.rb
+++ app/services/sipity/commands/notification_commands.rb
@@ -7,7 +7,6 @@ module Sipity
         # These instance variables are not needed; But to appeas Rubocop I'm
         # using them.
         @notification, @entity, @to_roles = notification, entity, to_roles
-        @to = emails_for_associated_users(roles: to_roles, entity: entity)
       end
     end
     private_constant :NotificationCommands
--- app/services/sipity/queries/notification_queries.rb
+++ app/services/sipity/queries/notification_queries.rb
@@ -2,8 +2,12 @@ module Sipity
   module Queries
     # Queries
     module NotificationQueries
-      def emails_for_associated_users(roles:, entity:, user_class: User, group_class: Models::Group)
-        user_table = User.arel_table
+      def emails_for_associated_users(roles:, entity:)
+        find_users_by_entity_and_roles(roles: roles, entity: entity).pluck(:email)
+      end
+
+      def find_users_by_entity_and_roles(roles:, entity:, user_class: User, group_class: Models::Group)
+        user_table = user_class.arel_table
         perm_table = Models::Permission.arel_table
         memb_table = Models::GroupMembership.arel_table
 
@@ -23,11 +27,10 @@ module Sipity
           and(perm_table[:entity_type].eq(entity.class.base_class)).
           and(perm_table[:entity_id].eq(entity.to_key))
         )
-        resolver = user_class.where(
+        user_class.where(
           user_table[:id].in(group_permission_query).
           or(user_table[:id].in(user_permission_query))
         )
-        resolver.pluck(:email)
       end
     end
   end
