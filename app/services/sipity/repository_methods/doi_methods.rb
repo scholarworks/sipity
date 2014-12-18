ceda75a9f7a740c1cdb20426909beec8cb298493
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-17 20:01:19 -0500

    Teasing apart Query and Command methods
    Does this even make sense?

--- app/services/sipity/repository_methods/doi_methods.rb
+++ app/services/sipity/repository_methods/doi_methods.rb
@@ -5,86 +5,97 @@ module Sipity
   module RepositoryMethods
     # DOI related methods
     module DoiMethods
-      # HACK: This is a query method
-      def doi_request_is_pending?(header)
-        # @todo This query is not entirely correct. It needs to interrogate
-        #   the states of the DoiCreationRequest. In this case, I have a leaky
-        #   state machine as its enforcement is in
-        #   Sipity::Jobs::DoiCreationRequestJob
-        Models::DoiCreationRequest.where(header: header).any?
+      extend ActiveSupport::Concern
+      included do |base|
+        base.send(:include, Queries)
+        base.send(:include, Commands)
       end
 
-      # HACK: This is a query method
-      def find_doi_creation_request(header:)
-        # Going to give you the header as part of the find; You'll probably want
-        # it.
-        Models::DoiCreationRequest.includes(:header).where(header: header).first!
-      end
+      module Queries
+        # HACK: This is a query method
+        def doi_request_is_pending?(header)
+          # @todo This query is not entirely correct. It needs to interrogate
+          #   the states of the DoiCreationRequest. In this case, I have a leaky
+          #   state machine as its enforcement is in
+          #   Sipity::Jobs::DoiCreationRequestJob
+          Models::DoiCreationRequest.where(header: header).any?
+        end
 
-      # HACK: This is a query method
-      def doi_already_assigned?(header)
-        AdditionalAttributeMethods.header_attribute_values_for(
-          header: header, key: Models::AdditionalAttribute::DOI_PREDICATE_NAME
-        ).any?
-      end
+        # HACK: This is a query method
+        def find_doi_creation_request(header:)
+          # Going to give you the header as part of the find; You'll probably want
+          # it.
+          Models::DoiCreationRequest.includes(:header).where(header: header).first!
+        end
 
-      # HACK: This is a query method
-      def build_assign_a_doi_form(attributes = {})
-        Forms::AssignADoiForm.new(attributes)
-      end
+        # HACK: This is a query method
+        def doi_already_assigned?(header)
+          AdditionalAttributeMethods.header_attribute_values_for(
+            header: header, key: Models::AdditionalAttribute::DOI_PREDICATE_NAME
+          ).any?
+        end
 
-      # HACK: This is a command method
-      def submit_assign_a_doi_form(form, requested_by:)
-        form.submit do |f|
-          EventLogMethods.log_event!(entity: f.header, user: requested_by, event_name: __method__)
-          AdditionalAttributeMethods.update_header_attribute_values!(header: f.header, key: f.identifier_key, values: f.identifier)
+        # HACK: This is a query method
+        def build_assign_a_doi_form(attributes = {})
+          Forms::AssignADoiForm.new(attributes)
+        end
+
+        # HACK: This is a query method
+        def gather_doi_creation_request_metadata(header:)
+          Services::DoiCreationRequestMetadataGatherer.call(header: header)
+        end
+
+        # HACK: This is a query method
+        def build_request_a_doi_form(attributes = {})
+          Forms::RequestADoiForm.new(attributes)
         end
       end
 
-      # HACK: This is a query method
-      def build_request_a_doi_form(attributes = {})
-        Forms::RequestADoiForm.new(attributes)
-      end
+      module Commands
 
-      # HACK: This is a command method
-      def submit_request_a_doi_form(form, requested_by:)
-        form.submit do |f|
+        # HACK: This is a command method
+        def submit_assign_a_doi_form(form, requested_by:)
+          form.submit do |f|
+            EventLogMethods.log_event!(entity: f.header, user: requested_by, event_name: __method__)
+            AdditionalAttributeMethods.update_header_attribute_values!(header: f.header, key: f.identifier_key, values: f.identifier)
+          end
+        end
+
+        # HACK: This is a command method
+        def submit_request_a_doi_form(form, requested_by:)
+          form.submit do |f|
+            AdditionalAttributeMethods.update_header_attribute_values!(
+              header: f.header, key: Models::AdditionalAttribute::PUBLISHER_PREDICATE_NAME, values: f.publisher
+            )
+            AdditionalAttributeMethods.update_header_publication_date!(header: f.header, publication_date: f.publication_date)
+            EventLogMethods.log_event!(entity: f.header, user: requested_by, event_name: __method__)
+            submit_doi_creation_request_job!(header: f.header)
+          end
+        end
+
+        # HACK: This is a command method
+        def update_header_doi_creation_request_state!(header:, state:, response_message: nil)
+          doi_creation_request = find_doi_creation_request(header: header)
+          attributes = { state: state.to_s.downcase }
+          attributes[:response_message] = response_message if response_message.present?
+          doi_creation_request.update(attributes)
+        end
+
+        # HACK: This is a command method
+        def update_header_with_doi_predicate!(header:, values:)
           AdditionalAttributeMethods.update_header_attribute_values!(
-            header: f.header, key: Models::AdditionalAttribute::PUBLISHER_PREDICATE_NAME, values: f.publisher
+            header: header, key: Models::AdditionalAttribute::DOI_PREDICATE_NAME, values: values
           )
-          AdditionalAttributeMethods.update_header_publication_date!(header: f.header, publication_date: f.publication_date)
-          EventLogMethods.log_event!(entity: f.header, user: requested_by, event_name: __method__)
-          submit_doi_creation_request_job!(header: f.header)
         end
-      end
 
-      # HACK: This is a command method
-      def update_header_doi_creation_request_state!(header:, state:, response_message: nil)
-        doi_creation_request = find_doi_creation_request(header: header)
-        attributes = { state: state.to_s.downcase }
-        attributes[:response_message] = response_message if response_message.present?
-        doi_creation_request.update(attributes)
-      end
+        private
 
-      # HACK: This is a command method
-      def update_header_with_doi_predicate!(header:, values:)
-        AdditionalAttributeMethods.update_header_attribute_values!(
-          header: header, key: Models::AdditionalAttribute::DOI_PREDICATE_NAME, values: values
-        )
-      end
-
-      # HACK: This is a query method
-      def gather_doi_creation_request_metadata(header:)
-        Services::DoiCreationRequestMetadataGatherer.call(header: header)
-      end
-
-      private
-
-      # HACK: This is a command method
-      def submit_doi_creation_request_job!(header:)
-        request = Models::DoiCreationRequest.create!(header: header)
-        Jobs.submit('doi_creation_request_job', header.id)
-        request
+        # HACK: This is a command method
+        def submit_doi_creation_request_job!(header:)
+          request = Models::DoiCreationRequest.create!(header: header)
+          Jobs.submit('doi_creation_request_job', header.id)
+          request
+        end
       end
     end
     private_constant :DoiMethods
--- app/services/sipity/repository_methods/notification_methods.rb
+++ app/services/sipity/repository_methods/notification_methods.rb
@@ -5,22 +5,30 @@ module Sipity
     # What is the notification layer? Don't know yet. But I suspect it will
     # be us sending various emails to recipients.
     module NotificationMethods
-      # HACK: This is a command method
-      def send_notification_for_entity_trigger(notification:, entity:, to_roles:)
-        # These instance variables are not needed; But to appeas Rubocop I'm
-        # using them.
-        @notification, @entity, @to_roles = notification, entity, to_roles
-        @to = convert_entity_and_roles_to_email_recipients(roles: to_roles, entity: entity)
+      extend ActiveSupport::Concern
+      included do |base|
+        base.send(:include, Commands)
+        base.send(:include, Queries)
       end
 
-      private
+      module Queries
+        # HACK: This is a query method
+        #
+        # Responsible for extracting the emails of people with the given role
+        # for the given entity. Note, a role is not a group.
+        def convert_entity_and_roles_to_email_recipients(roles:, entity:)
+          @entity, @to_roles = roles, entity
+        end
+      end
 
-      # HACK: This is a query method
-      #
-      # Responsible for extracting the emails of people with the given role
-      # for the given entity. Note, a role is not a group.
-      def convert_entity_and_roles_to_email_recipients(roles:, entity:)
-        @entity, @to_roles = roles, entity
+      module Commands
+        # HACK: This is a command method
+        def send_notification_for_entity_trigger(notification:, entity:, to_roles:)
+          # These instance variables are not needed; But to appeas Rubocop I'm
+          # using them.
+          @notification, @entity, @to_roles = notification, entity, to_roles
+          @to = convert_entity_and_roles_to_email_recipients(roles: to_roles, entity: entity)
+        end
       end
     end
     private_constant :NotificationMethods
