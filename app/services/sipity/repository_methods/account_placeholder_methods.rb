03cfa3c899aeb9b9c6a965da58f6314da70aea54
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-17 20:05:16 -0500

    Normalizing query/command separation
    I believe I want to keep these separate. It will help in looking for
    things? I hope. But this remains a question of how people will be
    interacting with the object.
    
    Does a Resourced based interaction make sense?

--- app/services/sipity/repository_methods/account_placeholder_methods.rb
+++ app/services/sipity/repository_methods/account_placeholder_methods.rb
@@ -7,9 +7,30 @@ module Sipity
       #   This is an abomination. I would like to separate the concerns a bit
       #   better.
       extend ActiveSupport::Concern
+
       included do |base|
-        base.send(:include, Queries::AccountPlaceholderQueries)
-        base.send(:include, Commands::AccountPlaceholderCommands)
+        base.send(:include, Queries)
+        base.send(:include, Commands)
+      end
+
+      module Commands
+        def submit_create_orcid_account_placeholder_form(form, requested_by:)
+          form.submit do |f|
+            placeholder = Models::AccountPlaceholder.create!(
+              identifier: f.identifier, identifier_type: Models::AccountPlaceholder::ORCID_IDENTIFIER_TYPE,
+              name: f.name
+            )
+            Models::Permission.create!(entity: placeholder, user: requested_by, role: Models::Permission::CREATING_USER)
+            EventLogMethods.log_event!(entity: placeholder, user: requested_by, event_name: __method__)
+            placeholder
+          end
+        end
+      end
+
+      module Queries
+        def build_create_orcid_account_placeholder_form(attributes: {})
+          Forms::CreateOrcidAccountPlaceholderForm.new(attributes)
+        end
       end
     end
     private_constant :AccountPlaceholderMethods
