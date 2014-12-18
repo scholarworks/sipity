e3a81d9803f2558dbc4f5e6f819fa9e803250332
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-17 21:36:00 -0500

    Extracting NotificationCommands

--- app/services/sipity/repository_methods/notification_methods.rb
+++ app/services/sipity/repository_methods/notification_methods.rb
@@ -7,27 +7,7 @@ module Sipity
     module NotificationMethods
       extend ActiveSupport::Concern
       included do |base|
-        base.send(:include, Commands)
-        base.send(:include, Queries)
-      end
-
-      # Queries
-      module Queries
-        # Responsible for extracting the emails of people with the given role
-        # for the given entity. Note, a role is not a group.
-        def convert_entity_and_roles_to_email_recipients(roles:, entity:)
-          @entity, @to_roles = roles, entity
-        end
-      end
-
-      # Commands
-      module Commands
-        def send_notification_for_entity_trigger(notification:, entity:, to_roles:)
-          # These instance variables are not needed; But to appeas Rubocop I'm
-          # using them.
-          @notification, @entity, @to_roles = notification, entity, to_roles
-          @to = convert_entity_and_roles_to_email_recipients(roles: to_roles, entity: entity)
-        end
+        base.send(:include, Commands::NotificationCommands)
       end
     end
   end
