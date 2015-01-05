8145ee6160b2fa829dbbf4e78c2049500de1b5a9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-05 12:29:41 -0500

    Teasing out more of NotificationCommands

--- spec/services/sipity/commands/notification_commands_spec.rb
+++ spec/services/sipity/commands/notification_commands_spec.rb
@@ -7,8 +7,11 @@ module Sipity
         let(:notification) { double }
         let(:entity) { double }
         let(:to_roles) { double }
+        let(:emails) { ['test@hello.com'] }
 
         it 'is a placeholder' do
+          allow(Queries::PermissionQueries).to receive(:emails_for_associated_users).and_return(emails)
+          allow(Services::Notifier).to receive(:deliver).with(notification: notification, email: emails, entity: entity)
           test_repository.send_notification_for_entity_trigger(notification: notification, entity: entity, to_roles: to_roles)
         end
       end
