042a3df56ed62d0d0c7a958c0fdbfdb4dc193b5d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-06 10:17:27 -0500

    Tweak to get tests passing.

--- spec/services/sipity/commands/notification_commands_spec.rb
+++ spec/services/sipity/commands/notification_commands_spec.rb
@@ -11,7 +11,7 @@ module Sipity
 
         it 'is a placeholder' do
           allow(Queries::PermissionQueries).to receive(:emails_for_associated_users).and_return(emails)
-          allow(Services::Notifier).to receive(:deliver).with(notification: notification, email: emails, entity: entity)
+          allow(Services::Notifier).to receive(:deliver).with(notification: notification, to: emails, entity: entity)
           test_repository.send_notification_for_entity_trigger(notification: notification, entity: entity, to_roles: to_roles)
         end
       end
