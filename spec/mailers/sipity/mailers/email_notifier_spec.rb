df46f8f7261bde241e4e023c9174faca1f0b7b93
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-07 14:13:32 -0500

    Spec-ing additional EmailNotifier methods
    I extracted the following notifications from Sipity::StateMachines::
    EtdStudentSubmission (see
    ./app/state_machines/sipity/state_machines/etd_student_submission.rb)
    
    * confirmation_of_entity_submitted_for_review
    * request_revisions_from_creator
    * entity_ready_for_review
    * entity_ready_for_cataloging
    * confirmation_of_entity_approved_for_ingest
    
    The :confirmation_of_entity_submitted_for_review is functioning,
    though the email templates are inadequate. This branch does not need
    to worry about fleshing out the templates, but to instead make sure
    that the above 5 notifications can be sent via Sipity.
    
    See the underlying spec for direction.
    
    [skip ci]

--- spec/mailers/sipity/mailers/email_notifier_spec.rb
+++ spec/mailers/sipity/mailers/email_notifier_spec.rb
@@ -19,6 +19,78 @@ module Sipity
           expect(ActionMailer::Base.deliveries.count).to eq(1)
         end
       end
+      context '#request_revisions_from_creator' do
+        let(:entity) { Models::Sip.new }
+        let(:to) { 'test@example.com' }
+        it 'should send an email' do
+          described_class.request_revisions_from_creator(entity: entity, to: to).deliver_now
+
+          expect(ActionMailer::Base.deliveries.count).to eq(1)
+        end
+      end
+      context '#entity_ready_for_review' do
+        let(:entity) { Models::Sip.new }
+        let(:to) { 'test@example.com' }
+        it 'should send an email' do
+          described_class.entity_ready_for_review(entity: entity, to: to).deliver_now
+
+          expect(ActionMailer::Base.deliveries.count).to eq(1)
+        end
+      end
+      context '#entity_ready_for_cataloging' do
+        let(:entity) { Models::Sip.new }
+        let(:to) { 'test@example.com' }
+        it 'should send an email' do
+          described_class.entity_ready_for_cataloging(entity: entity, to: to).deliver_now
+
+          expect(ActionMailer::Base.deliveries.count).to eq(1)
+        end
+      end
+      context '#confirmation_of_entity_approved_for_ingest' do
+        let(:entity) { Models::Sip.new }
+        let(:to) { 'test@example.com' }
+        it 'should send an email' do
+          described_class.confirmation_of_entity_approved_for_ingest(entity: entity, to: to).deliver_now
+
+          expect(ActionMailer::Base.deliveries.count).to eq(1)
+        end
+      end
+      context '#request_revisions_from_creator' do
+        let(:entity) { Models::Sip.new }
+        let(:to) { 'test@example.com' }
+        it 'should send an email' do
+          described_class.request_revisions_from_creator(entity: entity, to: to).deliver_now
+
+          expect(ActionMailer::Base.deliveries.count).to eq(1)
+        end
+      end
+      context '#entity_ready_for_review' do
+        let(:entity) { Models::Sip.new }
+        let(:to) { 'test@example.com' }
+        it 'should send an email' do
+          described_class.entity_ready_for_review(entity: entity, to: to).deliver_now
+
+          expect(ActionMailer::Base.deliveries.count).to eq(1)
+        end
+      end
+      context '#entity_ready_for_cataloging' do
+        let(:entity) { Models::Sip.new }
+        let(:to) { 'test@example.com' }
+        it 'should send an email' do
+          described_class.entity_ready_for_cataloging(entity: entity, to: to).deliver_now
+
+          expect(ActionMailer::Base.deliveries.count).to eq(1)
+        end
+      end
+      context '#confirmation_of_entity_approved_for_ingest' do
+        let(:entity) { Models::Sip.new }
+        let(:to) { 'test@example.com' }
+        it 'should send an email' do
+          described_class.confirmation_of_entity_approved_for_ingest(entity: entity, to: to).deliver_now
+
+          expect(ActionMailer::Base.deliveries.count).to eq(1)
+        end
+      end
     end
   end
 end
