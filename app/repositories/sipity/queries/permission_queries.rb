0d614f72ced0bcb692c68197beed6373936f87ba
Banurekha <banurekha.l@gmail.com>
2015-01-08 16:16:08 -0500

    Combining group queries into permission queries
    This commit merge group queries into permission queries since we decided
    that group queries is irrelavant

--- app/repositories/sipity/queries/permission_queries.rb
+++ app/repositories/sipity/queries/permission_queries.rb
@@ -19,6 +19,20 @@ module Sipity
       end
       public :permission_for
 
+      def roles_for_entity_and_group_name(options = {})
+        Models::Permission.where(options.slice(:acting_as, :entity)).pluck(:acting_as)
+      end
+      module_function :roles_for_entity_and_group_name
+      public :roles_for_entity_and_group_name
+
+      def group_names_for(options = {})
+        roles_array = roles_for_entity_and_group_name(options)
+        return [] if roles_array.empty?
+        roles_array.map { |an_acting_as| Queries::PermissionQueries.permission_for(acting_as: an_acting_as) }
+      end
+      module_function :group_names_for
+      public :group_names_for
+
       # Responsible for returning a User scope:
       # That will include all users
       # That have one or more acting_as
--- spec/repositories/sipity/queries/permission_queries_spec.rb
+++ spec/repositories/sipity/queries/permission_queries_spec.rb
@@ -4,6 +4,22 @@ module Sipity
   module Queries
     # Queries
     RSpec.describe PermissionQueries, type: :repository_methods do
+
+      context '#group_names_for_entity_and_acting_as' do
+        Given(:an_acting_as) { 'etd_reviewer' }
+        Given(:entity) { Models::Sip.new }
+        let(:actor) { Sipity::Factories.create_user(email: 'associated@hotmail.com') }
+
+        it 'returns the group names for the given sip and acting_as' do
+          Models::Permission.create!(actor: actor, acting_as: an_acting_as, entity: entity)
+          expect(test_repository.group_names_for(entity: entity, acting_as: an_acting_as)).to eq(['graduate_school'])
+        end
+
+        it 'will return an empty result if there are no roles' do
+          expect(test_repository.group_names_for(entity: entity, acting_as: [])).
+            to eq([])
+        end
+      end
       context '#emails_for_associated_users' do
         let(:entity) { Models::Sip.create! }
         let(:associated_user) { Sipity::Factories.create_user(email: 'associated@hotmail.com') }
