045e04feab6a4215733ebd9d2d965daa0be3c1a1
Banurekha <banurekha.l@gmail.com>
2015-01-08 13:35:37 -0500

    refactoring spec for group queries

--- spec/repositories/sipity/queries/group_queries_spec.rb
+++ spec/repositories/sipity/queries/group_queries_spec.rb
@@ -5,17 +5,16 @@ module Sipity
     describe GroupQueries, type: :repository_methods do
       context '#group_names_for_entity_and_roles' do
         Given(:role_name) { 'etd_reviewer' }
-        let(:entity) { Models::Sip.create! }
-        subject { test_repository }
+        Given(:entity) { Models::Sip.new }
+        let(:actor) { Sipity::Factories.create_user(email: 'associated@hotmail.com') }
 
-        When(:group_names) {
-          Sipity::Commands::PermissionCommands.grant_groups_permission_to_entity_for_role!(entity: entity, roles: role_name)
-          subject.group_names_for(entity: entity, role: role_name)
-        }
-        Then { group_names == ['graduate_school'] }
+        it 'returns the group names for the given sip and role' do
+          Models::Permission.create!(actor: actor, role: role_name, entity: entity)
+          expect(test_repository.group_names_for(entity: entity, role: role_name)).to eq(['graduate_school'])
+        end
 
         it 'will return an empty result if there are no roles' do
-          expect(subject.group_names_for(entity: entity, role: [])).
+          expect(test_repository.group_names_for(entity: entity, role: [])).
             to eq([])
         end
       end
