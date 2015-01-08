4b7079ec349289bc9037cd97aebc4d47de5b2679
Banurekha <banurekha.l@gmail.com>
2015-01-08 10:58:50 -0500

    adding spec for empty role

--- spec/repositories/sipity/queries/group_queries_spec.rb
+++ spec/repositories/sipity/queries/group_queries_spec.rb
@@ -7,11 +7,17 @@ module Sipity
         Given(:role_name) { 'etd_reviewer' }
         let(:entity) { Models::Sip.create! }
         subject { test_repository }
-          When(:group_names) {
-            Sipity::Commands::PermissionCommands.grant_groups_permission_to_entity_for_role!(entity: entity, roles: role_name)
-            subject.group_names_for(entity: entity, role: role_name)
-          }
-          Then { group_names == ['graduate_school'] }
+
+        When(:group_names) {
+          Sipity::Commands::PermissionCommands.grant_groups_permission_to_entity_for_role!(entity: entity, roles: role_name)
+          subject.group_names_for(entity: entity, role: role_name)
+        }
+        Then { group_names == ['graduate_school'] }
+
+        it 'will return an empty result if there are no roles' do
+            expect(subject.group_names_for(entity: entity, role: [])).
+                to eq([])
+        end
       end
     end
   end
