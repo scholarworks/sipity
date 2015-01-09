94dd871e049f6502ea176b2d524f0acacfdd66f8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-09 09:30:18 -0500

    Adding spec for ActorForPermission

--- spec/models/sipity/models/actor_for_permission_assignment_spec.rb
+++ spec/models/sipity/models/actor_for_permission_assignment_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 module Sipity
   module Models
     RSpec.describe ActorForPermissionAssignment, type: :model do
-      pending "add some examples to (or delete) #{__FILE__}"
+      subject { described_class }
+      its(:column_names) { should include('actor_id') }
+      its(:column_names) { should include('actor_type') }
+      its(:column_names) { should include('acting_as') }
+      its(:column_names) { should include('work_type') }
     end
   end
 end
