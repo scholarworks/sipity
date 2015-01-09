b7b6035d6beb51e52beea2b912ac380c6f6174cf
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-09 12:31:26 -0500

    Reworking low hanging module_function defs
    Instead of the important module_function declaration at the top of the
    module, I want close association with the defined method. This will
    help with pull requests legibility.
    
    See sipity@dacfcb4108b6afd98562249fed2a4abe1e5aaa1a

--- spec/support/sipity/factories.rb
+++ spec/support/sipity/factories.rb
@@ -1,8 +1,6 @@
 module Sipity
   module Factories
-    module_function
-
-    def create_user(overrides = {})
+    module_function def create_user(overrides = {})
       default_attributes = { name: 'Test User', email: 'test@example.com', password: 'please123' }
       User.create!(default_attributes.merge(overrides))
     end
