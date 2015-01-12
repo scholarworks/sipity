59b91c5c6bd4b0a5892cc17b9c0f023931ca6cb7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-12 13:00:38 -0500

    Teasing apart rendering order
    For the new sip, to reflect the changes I'm teasing apart the
    rendering groups. This is in preparation for dropping specific cards
    onto the form.

--- spec/views/sipity/controllers/sips/new.html.erb_spec.rb
+++ spec/views/sipity/controllers/sips/new.html.erb_spec.rb
@@ -7,13 +7,15 @@ RSpec.describe 'sipity/controllers/sips/new.html.erb', type: :view do
     # I want to pass the model to the view; Not using an instance variable
     render template: 'sipity/controllers/sips/new', locals: { model: model }
     expect(rendered).to have_tag('form.new_sip[action="/sips"][method="post"]') do
-      with_tag('fieldset.attributes_sip') do
+      with_tag('fieldset.title_sip') do
         with_tag('input', with: { name: 'sip[title]' })
+      end
+      with_tag('fieldset.work_publication_strategy_sip') do
         model.possible_work_publication_strategies.each do |name, _index|
           with_tag('input', with: { name: 'sip[work_publication_strategy]', value: name })
         end
       end
-      with_tag('fieldset.attributes_sip') do
+      with_tag('fieldset.actions_sip') do
         with_tag('input', with: { type: 'submit' })
       end
     end
