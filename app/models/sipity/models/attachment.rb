a3852dec46c67d0e98f48c47aab4b0d3f9c1d2aa
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-20 09:14:47 -0500

    Adding dragonfly behavior to attachment
    See https://github.com/markevans/dragonfly

--- app/models/sipity/models/attachment.rb
+++ app/models/sipity/models/attachment.rb
@@ -2,8 +2,12 @@ module Sipity
   module Models
     # Represents a single file attached to a given work.
     class Attachment < ActiveRecord::Base
+      extend Dragonfly::Model
       self.table_name = 'sipity_attachments'
+      self.primary_key = :pid
+
       belongs_to :work
+      dragonfly_accessor :file
     end
   end
 end
