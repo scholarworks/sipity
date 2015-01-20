3ec2d4f684ecf37f42f3d20b7f157d61e6d37b4e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-20 14:14:09 -0500

    Adding ability to retrieve an enrichment form
    At present this delivers a singular solution that is not as flexible
    as the method name and usage implies.

--- app/repositories/sipity/queries/description_queries.rb
+++ app/repositories/sipity/queries/description_queries.rb
@@ -5,6 +5,14 @@ module Sipity
       def build_create_describe_work_form(attributes = {})
         Forms::DescribeWorkForm.new(attributes)
       end
+
+      # TODO: Consolidate :build_enrichment_form and
+      #   :build_create_describe_work_form
+      #
+      # TODO: This is the wrong form, but works to solve the specified test.
+      def build_enrichment_form(attributes = {})
+        Forms::AttachFilesToWorkForm.new(attributes)
+      end
     end
   end
 end
---
+++ spec/repositories/sipity/queries/description_queries_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+module Sipity
+  module Queries
+    RSpec.describe DescriptionQueries, type: :repository_methods do
+      context '#build_create_describe_work_form' do
+        let(:work) { double }
+        subject { test_repository.build_create_describe_work_form(work: work) }
+
+        it { should respond_to :work }
+        it { should respond_to :submit }
+        it { should respond_to :valid? }
+        it { should respond_to :abstract }
+      end
+
+      context '#build_enrichment_form' do
+        let(:work) { double }
+        subject { test_repository.build_enrichment_form(work: work) }
+
+        it { should respond_to :work }
+        it { should respond_to :submit }
+        it { should respond_to :valid? }
+        it { should respond_to :files }
+      end
+    end
+  end
+end
