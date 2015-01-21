b182bd3982314e64a36db21deccff373918078a3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-21 15:43:40 -0500

    Adding Enrichment#update action
    This is the generalized behavior. I hope to push most enrichment forms
    through this controller. Ultimately it is a decision of the router to
    handle this behavior.

--- app/controllers/sipity/controllers/work_enrichments_controller.rb
+++ app/controllers/sipity/controllers/work_enrichments_controller.rb
@@ -13,6 +13,20 @@ module Sipity
         end
       end
 
+      def update
+        run(work_id: work_id, enrichment_type: enrichment_type, attributes: update_params) do |on|
+          on.success do |work|
+            redirect_to work_path(work), notice: message_for("#{work.enrichment_type}_enrichment", title: work.title)
+          end
+          on.failure do |model|
+            @model = model
+            respond_with(@model) do |wants|
+              wants.html { render action: @model.enrichment_type }
+            end
+          end
+        end
+      end
+
       attr_reader :model
       protected :model
       helper_method :model
@@ -25,6 +39,10 @@ module Sipity
 
       def enrichment_type
         params.require(:enrichment_type)
+      end
+
+      def update_params
+        params.require(:work)
       end
     end
   end
--- spec/controllers/sipity/controllers/work_enrichments_controller_spec.rb
+++ spec/controllers/sipity/controllers/work_enrichments_controller_spec.rb
@@ -4,21 +4,50 @@ require 'hesburgh/lib/mock_runner'
 module Sipity
   module Controllers
     RSpec.describe WorkEnrichmentsController, type: :controller do
-      let(:work) { double('Work', enrichment_type: 'describe') }
+      let(:work) { double('Work', enrichment_type: 'describe', persisted?: true, title: 'Hello World') }
       context 'GET #edit' do
-        let(:enrichment_type) { 'attach' }
         before { controller.runner = runner }
         let(:runner) do
           Hesburgh::Lib::MockRunner.new(
-            yields: yields, callback_name: callback_name,
-            run_with: { enrichment_type: enrichment_type, work_id: work.to_param }, context: controller
+            yields: yielded_object, callback_name: callback_name, context: controller,
+            run_with: { enrichment_type: work.enrichment_type, work_id: work.to_param }
           )
         end
-        let(:yields) { work }
+        let(:yielded_object) { work }
         let(:callback_name) { :success }
         it 'will render the edit page' do
-          get 'edit', work_id: work.to_param, enrichment_type: enrichment_type
+          get 'edit', work_id: work.to_param, enrichment_type: work.enrichment_type
           expect(assigns(:model)).to_not be_nil
+        end
+      end
+      context 'POST #update' do
+        let(:attributes) { { 'hello' => 'world' } }
+        let(:callback_name) { :success }
+        let(:runner) do
+          Hesburgh::Lib::MockRunner.new(
+            yields: yielded_object, callback_name: callback_name, context: controller,
+            run_with: { enrichment_type: work.enrichment_type, work_id: work.to_param, attributes: attributes }
+          )
+        end
+        before { controller.runner = runner }
+        context 'on success' do
+          let(:callback_name) { :success }
+          let(:yielded_object) { work }
+          it 'will redirect to the work' do
+            post 'update', work_id: work.to_param, enrichment_type: work.enrichment_type, work: attributes
+            expect(flash[:notice]).to_not be_empty
+            expect(assigns(:model)).to be_nil
+            expect(response).to redirect_to work_path(work.to_param)
+          end
+        end
+        context 'on failure' do
+          let(:callback_name) { :failure }
+          let(:yielded_object) { work }
+          it 'will redirect to the work' do
+            post 'update', work_id: work.to_param, enrichment_type: work.enrichment_type, work: attributes
+            expect(assigns(:model)).to be_present
+            expect(response).to render_template(work.enrichment_type)
+          end
         end
       end
     end
