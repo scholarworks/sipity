72d1e5edd779fd31610f36be808f646ea7e6a509
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-22 14:30:49 -0500

    Adding means for creating todo list items
    Given that there is a state related to todo items, I need to have a
    means of querying that state to present the todo list to a user. The
    most obvious method is to track what to do items are associated with
    the work at the given state.
    
    I'm working my way around the todo list/enrichment design, attempting
    to resolve how this information will be tracked. Its a multi-
    dimensional problem: WorkType, State, Items maps to Work, User, State,
    Items.

---
+++ spec/repositories/sipity/commands/todo_list_commands_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+module Sipity
+  module Commands
+    RSpec.describe TodoListCommands, type: :repository_methods do
+      let(:work) { Models::Work.new(id: 1, work_type: 'etd', processing_state: 'new') }
+
+      context '#create_work_todo_list_for_current_state' do
+        it 'will' do
+          expect { test_repository.create_work_todo_list_for_current_state(work: work) }.
+          to change { Models::TodoItemState.count }.by(2)
+        end
+      end
+    end
+  end
+end
