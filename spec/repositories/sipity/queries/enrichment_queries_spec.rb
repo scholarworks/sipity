485134b7fba11e4ec363b913a9463df7062edee3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-26 12:10:53 -0500

    Adding required_todo_items_are_all_done_for_work?
    I want to know when I can advance an object's processing state; This
    question can be answered by asking if all of the required todo items
    are done for the given work.
    
    The following query is what is generated by the AREL code.
    
    ```sql
    SELECT(
      "sipity_work_type_todo_list_configs"."enrichment_type",
      "sipity_todo_item_states"."enrichment_state",
      "sipity_work_type_todo_list_configs"."work_processing_state"
    )
    FROM "sipity_work_type_todo_list_configs"
    LEFT OUTER JOIN "sipity_todo_item_states"
    ON "sipity_todo_item_states"."enrichment_type" = "sipity_work_type_todo_list_configs"."enrichment_type"
      AND "sipity_todo_item_states"."entity_processing_state" = "sipity_work_type_todo_list_configs"."work_processing_state"
    WHERE (
      "sipity_work_type_todo_list_configs"."enrichment_group" = 'required'
      AND "sipity_work_type_todo_list_configs"."work_type" = 'etd'
      AND "sipity_work_type_todo_list_configs"."work_processing_state" = 'new'
    ) AND (
      "sipity_todo_item_states"."entity_id" = 1
      AND "sipity_todo_item_states"."entity_type" = 'Sipity::Models::Work'
      OR "sipity_todo_item_states"."entity_id" IS NULL
    )
    ```

--- spec/repositories/sipity/queries/enrichment_queries_spec.rb
+++ spec/repositories/sipity/queries/enrichment_queries_spec.rb
@@ -31,32 +31,60 @@ module Sipity
         end
       end
 
-      context '#required_todo_items_are_done?' do
-        let(:work) { Models::Work.new(id: 123, processing_state: 'new', work_type: 'etd') }
-        let(:required_enrichment_types) { ['describe', 'attach'] }
-        let(:persist_todo_item_state) do
-          lambda do |work, enrichment_type, enrichment_state|
-            Models::TodoItemState.create!(
-              entity_id: work.id, entity_type: Conversions::ConvertToPolymorphicType.call(work),
-              entity_processing_state: work.processing_state, enrichment_type: enrichment_type, enrichment_state: enrichment_state
+      context '#are_all_of_the_required_todo_items_done_for_work?' do
+        before do
+          [
+            ['etd', 'new', 'describe', 'required'],
+            ['etd', 'new', 'attach', 'required'],
+            ['etd', 'new', 'mogrify', 'optional'],
+            ['etd', 'next', 'attach', 'required'],
+            ['etd', 'next', 'another', 'required']
+          ].each do |work_type, processing_state, enrichment_type, enrichment_group|
+            Sipity::Models::WorkTypeTodoListConfig.create!(
+              work_type: work_type,
+              work_processing_state: processing_state,
+              enrichment_type: enrichment_type,
+              enrichment_group: enrichment_group
             )
           end
         end
-        before do
-          expect(test_repository).to receive(:required_enrichment_types_for_work_type_and_processing_state).
-            and_return(required_enrichment_types)
+        let(:work) { Models::Work.new(id: 123, processing_state: 'new', work_type: 'etd') }
+        let(:persist_todo_item_state) do
+          lambda do |options|
+            work = options.fetch(:work)
+            entity_type = Conversions::ConvertToPolymorphicType.call(work)
+            processing_state = options.fetch(:processing_state) { work.processing_state }
+            enrichment_type = options.fetch(:enrichment_type)
+            enrichment_state = options.fetch(:enrichment_state)
+            Models::TodoItemState.create!(
+              entity_id: work.id, entity_type: entity_type,
+              entity_processing_state: processing_state, enrichment_type: enrichment_type, enrichment_state: enrichment_state
+            )
+          end
         end
 
         it 'will return true if all required todo items are "done"' do
-          required_enrichment_types.each do |enrichment_type|
-            persist_todo_item_state.call(work, enrichment_type, 'done')
-          end
-          expect(test_repository.required_todo_items_are_done_for_work?(work: work)).to eq(true)
+          persist_todo_item_state.call(work: work, enrichment_type: 'describe', enrichment_state: 'done')
+          persist_todo_item_state.call(work: work, enrichment_type: 'attach', enrichment_state: 'done')
+          expect(test_repository.are_all_of_the_required_todo_items_done_for_work?(work: work)).to eq(true)
         end
 
-        it 'will return false if any required todo items are not "done"'
+        it 'will return false if any required todo items are not "done"' do
+          persist_todo_item_state.call(work: work, enrichment_type: 'describe', enrichment_state: 'done')
+          persist_todo_item_state.call(work: work, enrichment_type: 'attach', enrichment_state: 'incomplete')
+          expect(test_repository.are_all_of_the_required_todo_items_done_for_work?(work: work)).to eq(false)
+        end
 
-        it 'will return false if there are missing todo items that are required'
+        it 'will return false if there are missing todo items that are required' do
+          persist_todo_item_state.call(work: work, enrichment_type: 'describe', enrichment_state: 'done')
+          expect(test_repository.are_all_of_the_required_todo_items_done_for_work?(work: work)).to eq(false)
+        end
+
+        it 'will return false if I have additional done todo items that but not all required are done' do
+          persist_todo_item_state.call(work: work, enrichment_type: 'describe', enrichment_state: 'done')
+          persist_todo_item_state.call(work: work, enrichment_type: 'mogrify', enrichment_state: 'done')
+          expect(test_repository.are_all_of_the_required_todo_items_done_for_work?(work: work)).to eq(false)
+        end
       end
     end
   end
