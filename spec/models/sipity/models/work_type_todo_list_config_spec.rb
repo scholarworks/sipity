71ee40f1714a12423e17478b7f2a1074e7631ab7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-26 10:17:16 -0500

    Defining valid enrichment group names
    Instead of relying on magic strings, I'm opting for a well-defined
    enumeration of those values. As of yet, I do not have the concept of
    an EnrichmentGroup to encapsulation presentation information, but this
    is a good next step.

--- spec/models/sipity/models/work_type_todo_list_config_spec.rb
+++ spec/models/sipity/models/work_type_todo_list_config_spec.rb
@@ -9,6 +9,11 @@ module Sipity
         its(:column_names) { should include('enrichment_type') }
         its(:column_names) { should include('enrichment_group') }
       end
+
+      subject { described_class.new }
+      it 'will raise an ArgumentError if you provide an invalid enrichment_group' do
+        expect { subject.enrichment_group = '__invalid_enrichment_group__' }.to raise_error(ArgumentError)
+      end
     end
   end
 end
