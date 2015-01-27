c6aeea1b642dc604cd5133ffd45b24bca24bdf86
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-27 09:35:54 -0500

    Reworking route names
    I was not satisified with the name; This may or may not be the best
    name still.

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
       post 'works/:work_id/:enrichment_type', to: 'work_enrichments#update', constraints: enrichment_constraint
 
       # # Are there constraints?
-      get 'works/:work_id/request/:event_name', to: 'work_event_requests#new', as: 'event_for_work'
+      get 'works/:work_id/request/:event_name', to: 'work_event_requests#new', as: 'event_trigger_for_work'
       post 'works/:work_id/request/:event_name', to: 'work_event_requests#create'
     end
   end
