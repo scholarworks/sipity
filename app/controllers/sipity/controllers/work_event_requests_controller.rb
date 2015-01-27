a76a9fc82c47469c8bcd8b6990b0264a43d98e8c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-27 11:09:18 -0500

    Adding WorkEventRequests#new action

--- app/controllers/sipity/controllers/work_event_requests_controller.rb
+++ app/controllers/sipity/controllers/work_event_requests_controller.rb
@@ -4,6 +4,27 @@ module Sipity
     # of triggering an event.
     class WorkEventRequestsController < ApplicationController
       respond_to :html, :json
+
+      self.runner_container = Runners::WorkEventRequestRunners
+
+      def new
+        _status, @model = run(work_id: work_id, event_name: event_name)
+        respond_with(@model)
+      end
+
+      attr_reader :model
+      protected :model
+      helper_method :model
+
+      private
+
+      def work_id
+        params.require(:work_id)
+      end
+
+      def event_name
+        params.require(:event_name)
+      end
     end
   end
 end
---
+++ spec/controllers/sipity/controllers/work_event_requests_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'hesburgh/lib/mock_runner'
+
+module Sipity
+  module Controllers
+    RSpec.describe WorkEventRequestsController, type: :controller do
+      let(:work) { double('Work', persisted?: true, title: 'Hello World') }
+      let(:event_name) { 'submit_for_review' }
+      context 'GET #new' do
+        before { controller.runner = runner }
+        let(:runner) do
+          Hesburgh::Lib::MockRunner.new(
+            yields: yielded_object, callback_name: callback_name, context: controller,
+            run_with: { event_name: event_name, work_id: work.to_param }
+          )
+        end
+        let(:yielded_object) { work }
+        let(:callback_name) { :success }
+        it 'will render the new page' do
+          get 'new', work_id: work.to_param, event_name: event_name
+          expect(assigns(:model)).to_not be_nil
+        end
+      end
+    end
+  end
+end
