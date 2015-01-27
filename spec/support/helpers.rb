52ede7c2d0354eaf436151bc7670a604a05277f2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-27 11:12:49 -0500

    Adding handler to the text context
    I always need a handler, so I'm prefering to bake that into the
    context.

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -3,11 +3,12 @@ require 'support/helpers/session_helpers'
 module RunnersSupport
   class TestRunnerContext
     include RSpec::Mocks
-    attr_reader :repository, :current_user
+    attr_reader :repository, :current_user, :handler
     def initialize(methods = {})
       @current_user = methods.delete(:current_user) { :current_user_is_nil }
       methods[:policy_authorized_for?] = true unless methods.key?(:policy_authorized_for?)
       @repository = ExampleMethods.declare_double(Double, 'Repository', methods)
+      @handler = ExampleMethods.declare_double(Double, 'Handler', invoked: true)
     end
 
     def authenticate_user!
