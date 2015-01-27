db129ffee84b0264de3551c818a09241e956cb53
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-27 10:43:07 -0500

    Adding basic runner functionality
    This is incomplete, but highlights mapping the requested action into
    the repository.

---
+++ app/runners/sipity/runners/work_event_request_runners.rb
@@ -0,0 +1,19 @@
+module Sipity
+  module Runners
+    module WorkEventRequestRunners
+      # Responsible for responding with the correct form for the work's description
+      class New < BaseRunner
+        self.authentication_layer = :default
+        self.authorization_layer = :default
+
+        def run(work_id:, event_name:)
+          work = repository.find_work(work_id)
+          form = repository.build_event_trigger_form(work: work, event_name: event_name)
+          authorization_layer.enforce!(submit?: form) do
+            callback(:success, form)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/runners/sipity/runners/work_event_request_runners_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'sipity/runners/work_event_request_runners'
+
+module Sipity
+  module Runners
+    module WorkEventRequestRunners
+      include RunnersSupport
+      RSpec.describe New do
+        let(:work) { double('Work', id: '1234') }
+        let(:form) { double('Form') }
+        let(:event_name) { 'fandango' }
+        let(:context) { TestRunnerContext.new(find_work: work, build_event_trigger_form: form) }
+        subject do
+          described_class.new(context, authentication_layer: false, authorization_layer: false) do |on|
+            on.success { |a| context.handler.invoked("SUCCESS", a) }
+          end
+        end
+
+        it 'will require authentication by default' do
+          expect(described_class.authentication_layer).to eq(:default)
+        end
+
+        it 'enforces authorization' do
+          expect(described_class.authorization_layer).to eq(:default)
+        end
+
+        it 'issues the :success callback' do
+          response = subject.run(work_id: work.id, event_name: event_name)
+          expect(context.handler).to have_received(:invoked).with("SUCCESS", form)
+          expect(response).to eq([:success, form])
+        end
+      end
+    end
+  end
+end
