2a0f7bd6cb963d2922399a11c1624f474261664d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-29 11:39:05 -0500

    Remove dependence on the Queries bucket
    I have a proper class now!

--- app/repositories/sipity/query_repository.rb
+++ app/repositories/sipity/query_repository.rb
@@ -3,6 +3,15 @@ module Sipity
   # Defines and exposes the query methods for interacting with the public API of
   # the persistence layer.
   class QueryRepository
-    include Queries
+    include Queries::WorkQueries
+    include Queries::CitationQueries
+    include Queries::DoiQueries
+    include Queries::EventLogQueries
+    include Queries::AccountPlaceholderQueries
+    include Queries::AdditionalAttributeQueries
+    include Queries::CollaboratorQueries
+    include Queries::PermissionQueries
+    include Queries::EnrichmentQueries
+    include Queries::EventTriggerQueries
   end
 end
