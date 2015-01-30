03e0f90dee75e4728af7b6da0227ae6852683339
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-30 13:36:56 -0500

    Adding the resourceful action
    This action represents an action that is part of the Rails default
    list of Resource based actions (i.e. show, new, edit, create, update,
    destroy)

---
+++ app/decorators/sipity/decorators/actions/resourceful_action.rb
@@ -0,0 +1,68 @@
+module Sipity
+  module Decorators
+    module Actions
+      # This action represents an action that is part of the Rails default
+      # list of Resource based actions (i.e. show, new, edit, create, update,
+      # destroy)
+      class ResourcefulAction
+        ANALOGOUS_RESOURCEFUL_ACTION_NAMES = {
+          'show' => ['show'],
+          'update' => ['edit', 'update'],
+          'edit' => ['edit', 'update'],
+          'create' => ['new', 'create'],
+          'new' => ['new', 'create'],
+          'destroy' => ['destroy']
+        }.freeze
+
+        RESOURCEFUL_ACTION_NAMES = ANALOGOUS_RESOURCEFUL_ACTION_NAMES.freeze
+        def initialize(options = {})
+          self.name = options.fetch(:name)
+          @entity = options.fetch(:entity)
+          @view_context = options.fetch(:view_context) { default_view_context }
+        end
+        attr_reader :name, :entity
+
+        # This action, if it is rendered, is always available.
+        def availability_state
+          'available'
+        end
+
+        def available?
+          true
+        end
+
+        def path
+          case name.to_s
+          when 'show', 'destroy'
+            view_context.work_path(entity)
+          when 'edit', 'update'
+            view_context.edit_work_path(entity)
+          when 'new', 'create'
+            view_context.new_work_path
+          else
+            # A catch in case the above magic strings and the above constant
+            # get out of sink.
+            raise Exceptions::UnprocessableResourcefulActionNameError, name
+          end
+        end
+
+        private
+
+        def name=(value)
+          if RESOURCEFUL_ACTION_NAMES.include?(value)
+            @name = value
+          else
+            raise Exceptions::UnprocessableResourcefulActionNameError, value
+          end
+        end
+
+        attr_reader :view_context
+        private :view_context
+
+        def default_view_context
+          Draper::ViewContext.current
+        end
+      end
+    end
+  end
+end
