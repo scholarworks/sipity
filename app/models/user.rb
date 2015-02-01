4f897f5ac86316ff90254731f2ba99cc33129533
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-31 23:57:28 -0500

    Removing default user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,5 @@
 # Every application needs users. Right? This is that class.
 class User < ActiveRecord::Base
-  enum role: [:user, :vip, :admin]
-  after_initialize :set_default_role, if:  :new_record?
-
-  def set_default_role
-    self.role ||= :user
-  end
-
   devise :cas_authenticatable, :trackable
 
   has_many :permissions, as: :actor, dependent: :destroy, class_name: 'Sipity::Models::Permission'
