f0949d01f6ea1ffdefe6a4ae4f532ee330f944ec
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-02-02 12:11:01 -0500

    Switching to constantize
    Rails autoload/reload was causing issues with the const_defined?
    saying that the object could not be found. So I'm switching to the
    constantize method.

--- app/forms/sipity/forms/work_enrichments.rb
+++ app/forms/sipity/forms/work_enrichments.rb
@@ -12,11 +12,9 @@ module Sipity
 
       def find_enrichment_form_builder(enrichment_type:)
         form_name_by_convention = "#{enrichment_type.classify}Form"
-        if const_defined?(form_name_by_convention)
-          const_get(form_name_by_convention)
-        else
-          fail Exceptions::EnrichmentNotFoundError, name: form_name_by_convention, container: self
-        end
+        "#{self}::#{form_name_by_convention}".constantize
+      rescue NameError
+        raise Exceptions::EnrichmentNotFoundError, name: form_name_by_convention, container: self
       end
     end
   end
