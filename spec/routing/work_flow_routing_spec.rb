584d9bfa0c3115ccab887bdfb1393d929565748a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-02-02 09:49:25 -0500

    Adding routing to handle advisors

--- spec/routing/work_flow_routing_spec.rb
+++ spec/routing/work_flow_routing_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'work flow routing spec' do
   let(:work_id) { "12" }
   context 'enrichment routes' do
-    ['attach', 'describe'].each do |enrichment_type|
+    ['advisors', 'attach', 'describe'].each do |enrichment_type|
       it "will route GET /works/:work_id/#{enrichment_type}" do
         expect(get: "/works/#{work_id}/#{enrichment_type}").
           to route_to(controller: 'sipity/controllers/work_enrichments', action: 'edit', work_id: work_id, enrichment_type: enrichment_type)
