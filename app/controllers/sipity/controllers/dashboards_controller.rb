dea2a7fd10bc57a2f77a0dfd99893a68d6046e3b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-02-04 11:08:06 -0500

    Wiring in processing state filter

--- app/controllers/sipity/controllers/dashboards_controller.rb
+++ app/controllers/sipity/controllers/dashboards_controller.rb
@@ -6,11 +6,18 @@ module Sipity
       respond_to :html, :json
 
       def index
-        @view = repository.find_works_for(user: current_user)
+        # TODO: Extract the runner
+        @view = repository.find_works_for(user: current_user, processing_state: processing_state)
         respond_with(@view)
       end
       attr_reader :view
       helper_method :view
+
+      private
+
+      def processing_state
+        params[:processing_state]
+      end
     end
   end
 end
