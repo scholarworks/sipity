bfa6577f81eaf79609d3210e9f210cccb8a09a1d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-02-05 13:01:30 -0500

    Adding entity column spec
    Because I was wanting to see what the database columns were, I figured
    I may as well add tests to verify.

--- spec/models/sipity/models/processing/entity_spec.rb
+++ spec/models/sipity/models/processing/entity_spec.rb
@@ -4,7 +4,11 @@ module Sipity
   module Models
     module Processing
       RSpec.describe Entity, type: :model do
-        pending "add some examples to (or delete) #{__FILE__}"
+        subject { described_class }
+        its(:column_names) { should include("proxy_for_id") }
+        its(:column_names) { should include("proxy_for_type") }
+        its(:column_names) { should include("strategy_id") }
+        its(:column_names) { should include("strategy_state_id") }
       end
     end
   end
