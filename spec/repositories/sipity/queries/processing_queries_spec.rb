e000f6a1a3ac0fa1d71c5366d728ac50c0d4c99e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-02-10 13:35:45 -0800

    Adding query for proxies I can act on
    > @api public
    >
    > An ActiveRecord::Relation scope that meets the following criteria:
    >
    > * Processing Entities of the given proxy_for_type
    > * Processing Entities in a state in which I have access to based on:
    >   - The entity specific responsibility
    >     - For which I've been assigned either as a group member or a
    >       user
    >   - The strategy specific responsibility
    >     - For which I've been assigned either as a group member or a
    >       user
    >
    > In other words, for the given user and the given proxy for type (
    > i.e. Models::Work), fetch all of the processing entities that I can,
    > in some way, access based on the processing state.
    >
    > @param user [User]
    > @param proxy_for_type something that can be converted to a
    > polymorphic type.
    >
    > @return ActiveRecord::Relation<Models::Processing::Entity>

--- spec/repositories/sipity/queries/processing_queries_spec.rb
+++ spec/repositories/sipity/queries/processing_queries_spec.rb
@@ -110,6 +110,43 @@ module Sipity
         end
       end
 
+      context '#scope_processing_entities_for_the_user_and_proxy_for_type' do
+        before do
+          Sipity::SpecSupport.load_database_seeds!(seeds_path: 'spec/fixtures/seeds/trigger_work_state_change.rb')
+        end
+        let(:strategy) { Models::Processing::Strategy.first! }
+        let(:originating_state) { Models::Processing::StrategyState.first! }
+        let(:user) { User.create!(username: 'user') }
+        subject do
+          test_repository.scope_processing_entities_for_the_user_and_proxy_for_type(user: user, proxy_for_type: Sipity::Models::Work)
+        end
+
+        it "will resolve to an array of entities" do
+          entity = Models::Processing::Entity.find_or_create_by!(
+            proxy_for_id: 1, proxy_for_type: 'Sipity::Models::Work', strategy: strategy, strategy_state: originating_state
+          )
+          other_entity = Models::Processing::Entity.create!(
+            proxy_for_id: 2, proxy_for_type: 'Sipity::Models::Work', strategy: strategy, strategy_state: originating_state
+          )
+
+          # NOTE: The strategy is "correct", but the strategy_state is wrong
+          Models::Processing::Entity.create!(
+            proxy_for_id: 3, proxy_for_type: 'Sipity::Models::Work', strategy: strategy, strategy_state_id: 1234
+          )
+          user_actor = Models::Processing::Actor.find_or_create_by!(proxy_for: user)
+          Models::Processing::EntitySpecificResponsibility.find_or_create_by!(
+            strategy_role: strategy_role, actor: user_actor, entity: entity
+          )
+          Models::Processing::StrategyResponsibility.find_or_create_by!(strategy_role: strategy_role, actor: user_actor)
+
+          expect(subject).to eq([entity, other_entity])
+        end
+        xit 'bombard this with more scenarios'
+        it "will be a chainable scope" do
+          expect(subject).to be_a(ActiveRecord::Relation)
+        end
+      end
+
       context '#scope_proxied_objects_from_processing_entities' do
         let(:user) { User.create!(username: 'user') }
         subject do
