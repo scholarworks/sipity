6d6089a00c688a11da0bcd94c8b8fb829f5f19fc
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-02-11 10:26:39 -0800

    Teasing apart actions

--- spec/decorators/sipity/decorators/processing_actions_spec.rb
+++ spec/decorators/sipity/decorators/processing_actions_spec.rb
@@ -3,20 +3,19 @@ require 'spec_helper'
 module Sipity
   module Decorators
     RSpec.describe ProcessingActions do
-      before do
-        Sipity::SpecSupport.load_database_seeds!(seeds_path: 'spec/fixtures/seeds/trigger_work_state_change.rb')
+      let(:user) { double('User') }
+      let(:repository) { double('Repository', scope_permitted_strategy_actions_available_for_current_state: actions) }
+      let(:entity) { double('Entity') }
+
+      let(:actions) do
+        [
+          Models::Processing::StrategyAction.new(action_type: 'enrichment_action'),
+          Models::Processing::StrategyAction.new(action_type: 'resourceful_action'),
+          Models::Processing::StrategyAction.new(action_type: 'state_advancing_action')
+        ]
       end
 
-      let(:commands) { CommandRepository.new }
-      let(:user) { User.create!(username: 'user') }
-
-      let!(:entity) do
-        work = commands.create_work!(title: 'Book', work_type: 'etd', work_publication_strategy: 'will_not_publish')
-        commands.grant_creating_user_permission_for!(entity: work, user: user)
-        work
-      end
-
-      subject { described_class.new(user: user, entity: entity) }
+      subject { described_class.new(user: user, entity: entity, repository: repository) }
 
       its(:enrichment_actions) { should be_a Enumerable }
       its(:resourceful_actions) { should be_a Enumerable }
