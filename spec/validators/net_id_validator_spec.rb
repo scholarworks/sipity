9e9394ab1837ae4513a82f3588fdca9f865017d1
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-02-12 08:32:46 -0800

    Injecting both a failing and passing test
    Making sure to not hit the remote LDAP service that will be added
    later.

--- spec/validators/net_id_validator_spec.rb
+++ spec/validators/net_id_validator_spec.rb
@@ -11,26 +11,31 @@ describe NetIdValidator do
       validates :a_netid, net_id: true
     end
   end
-  subject { validatable.new }
+
+  subject { NetIdValidator.new(validator_parameters) }
+  let(:validator_parameters) { { attributes: { a_netid: "a_netid" } } }
 
   context 'with a netid' do
     it { expect(:valid?).to be_truthy }
   end
 
-  context '#default_netid_remote_validator' do
+  its(:default_netid_remote_validator) { should respond_to :call }
+
+  context '#validate_each' do
     let(:a_netid) { 'a_netid'  }
 
     it 'will add an error to the given attribute if the remote validator returns false' do
       record = validatable.new
       remote = double(call: false)
-      validator = described_class.new(netid_remote_validator: remote, attributes: { a_netid: "a_netid" })
+      validator = described_class.new(validator_parameters.merge(netid_remote_validator: remote))
       validator.validate_each(record, :a_netid, 'hello')
       expect(record.errors.messages).not_to be_empty
     end
 
     it 'will not add an error to the given attribute if the validator returns true' do
       record = validatable.new
-      validator = described_class.new(attributes: { a_netid: "a_netid" })
+      remote = double(call: true)
+      validator = described_class.new(validator_parameters.merge(netid_remote_validator: remote))
       validator.validate_each(record, :a_netid, 'hello')
       expect(record.errors.messages).to be_empty
     end
