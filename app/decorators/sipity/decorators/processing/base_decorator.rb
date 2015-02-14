e8317b0a7c3ef8ccea078010bae503d268472490
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-02-14 09:01:37 -0800

    Removing configuration of view context

--- app/decorators/sipity/decorators/processing/base_decorator.rb
+++ app/decorators/sipity/decorators/processing/base_decorator.rb
@@ -8,7 +8,6 @@ module Sipity
         def initialize(options = {})
           @action = options.fetch(:action)
           @entity = options.fetch(:entity)
-          @view_context = options.fetch(:view_context) { default_view_context }
         end
 
         attr_reader :action, :entity
@@ -28,12 +27,9 @@ module Sipity
           view_context.enrich_work_path(entity, name)
         end
 
-        attr_reader :view_context
-        private :view_context
-
         private
 
-        def default_view_context
+        def view_context
           Draper::ViewContext.current
         end
       end
