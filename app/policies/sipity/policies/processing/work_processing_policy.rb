c94ef9f906707ebdd8ec231b4b7e49e05d59c484
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-02-18 12:24:17 -0500

    Preserving external interface for WorkPolicy
    I want to preserve the external interface of the work policy so that
    I can replace its behavior going forward.

--- app/policies/sipity/policies/processing/work_processing_policy.rb
+++ app/policies/sipity/policies/processing/work_processing_policy.rb
@@ -8,6 +8,9 @@ module Sipity
       #
       # @see [Pundit gem](http://rubygems.org/gems/pundit) for more on object
       #   oriented authorizaiton.
+      #
+      # @note This class implements method_missing in an attempt to splice in
+      #   this policy into Sipity's interactions.
       class WorkProcessingPolicy
         def initialize(user, work, repository: default_repository)
           @user = user
@@ -25,6 +28,10 @@ module Sipity
         private :repository
 
         private
+
+        def method_missing(method_name, *)
+          authorize?(method_name)
+        end
 
         def default_repository
           QueryRepository.new
--- spec/policies/sipity/policies/processing/work_processing_policy_spec.rb
+++ spec/policies/sipity/policies/processing/work_processing_policy_spec.rb
@@ -8,6 +8,12 @@ module Sipity
         let(:work) { Models::Work.new(id: '2') }
         let(:repository) { double('Repository') }
         subject { described_class.new(user, work, repository: repository) }
+
+        it 'will capture method missing and attempt an authorize?' do
+          expect(repository).to receive(:authorized_for_processing?).with(user: user, entity: work, action: :show?).and_return(true)
+          expect(subject.show?).to be_truthy
+        end
+
         it 'will query the underlying processing system for answers by :action_name' do
           expect(repository).to receive(:authorized_for_processing?).with(user: user, entity: work, action: :show).and_return(true)
           expect(subject.authorize?(:show)).to be_truthy
