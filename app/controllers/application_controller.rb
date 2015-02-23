66e4a4dae0dbc8817f660cde20a0bd8c92a109ee
Rajesh Balekai <rbalekai@nd.edu>
2015-02-23 10:47:05 -0500

    Remove bug in devise flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include Hesburgh::Lib::ControllerWithRunner
+  before_filter :filter_notify
 
   # So you can easily invoke the public repository of Sipity.
   # It is the repository that indicates what the application can and is doing.
@@ -23,4 +24,14 @@ class ApplicationController < ActionController::Base
     t(key, { scope: "sipity/#{controller_name}.action/#{action_name}" }.merge(options))
   end
   private :message_for
+
+  # Remove error inserted since we are not showing a page before going to web access, this error message always shows up a page too late.
+  # for the moment just remove it always.  If we show a transition page in the future we may want to  display it then.
+  def filter_notify
+    return unless flash[:alert].present?
+    flash[:alert] = [flash[:alert]].flatten.reject do |item|
+      item == "You need to sign in or sign up before continuing."
+    end
+    flash[:alert] = nil if flash[:alert].blank?
+  end
 end
