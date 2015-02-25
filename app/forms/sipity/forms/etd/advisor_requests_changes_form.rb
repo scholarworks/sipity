6b7ad367726feddf343d2cc6218ca1aa4d02187b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-02-25 11:43:11 -0500

    Initial commit regarding AdvisorRequestsChanges
    This highlights the behavior, however it calls attention to a few
    interface changes that will need to occur.

---
+++ app/forms/sipity/forms/etd/advisor_requests_changes_form.rb
@@ -0,0 +1,46 @@
+module Sipity
+  module Forms
+    module Etd
+      # Responsible for capturing advisor comments and forwarding them on to
+      # the student.
+      class AdvisorRequestsChangesForm < ProcessingActionForm
+        def initialize(attributes = {})
+          super
+          @comment = attributes[:comment]
+          self.action = attributes.fetch(:processing_action_name) { default_processing_action_name }
+        end
+
+        attr_reader :action, :comment
+        validates :comment, presence: true
+
+        def processing_action_name
+          action.name
+        end
+
+        private
+
+        def save(repository:, requested_by:)
+          super do
+            repository.send_notification_for_entity_trigger(
+              notification: 'advisor_has_requested_changes',
+              entity: work,
+              acting_as: ['creating_user']
+            )
+            repository.update_processing_state!(entity: work, to: action.resulting_strategy_state)
+            repository.record_processing_comment(entity: work, commenter: requested_by, comment: comment)
+          end
+        end
+
+        def enrichment_type
+          self.class.to_s.demodulize.underscore.sub(/_form\Z/i, '')
+        end
+        alias_method :default_processing_action_name, :enrichment_type
+
+        include Conversions::ConvertToProcessingAction
+        def action=(value)
+          @action = convert_to_processing_action(value, scope: to_processing_entity)
+        end
+      end
+    end
+  end
+end
--- app/repositories/sipity/commands/todo_list_commands.rb
+++ app/repositories/sipity/commands/todo_list_commands.rb
@@ -2,9 +2,14 @@ module Sipity
   # :nodoc:
   module Commands
     # Responsible for interaction with the todo list.
+    # TODO: Rename to ProcessingCommands
     module TodoListCommands
       def register_action_taken_on_entity(work:, enrichment_type:, requested_by:)
         Services::RegisterActionTakenOnEntity.call(entity: work, action: enrichment_type, requested_by: requested_by)
+      end
+
+      # @api public
+      def record_processing_comment(*)
       end
     end
   end
