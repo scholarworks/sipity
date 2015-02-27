031cabed012dc5d70043fbd77f98de742d3758b7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-02-27 13:17:38 -0500

    Exposing method for deleting attachments
    The present form implies that we can update the metadata (i.e. name
    of the attached file). However, deletion is more important.

--- app/forms/sipity/forms/work_enrichments/attach_form.rb
+++ app/forms/sipity/forms/work_enrichments/attach_form.rb
@@ -22,7 +22,9 @@ module Sipity
         end
 
         # Exposed so that field_for will work
-        def attachments_attributes=(_value)
+        def attachments_attributes=(value)
+          @attachments_attributes = value
+          collect_files_for_deletion(value)
         end
 
         private
@@ -33,6 +35,19 @@ module Sipity
               repository.attach_file_to(work: work, file: file, user: requested_by)
             end
             repository.mark_as_representative(work: work, pid: mark_as_representative, user: requested_by)
+            repository.remove_files_from(work: work, user: requested_by, pids: ids_for_deletion)
+          end
+        end
+
+        def ids_for_deletion
+          @ids_for_deletion || []
+        end
+
+        def collect_files_for_deletion(value)
+          @ids_for_deletion = []
+          value.each do |_key, attributes|
+            next unless attributes['delete'] == '1' # Magic string; Craft a Boolean conversion
+            @ids_for_deletion << attributes.fetch('id')
           end
         end
 
