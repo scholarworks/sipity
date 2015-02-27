68f1a61ea01411b45c57d786ac031b9dcbfc0ba6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-02-27 12:35:40 -0500

    Moving class declaration for clarity
    The incorrect class was at the beginning of the file. This made the
    code more confusing.

--- app/forms/sipity/forms/work_enrichments/attach_form.rb
+++ app/forms/sipity/forms/work_enrichments/attach_form.rb
@@ -1,30 +1,6 @@
 module Sipity
   module Forms
     module WorkEnrichments
-      # Responsible for exposing a means of displaying and marking the object
-      # for deletion.
-      class AttachmentFormElement
-        THUMBNAIL_SIZE = '64x64#'
-        def initialize(object)
-          @object = object
-        end
-
-        def thumbnail_url(size = THUMBNAIL_SIZE)
-          thumbnail(size).url
-        end
-
-        delegate :id, :name, :persisted?, to: :object
-        attr_accessor :delete
-
-        private
-
-        attr_reader :object
-        private :object
-
-        def thumbnail(size = THUMBNAIL_SIZE)
-          object.file.thumb(size, format: 'png', frame: 0)
-        end
-      end
       # Exposes a means for attaching files to the associated work.
       class AttachForm < Forms::WorkEnrichmentForm
         def initialize(attributes = {})
@@ -65,6 +41,31 @@ module Sipity
           # I don't want this to be draped because the collection appeared to be
           # treated as a single model instead of as an enumeration of items.
           work.attachments.map { |attachment| AttachmentFormElement.new(attachment) }
+        end
+      end
+
+      # Responsible for exposing a means of displaying and marking the object
+      # for deletion.
+      class AttachmentFormElement
+        THUMBNAIL_SIZE = '64x64#'
+        def initialize(object)
+          @object = object
+        end
+
+        def thumbnail_url(size = THUMBNAIL_SIZE)
+          thumbnail(size).url
+        end
+
+        delegate :id, :name, :persisted?, to: :object
+        attr_accessor :delete
+
+        private
+
+        attr_reader :object
+        private :object
+
+        def thumbnail(size = THUMBNAIL_SIZE)
+          object.file.thumb(size, format: 'png', frame: 0)
         end
       end
     end
