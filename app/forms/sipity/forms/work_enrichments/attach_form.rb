8dc0e4d06d24aabe4dececdaf53d0531e4dfdd8a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-02-27 10:03:27 -0500

    Initial unverified stab at attachment deletion
    This is non-functioning code, but it does expose the correct
    attributes for enabling deletion and rendering of existing
    attachments.

--- app/forms/sipity/forms/work_enrichments/attach_form.rb
+++ app/forms/sipity/forms/work_enrichments/attach_form.rb
@@ -1,17 +1,39 @@
 module Sipity
   module Forms
     module WorkEnrichments
+      # Responsible for exposing a means of displaying and marking the object
+      # for deletion.
+      class AttachmentFormElement
+        THUMBNAIL_SIZE = '64x64#'
+        def initialize(object)
+          @object = object
+        end
+
+        def thumbnail_url(size = THUMBNAIL_SIZE)
+          thumbnail(size).url
+        end
+
+        delegate :id, :name, :persisted?, to: :object
+        attr_accessor :delete
+
+        private
+
+        attr_reader :object
+        private :object
+
+        def thumbnail(size = THUMBNAIL_SIZE)
+          object.file.thumb(size, format: 'png', frame: 0)
+        end
+      end
       # Exposes a means for attaching files to the associated work.
       class AttachForm < Forms::WorkEnrichmentForm
         def initialize(attributes = {})
           super
           @files = attributes[:files]
           @mark_as_representative = attributes[:mark_as_representative]
+          self.attachments_attributes = attributes.fetch(:attachments_attributes, {})
         end
 
-        # TODO: Write a custom file validator. There must be at least one file
-        #   uploaded.
-        validates :files, presence: true
         attr_accessor :files
         attr_accessor :mark_as_representative
 
@@ -20,7 +42,11 @@ module Sipity
         end
 
         def attachments
-          @attachments || attachments_from_work
+          @attachments ||= attachments_from_work
+        end
+
+        # Exposed so that field_for will work
+        def attachments_attributes=(_value)
         end
 
         private
@@ -36,7 +62,9 @@ module Sipity
 
         def attachments_from_work
           return [] unless work
-          work.attachments
+          # I don't want this to be draped because the collection appeared to be
+          # treated as a single model instead of as an enumeration of items.
+          work.attachments.map { |attachment| AttachmentFormElement.new(attachment) }
         end
       end
     end
