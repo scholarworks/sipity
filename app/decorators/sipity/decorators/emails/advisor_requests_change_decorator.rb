cf2fecf49f8bccfe4a8d71df46d851365c02c89b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-03-02 13:19:57 -0500

    Fleshing out AdvisorRequestsChangesDecorator
    Wiring in the required retrieval information

--- app/decorators/sipity/decorators/emails/advisor_requests_change_decorator.rb
+++ app/decorators/sipity/decorators/emails/advisor_requests_change_decorator.rb
@@ -1,18 +1,22 @@
 module Sipity
   module Decorators
     module Emails
+      # Responsible for exposing methods required for email delivery. Nothing
+      # too fancy.
       class AdvisorRequestsChangeDecorator
         def initialize(processing_comment)
           @processing_comment = processing_comment
         end
 
         def name_of_commentor
+          actor.proxy_for.name
         end
 
         def document_type
+          entity.proxy_for.work_type
         end
 
-        delegate :comment, to: :@processing_comment
+        delegate :comment, :actor, :entity, to: :@processing_comment
       end
     end
   end
--- spec/decorators/sipity/decorators/emails/advisor_requests_change_decorator_spec.rb
+++ spec/decorators/sipity/decorators/emails/advisor_requests_change_decorator_spec.rb
@@ -4,12 +4,16 @@ module Sipity
   module Decorators
     module Emails
       RSpec.describe AdvisorRequestsChangeDecorator do
-        let(:processing_comment) { Models::Processing::Comment.new(comment: 'hello') }
+        let(:user) { User.new(name: 'Hello World') }
+        let(:work) { Models::Work.new(work_type: 'doctoral_dissertation') }
+        let(:actor) { Models::Processing::Actor.new(proxy_for: user) }
+        let(:entity) { Models::Processing::Entity.new(proxy_for: work) }
+        let(:processing_comment) { Models::Processing::Comment.new(comment: 'hello', actor: actor, entity: entity) }
         subject { described_class.new(processing_comment) }
 
         its(:comment) { should eq processing_comment.comment }
-        it { should respond_to :name_of_commentor }
-        it { should respond_to :document_type }
+        its(:name_of_commentor) { should eq(user.name) }
+        its(:document_type) { should eq(work.work_type) }
       end
     end
   end
