0a39029fb366619482be68cfd689a1fab81de47b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-03-02 16:11:07 -0500

    Adding ExtractInputDateFromInput
    Because I am doing this logic in multiple places, I want to make a
    consistent method to handle these extractions.

---
+++ spec/conversions/sipity/conversions/extract_input_date_from_input_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+module Sipity
+  module Conversions
+    RSpec.describe ExtractInputDateFromInput do
+      include ExtractInputDateFromInput
+
+      it 'will extract based on direct input' do
+        attributes = { 'defense_date' => 'Hello World' }
+        expect(extract_input_date_from_input(:defense_date, attributes)).to eq('Hello World')
+      end
+
+      it 'will extract based on Rails convention' do
+        attributes = { 'defense_date(1i)' => '2014', 'defense_date(2i)' => '11', 'defense_date(3i)' => '28' }
+        expect(extract_input_date_from_input(:defense_date, attributes)).to eq('2014-11-28')
+      end
+
+      it 'will yield if neither is met' do
+        attributes = {}
+        expect(extract_input_date_from_input(:defense_date, attributes) { 'yielded' }).to eq('yielded')
+      end
+    end
+  end
+end
