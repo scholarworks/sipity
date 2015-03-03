7b9c51f1f1e938f4d8ab50af50513d7f06a4bec8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-03-03 09:21:03 -0500

    Exposing means of updating accessible objects
    This is incomplete, in that we are capturing the form information and
    not yet mapping it to the underlying layer. It also does not assume
    an unhappy path.

--- app/forms/sipity/forms/work_enrichments/access_policy_form.rb
+++ app/forms/sipity/forms/work_enrichments/access_policy_form.rb
@@ -13,11 +13,44 @@ module Sipity
       #   - Is there a default we want to "provide" for Embargos (1 year from
       #     now)
       class AccessPolicyForm < Forms::WorkEnrichmentForm
+
+        def initialize(attributes = {})
+          super
+          self.accessible_objects_attributes = attributes.fetch(:accessible_objects_attributes) { {} }
+        end
+
         # Because I am using `#fields_for` for rendering
-        attr_writer :accessible_objects_attributes
+        def accessible_objects_attributes=(values)
+          @accessible_objects_attributes = parse_accessible_objects_attributes(values)
+        end
 
         def accessible_objects
-          @accessible_objects ||= repository.accessible_objects(work: work).map { |obj| AccessibleObjectFromPersistence.new(obj) }
+          @accessible_objects ||= accessible_objects_from_repository
+        end
+
+        private
+
+        def save(requested_by:)
+          super do
+            repository.apply_access_policies_to(work: work, access_policies: access_objects_attributes_for_persistence)
+          end
+        end
+
+        def access_objects_attributes_for_persistence
+          @accessible_objects_attributes.map { |obj| obj.to_hash }
+        end
+
+        def accessible_objects_from_repository
+          repository.accessible_objects(work: work).map { |obj| AccessibleObjectFromPersistence.new(obj) }
+        end
+
+        def parse_accessible_objects_attributes(values = {})
+          from_persistence = accessible_objects_from_repository
+          values.map do |(_key, attrs)|
+            attributes = attrs.with_indifferent_access
+            persisted_object = from_persistence.detect { |obj| obj.id.to_s == attributes.fetch('id')  }
+            AccessibleObjectFromInput.new(persisted_object, attributes)
+          end
         end
 
         module AccessibleObjectCodes
@@ -53,7 +86,7 @@ module Sipity
           end
           attr_reader :access_right_code, :release_date, :persisted_object
 
-          delegate :persisted?, :id, :to_s, to: :persisted_object
+          delegate :persisted?, :entity_type, :to_param, :id, :to_s, to: :persisted_object
 
           def persisted?
             true
@@ -61,6 +94,15 @@ module Sipity
 
           validates :access_right_code, presence: true, inclusion: { in: :valid_access_right_codes, allow_nil: true }
           validates :release_date, presence: { if: :will_be_under_embargo? }
+
+          def to_hash
+            {
+              entity_id: id.to_s,
+              entity_type: entity_type,
+              access_right_code: access_right_code,
+              release_date: release_date
+            }
+          end
 
           private
 
@@ -84,7 +126,12 @@ module Sipity
             @object = object
           end
           attr_reader :access_right_code, :release_date
-          delegate :persisted?, :id, :to_s, to: :@object
+          delegate :persisted?, :id, :to_s, :to_param, to: :@object
+
+          include Conversions::ConvertToPolymorphicType
+          def entity_type
+            convert_to_polymorphic_type(@object)
+          end
         end
       end
     end
--- spec/forms/sipity/forms/work_enrichments/access_policy_form_spec.rb
+++ spec/forms/sipity/forms/work_enrichments/access_policy_form_spec.rb
@@ -2,16 +2,49 @@ module Sipity
   module Forms
     module WorkEnrichments
       RSpec.describe AccessPolicyForm do
+        let(:user) { double('User') }
         let(:work) { Models::Work.new(id: 1) }
         let(:attachment) { Models::Attachment.new(id: 2) }
         let(:repository) { CommandRepositoryInterface.new }
         subject { described_class.new(work: work, repository: repository) }
 
+        before do
+          allow(repository).to receive(:accessible_objects).with(work: work).and_return([work, attachment])
+        end
+
         it { should respond_to :accessible_objects_attributes= }
 
         it 'will expose accessible_objects' do
-          expect(repository).to receive(:accessible_objects).with(work: work).and_return([work, attachment])
-          subject.accessible_objects
+          expect(subject.accessible_objects.size).to eq(2)
+        end
+
+        it 'will validate the accessible_objects_attributes'
+
+        context '#submit' do
+          it 'will capture accessible_objects_attributes' do
+            attributes = {
+              "0" => { "id" => work.to_param, "access_right_code" => 'open_access', "release_date" => "" },
+              "1" => { "id" => attachment.to_param, "access_right_code" => 'embargo_then_open_access', "release_date" => "2032-12-01" }
+            }
+            subject = described_class.new(work: work, repository: repository, accessible_objects_attributes: attributes)
+            expect(repository).to receive(:apply_access_policies_to).with(
+              work: work, access_policies:
+              [
+                {
+                  entity_id: work.to_param,
+                  entity_type: Sipity::Models::Work,
+                  access_right_code: 'open_access',
+                  release_date: ''
+                }, {
+                  entity_id: attachment.to_param,
+                  entity_type: Sipity::Models::Attachment,
+                  access_right_code: 'embargo_then_open_access',
+                  release_date: '2032-12-01'
+                }
+              ]
+            )
+            subject.submit(requested_by: user)
+          end
         end
       end
 
