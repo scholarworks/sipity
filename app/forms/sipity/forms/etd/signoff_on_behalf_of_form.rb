5239011335cc959cf4afba024bda3cbcf55e0a82
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-03-04 15:19:01 -0500

    Renaming to clarify expectation
    I'm not dealing with a collaborator; I'm dealing with the identifier
    for a collaborator.

--- app/forms/sipity/forms/etd/signoff_on_behalf_of_form.rb
+++ app/forms/sipity/forms/etd/signoff_on_behalf_of_form.rb
@@ -6,24 +6,28 @@ module Sipity
       class SignoffOnBehalfOfForm < Forms::StateAdvancingAction
         def initialize(attributes = {})
           super
-          @on_behalf_of_collaborator = attributes[:on_behalf_of_collaborator]
+          @on_behalf_of_collaborator_id = attributes[:on_behalf_of_collaborator_id]
           @signoff_service = attributes.fetch(:signoff_service) { default_signoff_service }
         end
-        attr_reader :on_behalf_of_collaborator
+        attr_reader :on_behalf_of_collaborator_id
         attr_reader :signoff_service
         private :signoff_service
 
-        validates :on_behalf_of_collaborator, presence: true, inclusion: { in: :valid_on_behalf_of_collaborators }
+        validates :on_behalf_of_collaborator_id, presence: true, inclusion: { in: :valid_on_behalf_of_collaborator_ids }
 
         def valid_on_behalf_of_collaborators
           repository.collaborators_that_can_advance_the_current_state_of(work: work)
         end
 
         def render(f:)
-          f.input(:on_behalf_of_collaborator, collection: valid_on_behalf_of_collaborators, value_method: :id)
+          f.input(:on_behalf_of_collaborator_id, collection: valid_on_behalf_of_collaborators, value_method: :id)
         end
 
         private
+
+        def valid_on_behalf_of_collaborator_ids
+          valid_on_behalf_of_collaborators.map { |collaborator| collaborator.id.to_s }
+        end
 
         def save(requested_by:)
           super do
--- spec/forms/sipity/forms/etd/signoff_on_behalf_of_form_spec.rb
+++ spec/forms/sipity/forms/etd/signoff_on_behalf_of_form_spec.rb
@@ -17,18 +17,18 @@ module Sipity
 
         context 'validation' do
           before do
-            allow(repository).to receive(:collaborators_that_can_advance_the_current_state_of).and_return(['someone'])
+            allow(repository).to receive(:collaborators_that_can_advance_the_current_state_of).and_return([double(id: 'someone')])
           end
           it 'will require that someone be specified for approval' do
             subject.valid?
-            expect(subject.errors[:on_behalf_of_collaborator]).to be_present
+            expect(subject.errors[:on_behalf_of_collaborator_id]).to be_present
           end
           it 'will require that someone amongst the collaborators is specified' do
             subject = described_class.new(
-              work: work, repository: repository, on_behalf_of_collaborator: '__no_one__', processing_action_name: action
+              work: work, repository: repository, on_behalf_of_collaborator_id: '__no_one__', processing_action_name: action
             )
             subject.valid?
-            expect(subject.errors[:on_behalf_of_collaborator]).to be_present
+            expect(subject.errors[:on_behalf_of_collaborator_id]).to be_present
           end
         end
 
@@ -39,7 +39,7 @@ module Sipity
           end
           it 'will expose select box' do
             form_object = double('Form Object')
-            expect(form_object).to receive(:input).with(:on_behalf_of_collaborator, collection: [collaborator], value_method: :id).and_return("<input />")
+            expect(form_object).to receive(:input).with(:on_behalf_of_collaborator_id, collection: [collaborator], value_method: :id).and_return("<input />")
             expect(subject.render(f: form_object)).to eq("<input />")
           end
         end
@@ -49,7 +49,7 @@ module Sipity
           let(:signoff_service) { double('Signoff Service') }
           subject do
             described_class.new(
-              work: work, processing_action_name: action, repository: repository, on_behalf_of_collaborator: 'someone_valid',
+              work: work, processing_action_name: action, repository: repository, on_behalf_of_collaborator_id: 'someone_valid',
               signoff_service: signoff_service
             )
           end
