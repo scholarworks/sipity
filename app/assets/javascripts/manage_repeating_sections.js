ca800db7b2ab97bddb72a713f0e0d456bc4c01cd
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2015-03-04 12:28:44 -0500

    Collaborators table refinements; JavaScript legibility improvements

--- app/assets/javascripts/manage_repeating_sections.js
+++ app/assets/javascripts/manage_repeating_sections.js
@@ -13,12 +13,12 @@
     _create: function() {
       this.element.addClass("managed");
       $('.repeat', this.element).addClass("input-append");
-      this.controls = $("<span class=\"field-controls\">");
-      this.remover  = $("<button class=\"btn btn-danger remove\"><i class=\"icon-white icon-minus\"></i><span>Remove</span></button>");
-      this.adder    = $("<button class=\"btn btn-success add\" id=\"section_add_button\"><i class=\"icon-white icon-plus\"></i><span>Add</span></button>");
+      this.adder = $("<button class=\"btn btn-success add\" id=\"section_add_button\"><i class=\"icon-white icon-plus\"></i><span>Add</span></button>");
+      this.remover = $("<button class=\"btn btn-danger remove\"><i class=\"icon-white icon-minus\"></i><span>Remove</span></button>");
+      this.tableControls = $('.table-controls', this.element);
+      this.cannotAddNotification = $('<div class="alert alert-warning warning">You cannot add multiple empty entries.</div>');
 
-      $('.repeat', this.element).append(this.controls);
-      $('.repeat:not(:last-child) .field-controls', this.element).append(this.remover);
+      $('.repeat:not(:last-child) .row-controls', this.element).append(this.remover);
       $('.repeat:last', this.element).append(this.adder);
 
       this._on( this.element, {
@@ -30,38 +30,79 @@
       var $last_element_val = $last_line.children('td.name').first().children('div').children('input').val();
       if( $last_element_val ){
         $( '#section_add_button').trigger( 'click' );
-      } 
+      }
     },
 
     add_to_section_list: function( event ) {
       event.preventDefault();
+      console.log('add_to_section_list');
 
-      var num = $('.repeat').length;
       var $activeField = $(event.target).parents('.repeat'),
-          $activeFieldControls = $activeField.children('.field-controls'),
-          $removeControl = this.remover.clone(),
+          $activeFieldControls = $activeField.children('.row-controls'),
+          $lastNameField = $activeField.children('td.name').first().children('div').children('input'),
           $newField = $activeField.clone(),
           $listing = $activeField.parent(),
-          $warningSpan  = $("<span class=\'message warning\'>cannot add new empty field</span>");
-      if ($activeField.children('td.name').first().children('div').children('input').val() === '') {
-          $listing.children('.warning').remove();
-          $listing.append($warningSpan);
-      }
-      else{
-        $listing.children('.warning').remove();
+          $removeControl = this.remover.clone(),
+          $warning = this.tableControls.children('.warning'),
+          warningCount = $warning.length,
+          rowNumber = $('.repeat').length; // TODO: this is too simplistic to keep track of an ID
+
+      if ($lastNameField.val() === '') {
+        if (warningCount === 0) {
+          this.tableControls.prepend(this.cannotAddNotification);
+        }
+      } else {
+        $warning.remove();
         $('.add', $activeFieldControls).remove();
         $('.remove', $activeFieldControls).remove();
         $activeFieldControls.prepend($removeControl);
-        $newChild1 = $newField.children('td.name').first().children('div').children('input').attr('id', 'work_collaborators__attributes_'+num+'_name').attr('name', 'work[collaborators_attributes][' + num + '][name]');
-        $newChild2 = $newField.children('td.role').last().children('div').children('select').attr('id', 'work_collaborators_attributes_'+num+'_role').attr('name', 'work[collaborators_attributes][' + num + '][role]');
-        $newChild3 = $newField.children('td.review').last().children('div').children('select').attr('id', 'work_collaborators_attributes_'+num+'_responsible_for_review').attr('name', 'work[collaborators_attributes][' + num + '][responsible_for_review]');
-        $newChild4 = $newField.children('td.contact-information').first().children('span').first().children('div').children('input').attr('id', 'work_collaborators__attributes_'+num+'_netid').attr('name', 'work[collaborators_attributes][' + num + '][netid]');
-        $newChild5 = $newField.children('td.contact-information').first().children('span').last().children('div').children('input').attr('id', 'work_collaborators__attributes_'+num+'_email').attr('name', 'work[collaborators_attributes][' + num + '][email]');
+
+        $newChild1 = $newField.children('td.name')
+                       .first()
+                       .children('div')
+                       .children('input')
+                       .attr('id', 'work_collaborators__attributes_' + rowNumber + '_name')
+                       .attr('name', 'work[collaborators_attributes][' + rowNumber + '][name]');
+        $newChild2 = $newField
+                       .children('td.role')
+                       .last()
+                       .children('div')
+                       .children('select')
+                       .attr('id', 'work_collaborators_attributes_' + rowNumber + '_role')
+                       .attr('name', 'work[collaborators_attributes][' + rowNumber + '][role]');
+        $newChild3 = $newField
+                       .children('td.review')
+                       .last()
+                       .children('div')
+                       .children('select')
+                       .attr('id', 'work_collaborators_attributes_' + rowNumber + '_responsible_for_review')
+                       .attr('name', 'work[collaborators_attributes][' + rowNumber + '][responsible_for_review]');
+        $newChild4 = $newField
+                       .children('td.contact-information')
+                       .first()
+                       .children('span')
+                       .first()
+                       .children('div')
+                       .children('input')
+                       .attr('id', 'work_collaborators__attributes_' + rowNumber + '_netid')
+                       .attr('name', 'work[collaborators_attributes][' + rowNumber + '][netid]');
+        $newChild5 = $newField
+                       .children('td.contact-information')
+                       .first()
+                       .children('span')
+                       .last()
+                       .children('div')
+                       .children('input')
+                       .attr('id', 'work_collaborators__attributes_' + rowNumber + '_email')
+                       .attr('name', 'work[collaborators_attributes][' + rowNumber + '][email]');
+
         $newChild1.val('');
         $newChild2.val('');
         $newChild3.children("option[value='No']").attr("selected","selected");
         $newChild4.val('');
         $newChild5.val('');
+
+        $activeField.children('button').remove();
         $listing.append($newField);
         $('.remove', $newField).remove();
         $newChild1.first().focus();
