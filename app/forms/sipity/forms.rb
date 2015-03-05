301239099a184856c76c733989761239b3e2dcd4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-03-04 20:23:48 -0500

    Wordsmithing about Forms
    [skip ci]

--- app/forms/sipity/forms.rb
+++ app/forms/sipity/forms.rb
@@ -13,9 +13,16 @@ module Sipity
   # They are things that could be rendered via the `simple_form_for` view
   # template method.
   #
+  # In the case of Forms, I want to avoid exposing means of updating the data
+  # after initialization. That means no attribute accessors; Because adding
+  # public setter methods means I have to worry about mutability after
+  # initialization. Perhaps not a big deal, but if I can avoid that problem,
+  # I'd much prefer to. One added benefit is that I can do simple "change"
+  # checks without having to manage more internal state of the object.
+  #
   # @note By introducing Forms, there is less of a reliance on the anti-pattern
-  #   of Strong Patterns. That is to say, only expose explicit attributes to the
-  #   UI. And should the controller be the thing saying what parameters are
+  #   of Strong Parametesr. That is to say, only expose explicit attributes to
+  #   the UI. And should the controller be the thing saying what parameters are
   #   permitted for mass assignment? Not if you intend to create command line
   #   tools.
   # @see [Nick Sutterer's Reform gem](http://github.com/apotonick/reform) for
