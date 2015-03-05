a9b2ce9400977ff3f322583a7a0b69caf88a7c85
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-03-05 09:12:03 -0500

    Reworking LdapClient to ensure coverage
    The client was confusing. There were two methods that queried.
    
    One was a method that caught a time out exception. It would then raise
    a UserNotFound error. That is confusing, because the exception is now
    translating the timeout error to something with a different meaning.
    
    So I've opted to remove that; as it wasn't called. Also, I'm factoring
    towards accepting an connection instead of instantiating a new one;
    Perhaps there should be a connection that is initialized as part of
    boot process of the application. Don't know and not yet important.

--- app/services/sipity/services/ldap_client.rb
+++ app/services/sipity/services/ldap_client.rb
@@ -5,8 +5,6 @@ module Sipity
     class LdapClient
       attr_reader :connection
 
-      LDAP_TIME_OUT = 15
-
       # The ldap_options is a hash containing
       # :host, :port, :encryption and :base
       def initialize(ldap_options)
@@ -17,34 +15,16 @@ module Sipity
         ldap_entry_for(netid: netid).present?
       end
 
-      # Custom error class
-      class UserNotFoundError < RuntimeError
-        def initialize(error_message)
-          super(error_message)
-        end
-      end
-
       private
 
       def ldap_entry_for(netid:)
-        results = connection.search(
-                      attributes: ['uid', 'mail', 'displayName'],
-                      filter: Net::LDAP::Filter.eq('uid', netid),
-                      return_result: true
-        )
-        return nil if results.blank?
-        results.first
-      end
-
-      def ldap_query
-        Timeout.timeout(LDAP_TIME_OUT) do
-          result = ldap_lookup
-          if result.present?
-            return result
-          else
-            fail UserNotFoundError, "User: #{net_id} is not found."
-          end
-        end
+        Array.wrap(
+          connection.search(
+            attributes: ['uid', 'mail', 'displayName'],
+            filter: Net::LDAP::Filter.eq('uid', netid),
+            return_result: true
+          )
+        ).first
       end
     end
   end
--- spec/services/sipity/services/ldap_client_spec.rb
+++ spec/services/sipity/services/ldap_client_spec.rb
@@ -3,20 +3,20 @@ module Sipity
   module Services
     RSpec.describe LdapClient do
       let(:ldap_options) do
-        { host: 'directory.example.com', port: '636', encryption: :simple_tls, base: 'o="Example LLC", st=Indiana, c=US' }
+        { host: 'directory.example.com', port: '636', encryption: :simple_tls }
       end
       context "#netid_valid?" do
         subject { LdapClient.new(ldap_options) }
         let(:netid) { 'a possible netid' }
         context 'with a valid netid' do
           it "will return true for a valid netid" do
-            expect(subject).to receive(:ldap_entry_for).and_return(true)
+            expect(subject.connection).to receive(:search).and_return(true)
             expect(subject.valid_netid?(netid)).to eq(true)
           end
         end
         context "with invalid net id" do
           it "should return false" do
-            expect(subject).to receive(:ldap_entry_for).and_return(nil)
+            expect(subject.connection).to receive(:search).and_return(false)
             expect(subject.valid_netid?(netid)).to eq(false)
           end
         end
