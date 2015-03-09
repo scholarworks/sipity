bc582380fbf86b0ec0a9e5aff801647128d72243
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-03-09 11:05:00 -0400

    Adding order to controlled vocabulary
    Instead of arbitrary ordering of the returned values, opt for
    alphabetical.

--- app/repositories/sipity/queries/simple_controlled_vocabulary_queries.rb
+++ app/repositories/sipity/queries/simple_controlled_vocabulary_queries.rb
@@ -3,7 +3,7 @@ module Sipity
     # Queries
     module SimpleControlledVocabularyQueries
       def get_values_by_predicate_name(name:)
-        Models::SimpleControlledVocabulary.where(predicate_name: name).pluck(:predicate_value)
+        Models::SimpleControlledVocabulary.where(predicate_name: name).order(:predicate_value).pluck(:predicate_value)
       end
     end
   end
