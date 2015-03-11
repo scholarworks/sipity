cb4b57da54e434756159676e8399fe91294a8c33
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-03-11 15:05:01 -0400

    Adding QA script to add a qa_username
    ```console
    cap deploy:qa:add_qa_user_to_group
    ```
    
    ```console
    cap deploy:qa:remove_qa_user_to_group
    ```
    
    [skip ci]

--- config/deploy.rb
+++ config/deploy.rb
@@ -82,6 +82,29 @@ namespace :deploy do
       end
     end
   end
+
+  namespace :qa do
+    desc 'Add user with :qa_username to ETD Group'
+    task :add_qa_user_to_group do
+      on roles(:app) do
+        within release_path do
+          with rails_env: fetch(:rails_env) do
+            execute :rails, %(runner -e #{fetch(:rails_env)} "group = Sipity::Models::Group.where(name: 'Graduate School Reviewers').first!; user = User.where(username: Figaro.env.qa_username!).first!; group.users << user unless group.users.include?(user)")
+          end
+        end
+      end
+    end
+    desc 'Remove user with :qa_username from ETD Group'
+    task :remove_qa_user_to_group do
+      on roles(:app) do
+        within release_path do
+          with rails_env: fetch(:rails_env) do
+            execute :rails, %(runner -e #{fetch(:rails_env)} "group = Sipity::Models::Group.where(name: 'Graduate School Reviewers').first!; user = User.where(username: Figaro.env.qa_username!).first!; group.group_memberships.where(user_id: user.id).destroy_all")
+          end
+        end
+      end
+    end
+  end
 end
 
 namespace :configuration do
