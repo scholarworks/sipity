6015a8f0fb6ee55d04b8b92e576b1167acca3ead
Banurekha <banurekha.l@gmail.com>
2015-03-12 13:10:06 -0400

    adding server and pool config

--- app/services/sipity/services/noid_minter.rb
+++ app/services/sipity/services/noid_minter.rb
@@ -6,7 +6,18 @@ module Sipity
             host: Figaro.env.noid_server!,
             port: Figaro.env.noid_port!
         }
-        @service ||= ::NoidsClient::Connection.new(@server).get_pool(@pool)
+        new(configuration).call
+      end
+
+      # The ldap_options is a hash containing
+      # :host, :port, :encryption and :base
+      def initialize(noid_options)
+        @connection = ::NoidsClient::Connection.new(noid_options.fetch[:server])
+                      .get_pool(noid_options.fetch[:pool])
+      end
+
+      def template
+        @template ||= connection.template.split("+").first
       end
 
       # Returns a single NOID
