0e0d35760d791981129292092be7ee27885ab1a8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-03-13 11:19:27 -0400

    Amending the environment bootstrapper
    [skip ci]

--- config/environment_bootstrapper.rb
+++ config/environment_bootstrapper.rb
@@ -1,4 +1,10 @@
 # Don't worry, this will be obliterated by super secret environment bootstrap
 # type things.
 $stdout.puts "Creating User '#{ENV['USER']}' (from ENV['USER'])..."
-User.find_or_create_by!(username: ENV['USER'])
+user = User.find_or_create_by!(username: ENV['USER'])
+
+$stdout.puts "Finding or creating group for 'Graduate School Reviewers'"
+graduate_school = Sipity::Models::Group.find_or_create_by!(name: 'Graduate School Reviewers')
+
+$stdout.puts "Associating #{user.username} with #{graduate_school.name}"
+graduate_school.users << user unless graduate_school.users.include?(user)
