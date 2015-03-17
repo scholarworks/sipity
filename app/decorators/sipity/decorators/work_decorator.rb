30b603d1b705b307a257e06c7eb6e6016f556468
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-03-17 13:19:37 -0400

    Removing #with_action_pane
    This was a kludge and resulted in decorating an object within the
    repository methods; Something that I want to avoid. So I'm attempting
    to streamline the behavior.

--- app/decorators/sipity/decorators/work_decorator.rb
+++ app/decorators/sipity/decorators/work_decorator.rb
@@ -15,10 +15,6 @@ module Sipity
         h.render(layout: 'sipity/form_panel', locals: { name: name, theme: theme, object: self }, &block)
       end
 
-      def with_action_pane(name, css_class = '', &block)
-        h.render(layout: 'sipity/form_action_pane', locals: { name: name, css_class: css_class, object: self }, &block)
-      end
-
       def to_s
         title
       end
