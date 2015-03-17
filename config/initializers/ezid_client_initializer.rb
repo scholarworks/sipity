74b1be29f23478d9cd3b4b61cd9fbae42873954b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-03-17 14:08:10 -0400

    Tidying up configuration management
    * Alphabetizing keys
    * Removing some uses of the secrets.yml (Don't like it; Bleck!)

--- config/initializers/ezid_client_initializer.rb
+++ config/initializers/ezid_client_initializer.rb
@@ -1,5 +1,5 @@
 Ezid::Client.configure do |config|
-  config.default_shoulder = Rails.application.secrets.ezid_client_default_shoulder
-  config.user = Rails.application.secrets.ezid_client_user
-  config.password = Rails.application.secrets.ezid_client_password
+  config.default_shoulder = Figaro.env.ezid_client_default_shoulder!
+  config.user = Figaro.env.ezid_client_user!
+  config.password = Figaro.env.ezid_client_password!
 end
