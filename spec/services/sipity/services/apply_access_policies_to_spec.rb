b795ee8908ad0d9be8c074faa00201d8f0a996c6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-03-17 11:41:10 -0400

    Ensuring access policies are updated
    ```gherkin
    Given I have created a work
    When I go to update the policy form
    Then the updates are not persisted
    ```
    
    Closes #343

--- spec/services/sipity/services/apply_access_policies_to_spec.rb
+++ spec/services/sipity/services/apply_access_policies_to_spec.rb
@@ -14,17 +14,25 @@ module Sipity
         described_class.call(work: work, user: user, access_policies: access_policies)
       end
 
-      it 'will not allow specifying policies for objects not part of the existing work'
-
       let(:access_policies) do
         { entity_id: 1, entity_type: Sipity::Models::Work, access_right_code: Models::AccessRight::OPEN_ACCESS, release_date: '' }
       end
+
       it 'create a new AccessRight' do
         expect { subject.call }.to change { Models::AccessRight.count }.by(1)
       end
-      it 'will obliterate the previous AccessRights' do
+
+      it 'will preserve the given access rights based on keyed input' do
         subject.call
         expect { subject.call }.to_not change { Models::AccessRight.count }
+      end
+
+      it 'will update an existing access policy with new information' do
+        access_right = Models::AccessRight.create!(access_policies)
+        attributes = access_policies.merge(access_right_code: Models::AccessRight::PRIVATE_ACCESS)
+        subject = described_class.new(work: work, user: user, access_policies: attributes)
+        subject.call
+        expect(access_right.reload.access_right_code).to eq(Models::AccessRight::PRIVATE_ACCESS)
       end
     end
   end
