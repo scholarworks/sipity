723dc263d3506427e45d8b41155e59f76deb9a79
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-03-24 10:09:39 -0700

    Requiring acceptance for Grad School signoff
    Closes #274

--- app/forms/sipity/forms/etd/grad_school_signoff_form.rb
+++ app/forms/sipity/forms/etd/grad_school_signoff_form.rb
@@ -3,7 +3,47 @@ module Sipity
     module Etd
       # Responsible for submitting the final Grad School approval.
       class GradSchoolSignoffForm < Forms::StateAdvancingAction
+        def initialize(attributes = {})
+          super
+          self.agree_to_signoff = attributes[:agree_to_signoff]
+        end
+
+        attr_reader :agree_to_signoff
+        validates :agree_to_signoff, acceptance: { accept: true }
+
+        # @param f SimpleFormBuilder
+        #
+        # @return String
+        def render(f:)
+          markup = view_context.content_tag('legend', legend)
+          markup << f.input(:agree_to_signoff,
+                            as: :boolean,
+                            inline_label:
+                            signoff_agreement,
+                            input_html: { required: 'required' }, # There is no way to add true boolean attributes to simle_form fields.
+                            label: false,
+                            wrapper_class: 'checkbox'
+                            ).html_safe
+        end
+
+        def legend
+          view_context.t('etd/grad_school_signoff', scope: 'sipity/forms.state_advancing_actions.legend').html_safe
+        end
+
+        def signoff_agreement
+          view_context.t('i_agree', scope: 'sipity/forms.state_advancing_actions.verification.etd/grad_school_signoff').html_safe
+        end
+
         private
+
+        include Conversions::ConvertToBoolean
+        def agree_to_signoff=(value)
+          @agree_to_signoff = convert_to_boolean(value)
+        end
+
+        def view_context
+          Draper::ViewContext.current
+        end
 
         def save(requested_by:)
           super do
--- spec/forms/sipity/forms/etd/grad_school_signoff_form_spec.rb
+++ spec/forms/sipity/forms/etd/grad_school_signoff_form_spec.rb
@@ -9,7 +9,7 @@ module Sipity
         let(:repository) { CommandRepositoryInterface.new }
         let(:action) { Models::Processing::StrategyAction.new(strategy_id: processing_entity.strategy_id, name: "hello") }
         let(:user) { User.new(id: 1) }
-        subject { described_class.new(work: work, processing_action_name: action, repository: repository) }
+        subject { described_class.new(work: work, processing_action_name: action, agree_to_signoff: true, repository: repository) }
 
         its(:processing_action_name) { should eq(action.name) }
 
@@ -19,6 +19,25 @@ module Sipity
             expect(subject.action).to eq(action)
           end
         end
+
+        context 'validation' do
+          it 'will require agreement to the signoff' do
+            subject = described_class.new(work: work, processing_action_name: action, repository: repository)
+            subject.valid?
+            expect(subject.errors[:agree_to_signoff]).to be_present
+          end
+        end
+
+        context '#render' do
+          it 'will render HTML safe submission terms and confirmation' do
+            form_object = double('Form Object')
+            expect(form_object).to receive(:input).with(:agree_to_signoff, hash_including(as: :boolean)).and_return("<input />")
+            expect(subject.render(f: form_object)).to be_html_safe
+          end
+        end
+
+        its(:legend) { should be_html_safe }
+        its(:signoff_agreement) { should be_html_safe }
 
         it 'will log the event' do
           expect(repository).to receive(:log_event!).and_call_original
