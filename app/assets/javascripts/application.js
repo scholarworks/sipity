baf9e231783f1867ecbc2f2647052d419e33b00f
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2015-03-27 21:31:45 -0400

    Fixing load path for jQuery UI Widget

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery-ui-widget
+//= require vendor/jquery-ui-widget
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
