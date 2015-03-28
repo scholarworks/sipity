fcf6babb8bb95e5b78fa7d626710849a15d38cfd
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2015-03-27 20:46:50 -0400

    Vendoring modernizr

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
-//= stub modernizr
+//= stub vendor/modernizr
 
 (function($) {
   var adjustRequiredAttachements = function(){
