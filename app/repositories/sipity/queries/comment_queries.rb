489a8eb9d1e3cc488ac3b4733b89549215a28ad1
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-02 14:36:49 -0400

    Adding method to find all current comments
    > Only show “active” comments on the work show page. Active comments
    > come from the immediately preceding state.
    
    As a slight modification, we will find all comments that were written
    as part of any action that can transition the entity to its current
    state. Finding comments from the previous state is not feasible based
    on the current format.

--- app/repositories/sipity/queries/comment_queries.rb
+++ app/repositories/sipity/queries/comment_queries.rb
@@ -6,6 +6,25 @@ module Sipity
         entity = Conversions::ConvertToProcessingEntity.call(work)
         Sipity::Models::Processing::Comment.where(entity_id: entity.id).order('created_at DESC')
       end
+
+      def find_current_comments_for_work(work:)
+        entity = Conversions::ConvertToProcessingEntity.call(work)
+        comments = Models::Processing::Comment.arel_table
+        entities = Models::Processing::Entity.arel_table
+        actions = Models::Processing::StrategyAction.arel_table
+
+        Models::Processing::Comment.where(
+          comments[:entity_id].eq(entity.id).and(
+            comments[:originating_strategy_action_id].in(
+              actions.project(
+                actions[:id]
+              ).join(entities).on(
+                actions[:resulting_strategy_state_id].eq(entities[:strategy_state_id])
+              ).where(entities[:id].eq(entity.id))
+            )
+          )
+        ).order('created_at DESC')
+      end
     end
   end
 end
