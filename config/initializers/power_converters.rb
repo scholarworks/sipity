86fbd31ef1c21cee55074f2d7217f8aace41ae17
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-10 10:07:28 -0400

    Adding conversions for :strategy_state
    I was using this elsewhere, and want to extract the behavior.

---
+++ config/initializers/power_converters.rb
@@ -0,0 +1,10 @@
+require 'power_converter'
+
+PowerConverter.define_conversion_for(:strategy_state) do |input, strategy|
+  case input
+  when Sipity::Models::Processing::StrategyState
+    input
+  when Symbol, String
+    Sipity::Models::Processing::StrategyState.where(strategy_id: strategy.id, name: input).first
+  end
+end
