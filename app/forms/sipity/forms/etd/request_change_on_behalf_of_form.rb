39ae4f571d131c15364921f2478da3cc1ab84a5e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-10 13:49:59 -0400

    Appeasing rubocop through method extraction
    This is the first step in refactoring as I'll be able to make a
    service object that holds the given state.

--- app/forms/sipity/forms/etd/request_change_on_behalf_of_form.rb
+++ app/forms/sipity/forms/etd/request_change_on_behalf_of_form.rb
@@ -30,16 +30,35 @@ module Sipity
 
         def save(requested_by:)
           # TODO: Consider extracting common behavior to a service method (see AdvisorRequestsChangeForm#save)
-          processing_comment = repository.record_processing_comment(
+          send_notification_for(processing_comment: record_processing_comment)
+          register_action_taken(requested_by: requested_by)
+          log_event(requested_by: requested_by)
+          update_processing_state
+        end
+
+        def record_processing_comment
+          repository.record_processing_comment(
             entity: work, commenter: on_behalf_of_collaborator, comment: comment, action: action
           )
+        end
+
+        def send_notification_for(processing_comment:)
           repository.send_notification_for_entity_trigger(
             notification: enrichment_type, entity: processing_comment, acting_as: ['creating_user']
           )
+        end
+
+        def register_action_taken(requested_by:)
           repository.register_action_taken_on_entity(
             work: work, enrichment_type: enrichment_type, requested_by: requested_by, on_behalf_of: on_behalf_of_collaborator
           )
+        end
+
+        def log_event(requested_by:)
           repository.log_event!(entity: work, user: requested_by, event_name: event_name)
+        end
+
+        def update_processing_state
           repository.update_processing_state!(entity: work, to: action.resulting_strategy_state)
         end
 
