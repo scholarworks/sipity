9bb707bef90fe16eed351136334c0ba6f0df63ac
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-13 15:44:22 -0400

    Enumeration enforcement for recipient_strategy

--- app/models/sipity/models/notification/email_recipient.rb
+++ app/models/sipity/models/notification/email_recipient.rb
@@ -7,6 +7,14 @@ module Sipity
         self.table_name = 'sipity_notification_email_recipients'
         belongs_to :email, class_name: 'Sipity::Models::Notification::Email'
         belongs_to :role, class_name: 'Sipity::Models::Role'
+
+        enum(
+          recipient_strategy: {
+            'to' => 'to',
+            'cc' => 'cc',
+            'bcc' => 'bcc'
+          }
+        )
       end
     end
   end
--- spec/models/sipity/models/notification/email_recipient_spec.rb
+++ spec/models/sipity/models/notification/email_recipient_spec.rb
@@ -10,6 +10,11 @@ module Sipity
           its(:column_names) { should include('email_id') }
           its(:column_names) { should include('recipient_strategy') }
         end
+
+        subject { described_class.new }
+        it 'will raise an ArgumentError if you provide an invalid recipient_strategy' do
+          expect { subject.recipient_strategy = '__incorrect_name__' }.to raise_error(ArgumentError)
+        end
       end
     end
   end
