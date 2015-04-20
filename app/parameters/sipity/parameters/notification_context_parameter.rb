c30784566695190f6ea38435997c1f7472adcdab
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-20 15:32:07 -0400

    Removing a TODO item

--- app/parameters/sipity/parameters/notification_context_parameter.rb
+++ app/parameters/sipity/parameters/notification_context_parameter.rb
@@ -7,7 +7,6 @@ module Sipity
       attr_reader :scope, :reason, :the_thing, :requested_by, :on_behalf_of
       def initialize(**keywords)
         self.the_thing = keywords.fetch(:the_thing)
-        # TODO: Should this be converted to an scope via a conversion method?
         self.scope = keywords.fetch(:scope)
         self.requested_by = keywords.fetch(:requested_by) { nil }
         self.on_behalf_of = keywords[:on_behalf_of] || requested_by
