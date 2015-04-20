50b3b4078334469eda5dda70c057b7b347d8757a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-20 15:31:27 -0400

    Reworking NotificationContextParameter methods
    I want to make the :reason a bit more general

--- app/parameters/sipity/parameters/notification_context_parameter.rb
+++ app/parameters/sipity/parameters/notification_context_parameter.rb
@@ -4,22 +4,26 @@ module Sipity
     # notification.
     class NotificationContextParameter
       NOTIFYING_CONTEXT_ACTION_IS_TAKEN = 'action_is_taken'.freeze
-      attr_reader :scope, :the_thing, :requested_by, :on_behalf_of
+      attr_reader :scope, :reason, :the_thing, :requested_by, :on_behalf_of
       def initialize(**keywords)
         self.the_thing = keywords.fetch(:the_thing)
         # TODO: Should this be converted to an scope via a conversion method?
         self.scope = keywords.fetch(:scope)
         self.requested_by = keywords.fetch(:requested_by) { nil }
         self.on_behalf_of = keywords[:on_behalf_of] || requested_by
+        self.reason = keywords.fetch(:reason) { default_reason }
       end
 
-      def reason_for_notification
-        NOTIFYING_CONTEXT_ACTION_IS_TAKEN
-      end
+      alias_method :reason_for_notification, :reason
+      deprecate :reason_for_notification
 
       private
 
-      attr_writer :scope, :the_thing, :requested_by, :on_behalf_of
+      def default_reason
+        NOTIFYING_CONTEXT_ACTION_IS_TAKEN
+      end
+
+      attr_writer :scope, :the_thing, :requested_by, :on_behalf_of, :reason
     end
   end
 end
