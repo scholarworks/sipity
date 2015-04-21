21c16d829cd28e6ab8dfffa4d3a4e5f1292bc8eb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-21 18:48:21 -0400

    Creating the WorkArea experiment
    Instead of using the current convention of a Runner and inline
    response handling, I'm opting to create a custom respond handling eco-
    system.
    
    This should be considered experimental, but I'm enjoying the greatly
    simplified controller testing. I may need to inject a dummy responder
    that doesn't care about view rendering and such non-sense.

---
+++ app/controllers/sipity/controllers/work_areas_controller.rb
@@ -0,0 +1,41 @@
+module Sipity
+  module Controllers
+    # Responsible for handling an HTTP request to interact with a WorkArea.
+    #
+    # @note This is part of the Sipity::ResponseHandlers experimentation.
+    class WorkAreasController < ApplicationController
+      class_attribute :response_handler_container
+      self.runner_container = Sipity::Runners::WorkRunners
+      self.response_handler_container = Sipity::ResponseHandlers::WorkAreaHandler
+
+      def show
+        runner_response = run(work_area_slug: work_area_slug)
+        handle_response(runner_response)
+      end
+
+      attr_accessor :view_object
+      helper_method :view_object
+
+      private
+
+      def work_area_slug
+        params.require(:work_area_slug)
+      end
+
+      def handle_response(handled_response)
+        Sipity::ResponseHandlers.handle_response(
+          context: self,
+          handled_response: handled_response,
+          container: response_handler_container
+        )
+      end
+
+      def run(*args)
+        # TODO: This is an intermediary step that will be wrapped into the
+        #   existing #run method; However it should be considered experimental
+        status, object = super(*args)
+        Parameters::HandledResponseParameter.new(status: status, object: object)
+      end
+    end
+  end
+end
---
+++ spec/controllers/sipity/controllers/work_areas_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+module Sipity
+  module Controllers
+    RSpec.describe WorkAreasController, type: :controller do
+      let(:work_area) { Models::WorkArea.new(slug: 'The Title') }
+      let(:status) { :success }
+      # REVIEW: It is possible the runner will return a well formed object
+      let(:runner) { double('Runner', run: [status, work_area]) }
+      context 'configuration' do
+        its(:runner_container) { should eq(Sipity::Runners::WorkRunners) }
+        its(:response_handler_container) { should eq(Sipity::ResponseHandlers::WorkAreaHandler) }
+      end
+
+      context 'GET #show' do
+        before { controller.runner = runner }
+        it 'will pass along to the response handler' do
+          expect_any_instance_of(Sipity::ResponseHandlers::WorkAreaHandler::SuccessResponse).to receive(:respond)
+          get 'show', work_area_slug: work_area.slug
+
+          expect(controller.view_object).to be_present
+        end
+      end
+    end
+  end
+end
