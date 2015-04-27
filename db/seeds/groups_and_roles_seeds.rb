36cacd68b8972ffe99facbb8e7df8459d8454ce7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-27 15:45:22 -0400

    Tidying up some spec warnings

--- db/seeds/groups_and_roles_seeds.rb
+++ db/seeds/groups_and_roles_seeds.rb
@@ -1,5 +1,5 @@
 $stdout.puts 'Creating Groups...'
-GRADUATE_SCHOOL_REVIEWERS = 'Graduate School Reviewers'
+GRADUATE_SCHOOL_REVIEWERS = 'Graduate School Reviewers' unless defined?(GRADUATE_SCHOOL_REVIEWERS)
 Sipity::Conversions::ConvertToProcessingActor.call(
   Sipity::Models::Group.find_or_create_by!(name: GRADUATE_SCHOOL_REVIEWERS)
 )
