8f6ca1a5a7ff0e453818171c5e9519844ae87698
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-27 15:40:19 -0400

    Removing creation of ApplicationConcept
    As I said, this was experimental. As such I don't want to keep
    kicking it around.

--- app/services/sipity/services/create_work_area_service.rb
+++ app/services/sipity/services/create_work_area_service.rb
@@ -23,7 +23,6 @@ module Sipity
       end
 
       def call
-        find_or_create_the_work_area_application_concept!
         create_processing_strategy!
         create_work_area!
         associate_work_area_with_processing_strategy!
@@ -38,17 +37,7 @@ module Sipity
       private
 
       attr_writer :work_area
-      attr_reader :processing_strategy, :work_area_managers, :application_concept, :strategy_role
-
-      def find_or_create_the_work_area_application_concept!
-        @application_concept ||= begin
-          Models::ApplicationConcept.find_by(
-            class_name: work_area.class.to_s
-          ) || Models::ApplicationConcept.create!(
-            class_name: work_area.class.to_s, slug: 'areas', name: 'Work Area'
-          )
-        end
-      end
+      attr_reader :processing_strategy, :work_area_managers, :strategy_role
 
       def find_or_create_work_area(attributes)
         Models::WorkArea.find_by(attributes.slice(:name)) || Models::WorkArea.create!(attributes)
@@ -68,7 +57,7 @@ module Sipity
         @processing_strategy ||= begin
           Models::Processing::Strategy.find_by(proxy_for_type: work_area.class.to_s) ||
           Models::Processing::Strategy.create!(
-            proxy_for_id: 0, proxy_for_type: work_area.class, name: "#{application_concept.name} processing strategy"
+            proxy_for_id: 0, proxy_for_type: work_area.class, name: "#{work_area.class} processing strategy"
           )
         end
       end
