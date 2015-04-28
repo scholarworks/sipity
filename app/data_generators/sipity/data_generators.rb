32c2d6fecbbbc9b6a5bc0d7afa9d689b4499f9fb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-28 16:14:18 -0400

    Amending DataGenerators documentation
    [skip ci]

--- app/data_generators/sipity/data_generators.rb
+++ app/data_generators/sipity/data_generators.rb
@@ -3,6 +3,18 @@ module Sipity
   # to provide classes and infrastructure for bootstrapping the configuration
   # related data entries.
   #
+  # Instead of having these objects sit amongst the "services bucket", I want to
+  # sequester them into their own space.
+  #
+  # Objects in the DataGenerators module are present for "bootstrapping"
+  # via a developer/sys admin's command line; They represent the necessary
+  # steps to bootstrap a viable ecosystem.
+
+  # Yet they are not seeds; They encompass the logic required to get
+  # certain "foundational" elements up and running. They also represent
+  # a crease in the code that will allow us to craft plugins for new
+  # Work Areas, Submission Windows, and Work Types.
+  #
   # ## Why Not Factories?
   #
   # Because of the ubiquitous FactoryGirl, I am avoiding the name of Factory.
