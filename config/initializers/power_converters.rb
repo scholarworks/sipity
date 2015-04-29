3cbd1aa1c0f5dc2081b931e7b44c8f7fee3835b5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-29 13:51:17 -0400

    Expanding WorkType Power Conversion
    Instead of simply looking for the data, go ahead and create that data.

--- config/initializers/power_converters.rb
+++ config/initializers/power_converters.rb
@@ -38,7 +38,11 @@ end
 PowerConverter.define_conversion_for(:work_type) do |input|
   case input
   when Symbol, String
-    Sipity::Models::WorkType.find_by(name: input.to_s)
+    begin
+      Sipity::Models::WorkType.find_or_create_by!(name: input.to_s)
+    rescue ArgumentError
+      nil
+    end
   when Sipity::Models::WorkType
     input
   end
--- spec/config/initializers/power_converters_spec.rb
+++ spec/config/initializers/power_converters_spec.rb
@@ -65,19 +65,22 @@ RSpec.describe 'power converters' do
   context "work_type" do
     let(:a_work_type) { Sipity::Models::WorkType.new }
     it 'will attempt to find the given String' do
-      expect(Sipity::Models::WorkType).to receive(:find_by).with(name: 'doctoral_dissertation').and_return(a_work_type)
+      expect(Sipity::Models::WorkType).to receive(:find_or_create_by!).with(name: 'doctoral_dissertation').and_return(a_work_type)
       expect(PowerConverter.convert('doctoral_dissertation', to: :work_type)).to eq(a_work_type)
     end
 
     it 'will attempt to find the given Symbol' do
-      expect(Sipity::Models::WorkType).to receive(:find_by).with(name: 'doctoral_dissertation').and_return(a_work_type)
+      expect(Sipity::Models::WorkType).to receive(:find_or_create_by!).with(name: 'doctoral_dissertation').and_return(a_work_type)
       expect(PowerConverter.convert(:doctoral_dissertation, to: :work_type)).to eq(a_work_type)
     end
 
     it 'will raise an error if not found' do
-      expect(Sipity::Models::WorkType).to receive(:find_by).with(name: 'doctoral_dissertation').and_return(nil)
-      expect { PowerConverter.convert(:doctoral_dissertation, to: :work_type) }.
+      expect { PowerConverter.convert(:chicken, to: :work_type) }.
         to raise_error(PowerConverter::ConversionError)
+    end
+
+    it 'will create if we have a valid work type' do
+      expect(PowerConverter.convert(:doctoral_dissertation, to: :work_type)).to be_a(Sipity::Models::WorkType)
     end
 
     it 'will pass through a given WorkType' do
