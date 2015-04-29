e1978473aa9b8eac10c181139ee23b2f51b12b97
Banurekha <banurekha.l@gmail.com>
2015-04-29 13:13:39 -0400

    Seed an all_registered_users group

--- db/seeds/groups_and_roles_seeds.rb
+++ db/seeds/groups_and_roles_seeds.rb
@@ -4,6 +4,10 @@ Sipity::Conversions::ConvertToProcessingActor.call(
   Sipity::Models::Group.find_or_create_by!(name: GRADUATE_SCHOOL_REVIEWERS)
 )
 
+$stdout.puts 'Add existing users to All Registered Users Groups...'
+  Sipity::Models::Group.find_or_create_by!(name: ALL_REGISTERED_USERS)
+  #TODO add all existing users to that group (unless they are already there)
+
 $stdout.puts 'Creating Valid Roles...'
 Sipity::Models::Role.valid_names.each do |name|
   Sipity::Models::Role.find_or_create_by!(name: name)
