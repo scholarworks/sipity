1c6eb3c0f8378c14d27f738548e8f1aaaca862a7
Banurekha <banurekha.l@gmail.com>
2015-04-29 16:07:17 -0400

    fixing the issue with decalaring default_on_user_create_service

--- config/application.rb
+++ config/application.rb
@@ -31,8 +31,8 @@ module Sipity
       Services::NoidMinter.call
     end
 
-    config.default_on_user_create_service = lambda do
-      DataGenerators::OnUserCreate
+    config.default_on_user_create_service = lambda do |a_user|
+      Sipity::DataGenerators::OnUserCreate.call(a_user)
     end
 
     # Settings in config/environments/* take precedence over those specified here.
