1c1f275a6c81b5887f9392b7e52e549c1844d469
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-30 14:02:18 -0400

    Extracting logic via composition
    Instead of the assumptive behavior, leveraging the common
    PermissionGenerator class.

--- app/data_generators/sipity/data_generators/etd/work_area_processing_generator.rb
+++ app/data_generators/sipity/data_generators/etd/work_area_processing_generator.rb
@@ -16,8 +16,8 @@ module Sipity
 
         private
 
-        attr_accessor :processing_strategy
-        attr_reader :work_area, :work_area_viewer_role, :work_area_viewers
+        attr_accessor :processing_strategy, :work_area_viewers, :work_area_viewer_role
+        attr_reader :work_area
 
         def work_area=(input)
           @work_area = PowerConverter.convert(input, to: :work_area)
@@ -27,54 +27,23 @@ module Sipity
           Models::Role::WORK_AREA_VIEWER
         end
 
-        def work_area_viewer_role=(input)
-          @work_area_viewer_role = Conversions::ConvertToRole.call(input)
-        end
-
-        def work_area_viewers=(input)
-          @work_area_viewers = Array.wrap(input).map {|i| Conversions::ConvertToProcessingActor.call(i) }
-        end
-
         def default_work_area_viewers
           Models::Group.all_registered_users
         end
 
+        PERMITTED_WORK_AREA_VIEWER_ACTIONS = ['show'].freeze
+
         public
 
         def call
-          strategy_role = associate_authenticated_user_with_work_area!
-          allow_authenticated_user_to_view_the_work_area!(strategy_role: strategy_role)
-        end
-
-        private
-
-        # TODO: Extract associate user to role and grant permission to these actions for the state
-
-        def associate_authenticated_user_with_work_area!
-          strategy_role = Models::Processing::StrategyRole.find_or_create_by!(role: work_area_viewer_role, strategy: processing_strategy)
-          work_area_viewers.each do |viewer|
-            Models::Processing::EntitySpecificResponsibility.find_or_create_by!(
-              strategy_role: strategy_role,
-              entity: work_area.processing_entity,
-              actor: viewer
-            )
-          end
-          strategy_role
-        end
-
-        PERMITTED_WORK_AREA_VIEWER_ACTIONS = ['show'].freeze
-        def allow_authenticated_user_to_view_the_work_area!(strategy_role:)
-          PERMITTED_WORK_AREA_VIEWER_ACTIONS.each do |action_name|
-            strategy_action = Models::Processing::StrategyAction.find_or_create_by!(
-              strategy: processing_strategy, name: action_name, allow_repeat_within_current_state: true
-            )
-            state_action = Models::Processing::StrategyStateAction.find_or_create_by!(
-              strategy_action: strategy_action, originating_strategy_state: processing_strategy.initial_strategy_state
-            )
-            Models::Processing::StrategyStateActionPermission.find_or_create_by!(
-              strategy_role: strategy_role, strategy_state_action: state_action
-            )
-          end
+          PermissionGenerator.call(
+            actors: work_area_viewers,
+            role: work_area_viewer_role,
+            action_names: PERMITTED_WORK_AREA_VIEWER_ACTIONS,
+            entity: work_area,
+            strategy: processing_strategy,
+            strategy_state: processing_strategy.initial_strategy_state
+          )
         end
       end
     end
