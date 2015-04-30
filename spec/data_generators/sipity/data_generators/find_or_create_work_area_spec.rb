6d24692a7bbb9fb5c5075e5f809a47f406f33347
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-30 12:17:44 -0400

    Refactoring work area manager behavior

--- spec/data_generators/sipity/data_generators/find_or_create_work_area_spec.rb
+++ spec/data_generators/sipity/data_generators/find_or_create_work_area_spec.rb
@@ -4,7 +4,9 @@ module Sipity
   module DataGenerators
     RSpec.describe FindOrCreateWorkArea do
       let(:user) { Sipity::Factories.create_user }
-
+      before do
+        allow(Etd::WorkAreaProcessingGenerator).to receive(:call)
+      end
       it 'will create a processing strategy if none exists for work areas otherwise reuse it' do
         expect { described_class.call(name: 'Worm', slug: 'worm') }.
           to change { Models::Processing::Strategy.count }.by(1)
@@ -28,41 +30,21 @@ module Sipity
         end.to change { Models::Processing::StrategyUsage.count }.by(2)
       end
 
-      it 'will grant permission specific permission but not general permission' do
-        work_area = described_class.call(name: 'Worm', slug: 'worm', work_area_managers: user)
-
-        expect(
-          Models::Processing::EntitySpecificResponsibility.where(
-            entity: work_area.processing_entity,
-            actor: Conversions::ConvertToProcessingActor.call(user)
-          ).count
-        ).to eq(1)
-
-        expect(Models::Processing::StrategyResponsibility.count).to eq(0)
-      end
-
       it 'will grant permission to show the work area if a work_area_manager is given' do
         another_work_area = described_class.call(name: 'Another', slug: 'another')
         work_area = described_class.call(name: 'Worm', slug: 'worm', work_area_managers: user)
 
-        permission_to_show_work_area = Policies::Processing::ProcessingEntityPolicy.call(
-          user: user, entity: work_area, action_to_authorize: 'show'
-        )
-        expect(permission_to_show_work_area).to be_truthy
+        described_class::PERMITTED_WORK_MANAGER_ACTIONS.each do |action_name|
+          permission_to_work_area_action = Policies::Processing::ProcessingEntityPolicy.call(
+            user: user, entity: work_area, action_to_authorize: action_name
+          )
+          expect(permission_to_work_area_action).to be_truthy
 
-        permission_to_show_another_work_area = Policies::Processing::ProcessingEntityPolicy.call(
-          user: user, entity: another_work_area, action_to_authorize: 'show'
-        )
-        expect(permission_to_show_another_work_area).to be_falsey
-      end
-
-      it 'will grant permission to create a submission window (via the submission window form)' do
-        work_area = described_class.call(name: 'Worm', slug: 'worm', work_area_managers: user)
-
-        permission_to_create_a_submission_window = Policies::Processing::ProcessingEntityPolicy.call(
-          user: user, entity: work_area, action_to_authorize: 'create_submission_window'
-        )
-        expect(permission_to_create_a_submission_window).to be_truthy
+          permission_to_other_work_area_action = Policies::Processing::ProcessingEntityPolicy.call(
+            user: user, entity: another_work_area, action_to_authorize: 'show'
+          )
+          expect(permission_to_other_work_area_action).to be_falsey
+        end
       end
     end
   end
