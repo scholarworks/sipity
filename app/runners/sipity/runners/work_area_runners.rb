d3c5c9374fba70f0dca63b4cc0f7ff2690831f36
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-04-30 11:47:21 -0400

    Adding basic runner for submission window
    This is a rudimentary runner, but I believe it captures what we are
    after.

--- app/runners/sipity/runners/work_area_runners.rb
+++ app/runners/sipity/runners/work_area_runners.rb
@@ -14,6 +14,20 @@ module Sipity
           end
         end
       end
+
+      # Responsible for responding with a submission window
+      class SubmissionWindow < BaseRunner
+        self.authentication_layer = :default
+        self.authorization_layer = :default
+        self.action_name = :show?
+
+        def run(work_area_slug:, submission_window_slug:)
+          submission_window = repository.find_submission_window_by(slug: submission_window_slug, work_area: work_area_slug)
+          authorization_layer.enforce!(action_name => submission_window) do
+            callback(:success, submission_window)
+          end
+        end
+      end
     end
   end
 end
--- spec/runners/sipity/runners/work_area_runners_spec.rb
+++ spec/runners/sipity/runners/work_area_runners_spec.rb
@@ -31,6 +31,33 @@ module Sipity
           expect(response).to eq([:success, work_area])
         end
       end
+
+      RSpec.describe SubmissionWindow do
+        let(:submission_window) { double('Submission Window') }
+        let(:user) { double('User') }
+        let(:context) { TestRunnerContext.new(find_submission_window_by: submission_window, current_user: user) }
+        let(:handler) { double(invoked: true) }
+
+        subject do
+          described_class.new(context, authentication_layer: false, authorization_layer: false) do |on|
+            on.success { |a| handler.invoked("SUCCESS", a) }
+          end
+        end
+
+        it 'will require authentication by default' do
+          expect(described_class.authentication_layer).to eq(:default)
+        end
+
+        it 'enforces authorization' do
+          expect(described_class.authorization_layer).to eq(:default)
+        end
+
+        it 'issues the :success callback' do
+          response = subject.run(work_area_slug: 'a_work_area', submission_window_slug: 'a_submission_window')
+          expect(handler).to have_received(:invoked).with("SUCCESS", submission_window)
+          expect(response).to eq([:success, submission_window])
+        end
+      end
     end
   end
 end
