ecb3f33033fdf945887a96f23714d5e2f0b49f7c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-01 14:19:30 -0400

    Adding documentation concerning spec behavior

--- spec/forms/sipity/forms/work_area_forms_spec.rb
+++ spec/forms/sipity/forms/work_area_forms_spec.rb
@@ -5,6 +5,10 @@ module Sipity
     RSpec.describe WorkAreaForms do
       subject { described_class }
       it { should respond_to :build_the_form }
+
+      context '#build_the_form' do
+        xit 'will use the work area and action name to find the correct object'
+      end
     end
   end
 end
