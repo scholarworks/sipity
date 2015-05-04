e45183cd0b4262c4a36b834f1326ab8117ca4aaa
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-04 14:53:36 -0400

    Working on SubmissionWindowPresenter

--- app/presenters/sipity/controllers/submission_window_presenter.rb
+++ app/presenters/sipity/controllers/submission_window_presenter.rb
@@ -4,7 +4,22 @@ module Sipity
     class SubmissionWindowPresenter < Curly::Presenter
       presents :submission_window
 
-      delegate :slug, to: :@submission_window, prefix: :submission_window
+      delegate :work_area, :slug, to: :submission_window
+
+      attr_reader :submission_window
+      private :submission_window, :work_area
+
+      def link
+        link_to(slug, path)
+      end
+
+      def path
+        submission_window_for_work_area_path(work_area_slug: work_area_slug, submission_window_slug: slug)
+      end
+
+      def work_area_slug
+        work_area.slug
+      end
     end
   end
 end
--- spec/presenters/sipity/controllers/submission_window_presenter_spec.rb
+++ spec/presenters/sipity/controllers/submission_window_presenter_spec.rb
@@ -3,10 +3,15 @@ module Sipity
     RSpec.describe SubmissionWindowPresenter, type: :presenter do
       let(:context) { PresenterHelper::Context.new(submission_window: submission_window, current_user: current_user) }
       let(:current_user) { double('Current User') }
-      let(:submission_window) { Models::SubmissionWindow.new(slug: 'the-slug') }
+      let(:submission_window) { Models::SubmissionWindow.new(slug: 'the-slug', work_area: work_area) }
+      let(:work_area) { Models::WorkArea.new(slug: 'work-area') }
       let(:repository) { QueryRepositoryInterface.new }
       subject { described_class.new(context, submission_window: submission_window, repository: repository) }
-      its(:submission_window_slug) { should eq(submission_window.slug) }
+
+      its(:slug) { should eq(submission_window.slug) }
+      its(:work_area_slug) { should eq(work_area.slug) }
+      its(:path) { should eq("/areas/#{work_area.slug}/#{submission_window.slug}") }
+      its(:link) { should eq(%(<a href="/areas/#{work_area.slug}/#{submission_window.slug}">the-slug</a>)) }
     end
   end
 end
