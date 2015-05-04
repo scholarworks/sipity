b3d0e0f5df7049e170769ded6e4f60c816d171d1
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-04 15:53:57 -0400

    Refactoring to use base class
    Instead of all of the logic in the specific case, instead prefer the
    base class as the container of the information.

--- app/presenters/sipity/controllers/work_areas/resourceful_action_presenter.rb
+++ app/presenters/sipity/controllers/work_areas/resourceful_action_presenter.rb
@@ -3,75 +3,13 @@ module Sipity
     module WorkAreas
       # Responsible for rendering a resourceful action in the context of a
       # WorkArea.
-      class ResourcefulActionPresenter < Curly::Presenter
-        presents :resourceful_action
+      class ResourcefulActionPresenter < Sipity::Controllers::ResourcefulActionPresenter
+        presents :work_area
 
-        delegate :name, to: :@resourceful_action, prefix: :action
-
-        def render_entry_point
-          content_tag('div', itemprop: 'target', itemscope: true, itemtype: "http://schema.org/EntryPoint", class: "action") do
-            if available?
-              render_available_inner_html_entry_point
-            else
-              render_unavailable_inner_html_for_entry_point
-            end
-          end
-        end
+        delegate :slug, to: :work_area, prefix: :work_area
 
         def path
           work_area_query_action_path(work_area_slug: work_area_slug, query_action_name: action_name)
-        end
-
-        def action_availability_state
-          STATE_AVAILABLE
-        end
-
-        private
-
-        delegate :slug, to: :view_object, prefix: :work_area
-
-        DESTROY_ACTION_NAME = 'destroy'.freeze
-        STATE_AVAILABLE = 'available'
-
-        def available?
-          action_availability_state == STATE_AVAILABLE
-        end
-
-        def render_available_inner_html_entry_point
-          link_to(entry_point_text, path, entry_point_attributes)
-        end
-
-        def render_unavailable_inner_html_for_entry_point
-          (
-            content_tag('meta', '', itemprop: 'name', content: action_name) +
-            content_tag('span', class: 'btn btn-default disabled') do
-              I18n.t("sipity/works.resourceful_actions.label.#{ action_name }")
-            end
-          ).html_safe
-        end
-
-        def entry_point_attributes
-          attributes = { itemprop: 'url', class: "btn #{button_class}" }
-          if action_name == DESTROY_ACTION_NAME
-            attributes[:data] = { confirm: I18n.t('sipity/decorators/resourceful_actions.confirm.destroy') }
-            attributes[:method] = :delete
-            attributes[:rel] = 'nofollow'
-          end
-          attributes
-        end
-
-        def entry_point_text
-          text = I18n.t("sipity/decorators/resourceful_actions.label.#{ action_name }")
-          text << %(<meta itemprop="name" content="#{action_name}" />)
-          text.html_safe
-        end
-
-        def button_class
-          dangerous? ? 'btn-danger' : 'btn-primary'
-        end
-
-        def dangerous?
-          action_name.to_s == DESTROY_ACTION_NAME
         end
       end
     end
--- spec/presenters/sipity/controllers/work_areas/resourceful_action_presenter_spec.rb
+++ spec/presenters/sipity/controllers/work_areas/resourceful_action_presenter_spec.rb
@@ -5,44 +5,18 @@ module Sipity
     module WorkAreas
       RSpec.describe ResourcefulActionPresenter, type: :presenter do
         let(:context) do
-          PresenterHelper::Context.new(view_object: view_object, current_user: current_user, resourceful_action: resourceful_action)
+          PresenterHelper::Context.new(work_area: work_area, current_user: current_user, resourceful_action: resourceful_action)
         end
         let(:current_user) { double('Current User') }
-        let(:view_object) { Models::WorkArea.new(slug: 'the-slug') }
+        let(:work_area) { Models::WorkArea.new(slug: 'the-slug') }
         let(:resourceful_action) { Models::Processing::StrategyAction.new(name: 'show') }
-        subject { described_class.new(context, resourceful_action: resourceful_action, view_object: view_object) }
+        subject { described_class.new(context, resourceful_action: resourceful_action, work_area: work_area) }
 
         it "will have a path based on the work area query action" do
-          expect(subject.path).to eq("/areas/#{view_object.slug}/do/#{resourceful_action.name}")
+          expect(subject.path).to eq("/areas/#{work_area.slug}/do/#{resourceful_action.name}")
         end
 
-        context '#render_entry_point' do
-          it 'will render an entry point with data-method="delete" for :destroy action' do
-            resourceful_action = double(name: 'destroy')
-            subject = described_class.new(context, resourceful_action: resourceful_action, view_object: view_object)
-            expect(subject.render_entry_point).to have_tag('.action[itemprop="target"][itemtype="http://schema.org/EntryPoint"]') do
-              with_tag("a[data-method='delete'][href='#{subject.path}']")
-            end
-          end
-
-          it 'will render an entry point link for an available action' do
-            resourceful_action = double(name: 'edit')
-            subject = described_class.new(context, resourceful_action: resourceful_action, view_object: view_object)
-            expect(subject.render_entry_point).to have_tag('.action[itemprop="target"][itemtype="http://schema.org/EntryPoint"]') do
-              with_tag("a[href='#{subject.path}']")
-            end
-          end
-
-          it 'will not render an entry point link when an action is not avaialble' do
-            resourceful_action = double(name: 'edit')
-            subject = described_class.new(context, resourceful_action: resourceful_action, view_object: view_object)
-            expect(subject).to receive(:available?).and_return(false)
-            expect(subject.render_entry_point).to have_tag('.action[itemprop="target"][itemtype="http://schema.org/EntryPoint"]') do
-              with_tag("meta[itemprop='name'][content='#{resourceful_action.name}']")
-              without_tag("a[href='#{subject.path}']")
-            end
-          end
-        end
+        its(:work_area_slug) { should eq(work_area.slug) }
       end
     end
   end
