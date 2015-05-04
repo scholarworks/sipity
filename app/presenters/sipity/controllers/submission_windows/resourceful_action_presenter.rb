d89f615674ea964e011b320932229995b13fd51c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-04 15:55:52 -0400

    Adding ResourcefulAction for SubmissionWindow

---
+++ app/presenters/sipity/controllers/submission_windows/resourceful_action_presenter.rb
@@ -0,0 +1,19 @@
+module Sipity
+  module Controllers
+    module SubmissionWindows
+      # Responsible for rendering a resourceful action in the context of a
+      # WorkArea.
+      class ResourcefulActionPresenter < Sipity::Controllers::ResourcefulActionPresenter
+        presents :submission_window
+        delegate :slug, to: :@submission_window, prefix: :submission_window
+        delegate :work_area_slug, to: :@submission_window
+
+        def path
+          submission_window_query_action_path(
+            work_area_slug: work_area_slug, submission_window_slug: submission_window_slug, query_action_name: action_name
+          )
+        end
+      end
+    end
+  end
+end
---
+++ spec/presenters/sipity/controllers/submission_windows/resourceful_action_presenter_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+module Sipity
+  module Controllers
+    module SubmissionWindows
+      RSpec.describe ResourcefulActionPresenter, type: :presenter do
+        let(:context) do
+          PresenterHelper::Context.new(
+            submission_window: submission_window, current_user: current_user, resourceful_action: resourceful_action
+          )
+        end
+        let(:current_user) { double('Current User') }
+        let(:work_area) { Models::WorkArea.new(slug: 'the-slug') }
+        let(:submission_window) { Models::SubmissionWindow.new(slug: 'the-slug', work_area: work_area) }
+        let(:resourceful_action) { Models::Processing::StrategyAction.new(name: 'show') }
+        subject { described_class.new(context, resourceful_action: resourceful_action, submission_window: submission_window) }
+
+        it "will have a path based on the work area query action" do
+          expect(subject.path).to eq("/areas/#{submission_window.work_area_slug}/#{submission_window.slug}/do/#{resourceful_action.name}")
+        end
+
+        its(:work_area_slug) { should eq(submission_window.work_area_slug) }
+        its(:submission_window_slug) { should eq(submission_window.slug) }
+      end
+    end
+  end
+end
