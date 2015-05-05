fca432ce5c825fd89825507c03ddf1c5f6c8f1db
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-05 13:38:22 -0400

    Exposing rudimentary submission window actions
    These are duplicates of the work area and can be composed into a
    common object.

--- spec/controllers/sipity/controllers/submission_windows_controller_spec.rb
+++ spec/controllers/sipity/controllers/submission_windows_controller_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 module Sipity
   module Controllers
     RSpec.describe SubmissionWindowsController, type: :controller do
-      let(:submission_window) { Models::WorkArea.new(slug: 'window') }
+      let(:submission_window) { Models::SubmissionWindow.new(slug: 'start', work_area: work_area) }
+      let(:work_area) { Models::WorkArea.new(slug: 'etd') }
       let(:status) { :success }
       # REVIEW: It is possible the runner will return a well formed object
       let(:runner) { double('Runner', run: [status, submission_window]) }
@@ -16,7 +17,65 @@ module Sipity
         before { controller.runner = runner }
         it 'will pass along to the response handler' do
           expect_any_instance_of(ResponseHandlers::SubmissionWindowHandler::SuccessResponse).to receive(:respond).and_call_original
-          get 'show', work_area_slug: 'work-area', submission_window_slug: submission_window.slug
+          get 'show', work_area_slug: work_area.slug, submission_window_slug: submission_window.slug
+
+          expect(controller.view_object).to be_present
+        end
+      end
+
+      context 'GET #query_action' do
+        before { controller.runner = runner }
+        let(:query_action_name) { 'fun_things' }
+        it 'will pass along to the response handler' do
+          expect_any_instance_of(Sipity::ResponseHandlers::SubmissionWindowHandler::SuccessResponse).to receive(:respond).and_call_original
+
+          # I don't want to mess around with all the possible actions
+          expect do
+            get(
+              'query_action',
+              work_area_slug: work_area.slug,
+              submission_window_slug: submission_window.slug,
+              query_action_name: query_action_name,
+              submission_window: { title: 'Hello' }
+            )
+          end.to raise_error(ActionView::MissingTemplate, %r{sipity/controllers/submission_windows/#{query_action_name}})
+
+          expect(runner).to have_received(:run).with(
+            Sipity::Controllers::SubmissionWindowsController,
+            work_area_slug: work_area.slug,
+            submission_window_slug: submission_window.slug,
+            processing_action_name: query_action_name,
+            attributes: { 'title' => 'Hello' }
+          )
+
+          expect(controller.view_object).to be_present
+        end
+      end
+
+      context 'POST #command_action' do
+        before { controller.runner = runner }
+        let(:command_action_name) { 'fun_things' }
+        it 'will pass along to the response handler' do
+          expect_any_instance_of(Sipity::ResponseHandlers::SubmissionWindowHandler::SuccessResponse).to receive(:respond).and_call_original
+
+          # I don't want to mess around with all the possible actions
+          expect do
+            post(
+              'command_action',
+              work_area_slug: work_area.slug,
+              submission_window_slug: submission_window.slug,
+              command_action_name: command_action_name,
+              submission_window: { title: 'Hello' }
+            )
+          end.to raise_error(ActionView::MissingTemplate, %r{sipity/controllers/submission_windows/#{command_action_name}})
+
+          expect(runner).to have_received(:run).with(
+            Sipity::Controllers::SubmissionWindowsController,
+            work_area_slug: work_area.slug,
+            submission_window_slug: submission_window.slug,
+            processing_action_name: command_action_name,
+            attributes: { 'title' => 'Hello' }
+          )
 
           expect(controller.view_object).to be_present
         end
