17f847a9b912895a9104e7bafacdc6f3db6a9453
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-05 10:33:40 -0400

    Rendering the submission window show page
    In the case of ETDs this is analogous to the start page of the
    controller.
    
    Huzzah for favoring composition over inheritance and for breaking
    apart the Action from the controller end point.

--- app/presenters/sipity/controllers/submission_windows/show_presenter.rb
+++ app/presenters/sipity/controllers/submission_windows/show_presenter.rb
@@ -3,6 +3,31 @@ module Sipity
     module SubmissionWindows
       # Responsible for presenting a work area
       class ShowPresenter < SubmissionWindowPresenter
+        def render_submission_window
+          # HACK: Oh boy is this ugly, but it delivers what I am after.
+          # It also draws attention to the new rendering that I'm after
+          slug_to_method_name_suffix = PowerConverter.convert_to_safe_for_method_name(work_area_slug)
+          send("render_submission_window_for_#{slug_to_method_name_suffix}")
+        end
+
+        private
+
+        def render_submission_window_for_etd
+          deprecated_render_submission_window_for_etd
+        end
+
+        def deprecated_render_submission_window_for_etd
+          # This is a whole lot of ugly. For now, its a nasty method that sucks.
+          # Its purpose is to duplicate the existing /works/new functionality.
+          # I do not want to continue carrying this behavior forward; It is
+          # instead something that should be better accounted for when rendering
+          # the submission window
+          controller = @_context.controller
+          _status, form = Runners::WorkRunners::New.run(controller, attributes: {})
+          decorated_form = Decorators::WorkDecorator.decorate(form)
+          render template: 'sipity/controllers/works/new', locals: { model: decorated_form }
+        end
+        deprecate :deprecated_render_submission_window_for_etd
       end
     end
   end
--- spec/presenters/sipity/controllers/submission_windows/show_presenter_spec.rb
+++ spec/presenters/sipity/controllers/submission_windows/show_presenter_spec.rb
@@ -4,12 +4,34 @@ module Sipity
   module Controllers
     module SubmissionWindows
       RSpec.describe ShowPresenter do
-        let(:context) { PresenterHelper::Context.new(submission_window: submission_window, current_user: current_user) }
+        let(:context) { PresenterHelper::Context.new(current_user: current_user) }
         let(:current_user) { double('Current User') }
-        let(:submission_window) { double(slug: 'the-slug', work_area: work_area) }
+        let(:submission_window) { double(slug: 'the-slug', work_area: work_area, work_area_slug: work_area.slug) }
         let(:work_area) { double(slug: 'work-area') }
         subject { described_class.new(context, submission_window: submission_window) }
         it { should be_a SubmissionWindowPresenter }
+
+        context '#render_submission_window' do
+          context 'for an ETD' do
+            let(:controller) { double('Controller') }
+            let(:context) { PresenterHelper::Context.new(current_user: current_user, controller: controller) }
+            let(:work_area) { double(slug: 'etd') }
+            let(:etd_form) { double }
+            let(:decorated_form) { double }
+
+            it 'will render the current behavior of the ETD submissions' do
+              expect(Runners::WorkRunners::New).to receive(:run).
+                with(controller, attributes: {}).and_return([:status, etd_form])
+              expect(Decorators::WorkDecorator).to receive(:decorate).
+                with(etd_form).
+                and_return(decorated_form)
+              expect(subject).to receive(:render).
+                with(template: 'sipity/controllers/works/new', locals: { model: decorated_form }).
+                and_return(decorated_form)
+              subject.render_submission_window
+            end
+          end
+        end
       end
     end
   end
