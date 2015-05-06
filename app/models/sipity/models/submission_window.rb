3124814f0dae0550cb3b5a05e18c954cc8109b40
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-06 17:07:39 -0400

    Adding SubmissionWindow#work_area_partial_prefix
    I want to have access to this method without undo antics.

--- app/models/sipity/models/submission_window.rb
+++ app/models/sipity/models/submission_window.rb
@@ -5,7 +5,7 @@ module Sipity
       self.table_name = 'sipity_submission_windows'
 
       belongs_to :work_area
-      delegate :slug, to: :work_area, prefix: :work_area
+      delegate :slug, :partial_suffix, to: :work_area, prefix: :work_area
 
       has_many :submission_window_work_types, dependent: :destroy
       has_many :work_submissions, dependent: :destroy
