6dcba1d026de3d8cbe2368cd57164f34eb84526a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-06 13:16:49 -0400

    Expanding what is found for a given work
    I'm often going to need the processing entity, work_area, and
    submission window when dealing with a work.

--- app/repositories/sipity/queries/work_queries.rb
+++ app/repositories/sipity/queries/work_queries.rb
@@ -4,7 +4,7 @@ module Sipity
     module WorkQueries
       BASE_HEADER_ATTRIBUTES = [:title, :work_publication_strategy].freeze
       def find_work(work_id)
-        Models::Work.find(work_id)
+        Models::Work.includes(:processing_entity, work_submission: [:work_area, :submission_window]).find(work_id)
       end
 
       def build_dashboard_view(user:, filter: {})
