8c7b1434fc8446af09a540991d1f326ae7525909
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-06 09:19:37 -0400

    Adding general case for Job.submit

--- spec/jobs/sipity/jobs_spec.rb
+++ spec/jobs/sipity/jobs_spec.rb
@@ -12,6 +12,14 @@ module Sipity
       it 'will raise a Name Error if the job does not exist' do
         expect { described_class.submit('i_dont_want_to_work') }.to raise_error(Exceptions::JobNotFoundError)
       end
+
+      it 'will submit the found job' do
+        a_job_object = double(submit: true)
+        allow(described_class).to receive(:find_job_by_name).with('a_job_name').and_return(a_job_object)
+        described_class.submit('a_job_name', hello: :world)
+
+        expect(a_job_object).to have_received(:submit).with(hello: :world)
+      end
     end
   end
 end
