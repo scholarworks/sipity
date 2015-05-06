49b0219f53021b36621ecddd2ab2677482afd81d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-06 12:22:21 -0400

    Creating a work associates with submission window
    At present the :submission_window is an optional parameter. If none is
    provided things default to the etd/start
    `work_area/submission_window`.
    
    At a future point, as we splice in the ULRA forms, this default will
    be removed. See #617 for reference to that work.

--- app/services/sipity/services/create_work_service.rb
+++ app/services/sipity/services/create_work_service.rb
@@ -9,11 +9,13 @@ module Sipity
       def initialize(pid_minter: default_pid_minter, repository: default_repository, **attributes)
         self.pid_minter = pid_minter
         self.repository = repository
+        self.submission_window = attributes.delete(:submission_window) { default_submission_window }
         self.attributes = attributes
       end
 
       def call
         create_the_work!
+        associated_the_work_and_submission_window!
         create_the_work_processing_entity!
         work
       end
@@ -22,6 +24,12 @@ module Sipity
 
       def create_the_work!
         @work = Models::Work.create!(attributes)
+      end
+
+      def associated_the_work_and_submission_window!
+        Models::WorkSubmission.create!(
+          work_id: work.id, submission_window_id: submission_window.id, work_area_id: submission_window.work_area_id
+        )
       end
 
       def create_the_work_processing_entity!
@@ -33,7 +41,7 @@ module Sipity
         end
       end
 
-      attr_accessor :pid_minter, :repository, :attributes
+      attr_accessor :pid_minter, :repository, :attributes, :submission_window
       attr_reader :work
 
       def default_repository
@@ -49,6 +57,24 @@ module Sipity
       def default_pid_minter
         Rails.application.config.default_pid_minter
       end
+
+      def default_submission_window
+        # This is a conceit to the tests; It is also indicative of a greater problem.
+        # TODO: Remove the default behavior; Its presumptive and an expensive test.
+        #   Also as we look at things, pay attention to the deprecation warnings.
+        #   Those methods may be doing too much and not operating in isolation.
+        submission_window_slug = Forms::Etd::StartASubmissionForm::DEFAULT_SUBMISSION_WINDOW_SLUG
+        work_area_slug = Forms::Etd::StartASubmissionForm::DEFAULT_WORK_AREA_SLUG
+        Sipity::DataGenerators::FindOrCreateWorkArea.call(name: 'Electronic Thesis and Dissertation', slug: work_area_slug) do |work_area|
+          Sipity::DataGenerators::FindOrCreateSubmissionWindow.call(slug: submission_window_slug, work_area: work_area)
+        end
+        repository.find_submission_window_by(
+          slug: submission_window_slug,
+          work_area: work_area_slug
+        )
+      end
+      # The submission window will be a requirement after ULRA is added.
+      deprecate :default_submission_window
     end
   end
 end
