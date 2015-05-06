3a5aa6185929f7052bc9e9ee1c7df5a90758ddeb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-06 07:52:46 -0400

    Adding missing spec

---
+++ spec/models/sipity/models/notification_spec.rb
@@ -0,0 +1,8 @@
+module Sipity
+  module Models
+    RSpec.describe Notification do
+      subject { described_class }
+      its(:table_name_prefix) { should eq('sipity_notification_') }
+    end
+  end
+end
