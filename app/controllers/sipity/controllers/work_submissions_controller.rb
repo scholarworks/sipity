e79c62efc8ab381eab151c67b7d19a5fea7cba24
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-07 11:16:27 -0400

    Adding a WorkSubmissionController
    This is a preliminary commit loaded with stop gap solutions.

---
+++ app/controllers/sipity/controllers/work_submissions_controller.rb
@@ -0,0 +1,73 @@
+module Sipity
+  module Controllers
+    # The controller for creating works.
+    class WorkSubmissionsController < ApplicationController
+      class_attribute :response_handler_container
+      # TODO: Review this
+      self.runner_container = Sipity::Runners::WorkRunners
+      # TODO: This is a stop-gap
+      self.response_handler_container = Sipity::ResponseHandlers::WorkAreaHandler
+
+      def query_action
+        runner_response = run(
+          work_id: work_id,
+          processing_action_name: query_action_name,
+          attributes: query_or_command_attributes
+        )
+
+        # I could use action instead of template, but I feel the explicit path
+        # for template is better than the implicit pathing of :action
+        handle_response(runner_response, template: "sipity/controllers/work_submissions/#{query_action_name}")
+      end
+
+      def command_action
+        runner_response = run(
+          work_id: work_id,
+          processing_action_name: command_action_name,
+          attributes: query_or_command_attributes
+        )
+
+        # I could use action instead of template, but I feel the explicit path
+        # for template is better than the implicit pathing of :action
+        handle_response(runner_response, template: "sipity/controllers/work_submissions/#{command_action_name}")
+      end
+
+      attr_accessor :view_object
+      helper_method :view_object
+
+      private
+
+      def work_id
+        params.require(:work_id)
+      end
+
+      def query_action_name
+        params.require(:query_action_name)
+      end
+
+      def command_action_name
+        params.require(:command_action_name)
+      end
+
+      def query_or_command_attributes
+        params.fetch(:work) { HashWithIndifferentAccess.new }
+      end
+
+      def handle_response(handled_response, template:)
+        Sipity::ResponseHandlers.handle_response(
+          context: self,
+          handled_response: handled_response,
+          container: response_handler_container,
+          template: template
+        )
+      end
+
+      def run(*args)
+        # TODO: This is an intermediary step that will be wrapped into the
+        #   existing #run method; However it should be considered experimental
+        status, object = super(*args)
+        Parameters::HandledResponseParameter.new(status: status, object: object)
+      end
+    end
+  end
+end
---
+++ spec/controllers/sipity/controllers/work_submissions_controller_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+module Sipity
+  module Controllers
+    RSpec.describe WorkSubmissionsController, type: :controller do
+      let(:work) { Models::Work.new(id: 'abc') }
+      let(:status) { :success }
+      # REVIEW: It is possible the runner will return a well formed object
+      let(:runner) { double('Runner', run: [status, work]) }
+
+      context 'configuration' do
+        its(:runner_container) { should eq(Sipity::Runners::WorkRunners) }
+        its(:response_handler_container) { should eq(Sipity::ResponseHandlers::WorkAreaHandler) }
+      end
+
+      context 'GET #query_action' do
+        before { controller.runner = runner }
+        let(:query_action_name) { 'fun_things' }
+        it 'will pass along to the response handler' do
+          expect_any_instance_of(Sipity::ResponseHandlers::WorkAreaHandler::SuccessResponse).to receive(:respond).and_call_original
+
+          # I don't want to mess around with all the possible actions
+          expect do
+            get 'query_action', work_id: work.id, query_action_name: query_action_name, work: { title: 'Hello' }
+          end.to raise_error(ActionView::MissingTemplate, %r{sipity/controllers/work_submissions/#{query_action_name}})
+
+          expect(runner).to have_received(:run).with(
+            described_class,
+            work_id: work.id, processing_action_name: query_action_name, attributes: { 'title' => 'Hello' }
+          )
+
+          expect(controller.view_object).to be_present
+        end
+      end
+
+      context 'POST #command_action' do
+        before { controller.runner = runner }
+        let(:command_action_name) { 'fun_things' }
+        it 'will pass along to the response handler' do
+          expect_any_instance_of(Sipity::ResponseHandlers::WorkAreaHandler::SuccessResponse).to receive(:respond).and_call_original
+
+          # I don't want to mess around with all the possible actions
+          expect do
+            post 'command_action', work_id: work.id, command_action_name: command_action_name, work: { title: 'Hello' }
+          end.to raise_error(ActionView::MissingTemplate, %r{sipity/controllers/work_submissions/#{command_action_name}})
+
+          expect(runner).to have_received(:run).with(
+            described_class,
+            work_id: work.id, processing_action_name: command_action_name, attributes: { 'title' => 'Hello' }
+          )
+
+          expect(controller.view_object).to be_present
+        end
+      end
+    end
+  end
+end
