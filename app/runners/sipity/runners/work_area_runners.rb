b3fa411a6efefa4f56a1eb5b8cea92ce23c838d0
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-07 09:46:26 -0400

    Normalzing WorkAreaRunners

--- app/runners/sipity/runners/work_area_runners.rb
+++ app/runners/sipity/runners/work_area_runners.rb
@@ -1,49 +1,41 @@
 module Sipity
   module Runners
     module WorkAreaRunners
-      # Responsible for responding with a work area
-      class Show < BaseRunner
+      # :nodoc:
+      class CommandQueryAction < BaseRunner
         self.authentication_layer = :default
         self.authorization_layer = :default
-        self.action_name = :show?
 
-        def run(work_area_slug:)
+        def run(work_area_slug:, processing_action_name:, attributes: {})
           work_area = repository.find_work_area_by(slug: work_area_slug)
-          authorization_layer.enforce!(action_name => work_area) do
-            callback(:success, work_area)
+          form = repository.build_work_area_processing_action_form(
+            work_area: work_area, processing_action_name: processing_action_name, attributes: attributes
+          )
+          authorization_layer.enforce!(processing_action_name => form) do
+            yield(form, work_area)
           end
         end
       end
 
       # The general handler for general query actions (show may be a customized
       # case).
-      class QueryAction < BaseRunner
-        self.authentication_layer = :default
-        self.authorization_layer = :default
-
+      class QueryAction < CommandQueryAction
         def run(work_area_slug:, processing_action_name:, attributes: {})
-          work_area = repository.find_work_area_by(slug: work_area_slug)
-          form = repository.build_work_area_processing_action_form(
-            work_area: work_area, processing_action_name: processing_action_name, attributes: attributes
-          )
-          authorization_layer.enforce!(processing_action_name => form) do
+          super do |form, _work_area|
             callback(:success, form)
           end
         end
       end
 
       # The general handler for all command actions
-      class CommandAction < BaseRunner
-        self.authentication_layer = :default
-        self.authorization_layer = :default
-
+      class CommandAction < CommandQueryAction
         def run(work_area_slug:, processing_action_name:, attributes: {})
-          work_area = repository.find_work_area_by(slug: work_area_slug)
-          form = repository.build_work_area_processing_action_form(
-            work_area: work_area, processing_action_name: processing_action_name, attributes: attributes
-          )
-          authorization_layer.enforce!(processing_action_name => form) do
-            form.submit(requested_by: current_user) ? callback(:success, work_area) : callback(:failure, form)
+          super do |form, work_area|
+            if form.submit(requested_by: current_user)
+              callback(:success, work_area)
+            else
+              callback(:failure, form)
+            end
           end
         end
       end
