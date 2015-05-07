ddd440e564e2e03b782673d8fe85c2480b557a05
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-06 20:10:58 -0400

    Adding SubmissionWindow::Command and QueryAction

--- app/runners/sipity/runners/submission_window_runners.rb
+++ app/runners/sipity/runners/submission_window_runners.rb
@@ -15,9 +15,8 @@ module Sipity
         end
       end
 
-      # The general handler for general query actions (show may be a customized
-      # case).
-      class QueryAction < BaseRunner
+      # :nodoc:
+      class CommandQueryAction < BaseRunner
         self.authentication_layer = :default
         self.authorization_layer = :default
 
@@ -27,7 +26,31 @@ module Sipity
             submission_window: submission_window, processing_action_name: processing_action_name, attributes: attributes
           )
           authorization_layer.enforce!(processing_action_name => form) do
+            yield(form, submission_window)
+          end
+        end
+      end
+
+      # The general handler for general query actions (show may be a customized
+      # case).
+      class QueryAction < CommandQueryAction
+        def run(**keywords)
+          super do |form, _submission_window|
             callback(:success, form)
+          end
+        end
+      end
+
+      # The general handler for general query actions (show may be a customized
+      # case).
+      class CommandAction < CommandQueryAction
+        def run(**keywords)
+          super do |form, submission_window|
+            if form.submit(requested_by: current_user)
+              callback(:success, submission_window)
+            else
+              callback(:failure, form)
+            end
           end
         end
       end
--- spec/runners/sipity/runners/submission_window_runners_spec.rb
+++ spec/runners/sipity/runners/submission_window_runners_spec.rb
@@ -69,6 +69,54 @@ module Sipity
           expect(response).to eq([:success, form])
         end
       end
+
+      RSpec.describe CommandAction do
+        let(:submission_window) { double('Submission Window') }
+        let(:form) { double('Form') }
+        let(:user) { double('User') }
+        let(:context) do
+          TestRunnerContext.new(
+            find_submission_window_by: submission_window,
+            current_user: user,
+            build_submission_window_processing_action_form: form
+          )
+        end
+        let(:handler) { double(invoked: true) }
+
+        subject do
+          described_class.new(context, authentication_layer: false, authorization_layer: false) do |on|
+            on.success { |a| handler.invoked("SUCCESS", a) }
+            on.failure { |a| handler.invoked("FAILURE", a) }
+          end
+        end
+
+        it 'will require authentication by default' do
+          expect(described_class.authentication_layer).to eq(:default)
+        end
+
+        it 'enforces authorization' do
+          expect(described_class.authorization_layer).to eq(:default)
+        end
+
+        it 'issues the :success callback when form is submitted' do
+          expect(form).to receive(:submit).with(requested_by: user).and_return(true)
+          response = subject.run(
+            work_area_slug: 'a_work_area', submission_window_slug: 'a_submission_window', processing_action_name: 'a_funny'
+          )
+          expect(handler).to have_received(:invoked).with("SUCCESS", submission_window)
+          expect(response).to eq([:success, submission_window])
+        end
+
+        it 'issues the :failure callback when form fails to submit' do
+          expect(form).to receive(:submit).with(requested_by: user).and_return(false)
+          response = subject.run(
+            work_area_slug: 'a_work_area', submission_window_slug: 'a_submission_window', processing_action_name: 'a_funny'
+          )
+          expect(handler).to have_received(:invoked).with("FAILURE", form)
+          expect(response).to eq([:failure, form])
+        end
+      end
+
     end
   end
 end
