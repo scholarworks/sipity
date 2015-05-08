2c2ede07740ec9422640bbd4054cd1295eac0de6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-08 08:30:57 -0400

    Teasing apart the Handler and Responder
    I was leveraging lots of inheritance. So instead I'm opting for
    composition.

--- app/response_handlers/sipity/response_handlers.rb
+++ app/response_handlers/sipity/response_handlers.rb
@@ -22,5 +22,62 @@ module Sipity
     def build_response_handler(container:, handled_response_status:)
       container.qualified_const_get("#{handled_response_status.to_s.classify}Response")
     end
+
+    # The default response handler. It makes sure things are well composed,
+    # guarding the interface of collaborating objects.
+    class DefaultHandler
+      def self.respond(**keywords)
+        new(**keywords).respond
+      end
+
+      attr_reader :context, :handled_response, :template
+      def initialize(context:, handled_response:, template:, responder: default_responder)
+        self.context = context
+        self.handled_response = handled_response
+        self.template = template
+        self.responder = responder
+        prepare_context_for_response
+      end
+
+      def respond
+        responder.call(handler: self)
+      end
+
+      delegate :render, :redirect_to, to: :context
+      delegate :object, to: :handled_response, prefix: :response
+
+      private
+
+      attr_accessor :responder
+      attr_writer :template
+
+      def default_responder
+        -> (handler:) { handler.render(template: handler.template) }
+      end
+
+      def prepare_context_for_response
+        # Wouldn't it be great if we had proper View objects in Rails? Instead
+        # of this crazy copy instance variables from the controller to the
+        # template (aka 'ActionView') layer.
+        context.view_object = handled_response.object
+      end
+
+      def context=(input)
+        guard_interface_expectation!(input, :view_object=, :render, :redirect_to)
+        @context = input
+      end
+
+      def handled_response=(input)
+        guard_interface_expectation!(input, :object)
+        @handled_response = input
+      end
+
+      # TODO: Extract this concept?
+      def guard_interface_expectation!(input, *expectations)
+        expectations.each do |expectation|
+          fail(Exceptions::InterfaceExpectationError, object: input, expectation: expectation) unless input.respond_to?(expectation)
+        end
+      end
+    end
   end
 end
--- spec/response_handlers/sipity/response_handlers_spec.rb
+++ spec/response_handlers/sipity/response_handlers_spec.rb
@@ -30,4 +30,36 @@ module Sipity
       end
     end
   end
+
+  module ResponseHandlers
+    RSpec.describe DefaultHandler do
+      let(:context) { double(render: 'rendered', :view_object= => true, redirect_to: 'redirected_to') }
+      let(:viewable_object) { double }
+      let(:handled_response) { double(object: viewable_object) }
+      subject { described_class.new(context: context, handled_response: handled_response, template: 'show') }
+      it 'will #respond by rendering the context' do
+        expect(subject.respond).to eq(context.render)
+      end
+
+      it 'will .respond by rendering the context' do
+        expect(described_class.respond(context: context, handled_response: handled_response, template: 'show')).to eq(context.render)
+        expect(context).to have_received(:render).with(template: 'show')
+      end
+
+      context 'collaborating objects expected interface' do
+        it '#context must implement #view_object=' do
+          expect { described_class.new(context: double(render: true), handled_response: handled_response, template: 'show') }.
+            to raise_error(Exceptions::InterfaceExpectationError)
+        end
+        it '#context must implement #render' do
+          expect { described_class.new(context: double(:view_object= => true), handled_response: handled_response, template: 'show') }.
+            to raise_error(Exceptions::InterfaceExpectationError)
+        end
+        it '#handled_response must implement #object' do
+          expect { described_class.new(context: context, handled_response: double, template: 'show') }.
+            to raise_error(Exceptions::InterfaceExpectationError)
+        end
+      end
+    end
+  end
 end
