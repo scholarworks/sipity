01a612bd9fe982f8b863ad73aa09abdd56cd8325
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-08 08:52:51 -0400

    Reworking WorkArea response handling
    Removing lots of cruft as the coordination is handled at a higher
    level.

--- spec/response_handlers/sipity/response_handlers/work_area_handler_spec.rb
+++ spec/response_handlers/sipity/response_handlers/work_area_handler_spec.rb
@@ -4,33 +4,12 @@ require 'sipity/response_handlers/work_area_handler'
 module Sipity
   module ResponseHandlers
     module WorkAreaHandler
-      RSpec.describe SuccessResponse do
-        let(:context) { double(render: 'rendered', :view_object= => true) }
-        let(:viewable_object) { double }
-        let(:handled_response) { double(object: viewable_object) }
-        subject { described_class.new(context: context, handled_response: handled_response, template: 'show') }
-        it 'will #respond by rendering the context' do
-          expect(subject.respond).to eq(context.render)
-        end
+      RSpec.describe SuccessResponder do
+        let(:handler) { double(render: 'rendered', template: 'show') }
 
-        it 'will .respond by rendering the context' do
-          expect(described_class.respond(context: context, handled_response: handled_response, template: 'show')).to eq(context.render)
-          expect(context).to have_received(:render).with(template: 'show')
-        end
-
-        context 'collaborating objects expected interface' do
-          it '#context must implement #view_object=' do
-            expect { described_class.new(context: double(render: true), handled_response: handled_response, template: 'show') }.
-              to raise_error(Exceptions::InterfaceExpectationError)
-          end
-          it '#context must implement #render' do
-            expect { described_class.new(context: double(:view_object= => true), handled_response: handled_response, template: 'show') }.
-              to raise_error(Exceptions::InterfaceExpectationError)
-          end
-          it '#handled_response must implement #object' do
-            expect { described_class.new(context: context, handled_response: double, template: 'show') }.
-              to raise_error(Exceptions::InterfaceExpectationError)
-          end
+        it 'will coordinate the rendering of the template' do
+          described_class.call(handler: handler)
+          expect(handler).to have_received(:render).with(template: handler.template)
         end
       end
     end
