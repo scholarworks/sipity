62339b1dd3f4d49994801f42dcecefa8534d7519
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-08 09:11:19 -0400

    Adding WorkAreaHandler::SubmitFailureResponder

--- app/response_handlers/sipity/response_handlers/work_area_handler.rb
+++ app/response_handlers/sipity/response_handlers/work_area_handler.rb
@@ -8,6 +8,13 @@ module Sipity
           handler.render(template: handler.template)
         end
       end
+
+      # Forms that fail to submit may have different errors.
+      module SubmitFailureResponder
+        def self.call(handler:)
+          handler.render(template: handler.template, status: :unprocessable_entity)
+        end
+      end
     end
   end
 end
--- spec/response_handlers/sipity/response_handlers/work_area_handler_spec.rb
+++ spec/response_handlers/sipity/response_handlers/work_area_handler_spec.rb
@@ -12,6 +12,15 @@ module Sipity
           expect(handler).to have_received(:render).with(template: handler.template)
         end
       end
+
+      RSpec.describe SubmitFailureResponder do
+        let(:handler) { double(render: 'rendered', template: 'show') }
+
+        it 'will coordinate the rendering of the template' do
+          described_class.call(handler: handler)
+          expect(handler).to have_received(:render).with(template: handler.template, status: :unprocessable_entity)
+        end
+      end
     end
   end
 end
