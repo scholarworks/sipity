fcbdff0258fdf26161275bbb197302d7525535f1
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-08 09:08:24 -0400

    Adding SubmissionWindow responders
    Composition is much more favorable than inheritance
    
    Yes the responder is a nosy neighbor, but the logic is encoded there.

--- spec/response_handlers/sipity/response_handlers/submission_window_handler_spec.rb
+++ spec/response_handlers/sipity/response_handlers/submission_window_handler_spec.rb
@@ -4,104 +4,69 @@ require 'sipity/response_handlers/submission_window_handler'
 module Sipity
   module ResponseHandlers
     module SubmissionWindowHandler
-      RSpec.describe SuccessResponse do
-        let(:context) { double(render: 'rendered', :view_object= => true) }
-        let(:viewable_object) { double }
-        let(:handled_response) { double(object: viewable_object) }
-        subject { described_class.new(context: context, handled_response: handled_response, template: 'show') }
-        it 'will #respond by rendering the context' do
-          expect(subject.respond).to eq(context.render)
-        end
+      RSpec.describe SuccessResponder do
+        let(:handler) { double(render: 'rendered', template: 'show') }
 
-        it 'will .respond by rendering the context' do
-          expect(described_class.respond(context: context, handled_response: handled_response, template: 'show')).to eq(context.render)
-          expect(context).to have_received(:render).with(template: 'show')
-        end
-
-        context 'collaborating objects expected interface' do
-          it '#context must implement #view_object=' do
-            expect { described_class.new(context: double(render: true), handled_response: handled_response, template: 'show') }.
-              to raise_error(Exceptions::InterfaceExpectationError)
-          end
-          it '#context must implement #render' do
-            expect { described_class.new(context: double(:view_object= => true), handled_response: handled_response, template: 'show') }.
-              to raise_error(Exceptions::InterfaceExpectationError)
-          end
-          it '#handled_response must implement #object' do
-            expect { described_class.new(context: context, handled_response: double, template: 'show') }.
-              to raise_error(Exceptions::InterfaceExpectationError)
-          end
+        it 'will coordinate the rendering of the template' do
+          described_class.call(handler: handler)
+          expect(handler).to have_received(:render).with(template: handler.template)
         end
       end
 
-      RSpec.describe SubmitSuccessResponse do
+      RSpec.describe SubmitSuccessResponder do
         let(:submission_window) { Models::SubmissionWindow.new(slug: 'a_slug', work_area: work_area) }
-        let(:viewable_object) { submission_window }
         let(:work_area) { Models::WorkArea.new(slug: 'area_slug') }
-        let(:context) { double(render: :rendered, redirect_to: :redirected_to, :view_object= => true) }
-        let(:handled_response) { double(object: viewable_object) }
-        subject { described_class.new(context: context, handled_response: handled_response, template: 'show') }
-
-        it 'will .respond by delegating to an instance' do
-          expect_any_instance_of(described_class).to receive(:respond)
-          described_class.respond(context: context, handled_response: handled_response, template: 'show')
-        end
+        let(:handler) { double(redirect_to: true, submission_window_path: true, work_submission_path: true) }
+        let(:work) { Models::Work.new(id: 'an_id') }
+        let(:path) { '/redirected_to/this/path' }
 
         context 'for a SubmissionWindow' do
           it "will #respond by redirecting to the submission window's path" do
-            path = '/path/to/submission_window'
-            expect(context).to receive(:submission_window_path).
+            allow(handler).to receive(:response_object).and_return(submission_window)
+            expect(handler).to receive(:submission_window_path).
               with(work_area_slug: work_area.slug, submission_window_slug: submission_window.slug).
               and_return(path)
-            expect(context).to receive(:redirect_to).with(path)
-            subject.respond
+            expect(handler).to receive(:redirect_to).with(path)
+            described_class.call(handler: handler)
           end
         end
 
         context 'for a Work' do
-          let(:viewable_object) { Models::Work.new(id: 'an_id') }
-
-          it "will #respond by redirecting to the submission window's path" do
-            path = '/path/to/work'
-            expect(context).to receive(:work_submission_path).with(work_id: viewable_object.id).and_return(path)
-            expect(context).to receive(:redirect_to).with(path)
-            subject.respond
+          it "will #respond by redirecting to the work's path" do
+            allow(handler).to receive(:response_object).and_return(work)
+            expect(handler).to receive(:work_submission_path).with(work_id: work.id).and_return(path)
+            expect(handler).to receive(:redirect_to).with(path)
+            described_class.call(handler: handler)
           end
         end
 
         context 'for something that can be converted to a submission window' do
           let(:viewable_object) { double(to_submission_window: submission_window) }
           it "will attempt to convert the object" do
-            path = '/path/to/submission_window'
-            expect(context).to receive(:submission_window_path).
+            allow(handler).to receive(:response_object).and_return(viewable_object)
+            expect(handler).to receive(:submission_window_path).
               with(work_area_slug: work_area.slug, submission_window_slug: submission_window.slug).
               and_return(path)
-            expect(context).to receive(:redirect_to).with(path)
-            subject.respond
+            expect(handler).to receive(:redirect_to).with(path)
+            described_class.call(handler: handler)
           end
         end
 
         context 'for something else' do
           let(:viewable_object) { double }
-          it "will attempt to convert the object" do
+          it "will attempt to convert the object but fail" do
+            allow(handler).to receive(:response_object).and_return(viewable_object)
             expect(PowerConverter).to receive(:convert).with(viewable_object, to: :submission_window).and_call_original
-            expect { subject.respond }.to raise_error(PowerConverter::ConversionError)
+            expect { described_class.call(handler: handler) }.to raise_error(PowerConverter::ConversionError)
           end
         end
+      end
 
-        context 'collaborating objects expected interface' do
-          it '#context must implement #view_object=' do
-            expect { described_class.new(context: double(render: true), handled_response: handled_response, template: 'show') }.
-              to raise_error(Exceptions::InterfaceExpectationError)
-          end
-          it '#context must implement #render' do
-            expect { described_class.new(context: double(:view_object= => true), handled_response: handled_response, template: 'show') }.
-              to raise_error(Exceptions::InterfaceExpectationError)
-          end
-          it '#handled_response must implement #object' do
-            expect { described_class.new(context: context, handled_response: double, template: 'show') }.
-              to raise_error(Exceptions::InterfaceExpectationError)
-          end
+      RSpec.describe SubmitFailureResponder do
+        let(:handler) { double(render: 'rendered', template: 'show') }
+        it 'will coordinate the rendering of the template' do
+          described_class.call(handler: handler)
+          expect(handler).to have_received(:render).with(template: handler.template, status: :unprocessable_entity)
         end
       end
     end
