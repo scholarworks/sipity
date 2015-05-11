0684ac1b3719fa905c6ba4f1ee59dbdb6f5c847f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-11 15:07:17 -0400

    Wordsmithing

--- app/forms/sipity/forms/core/work_submissions/show_form.rb
+++ app/forms/sipity/forms/core/work_submissions/show_form.rb
@@ -13,7 +13,8 @@ module Sipity
           def initialize(work:, processing_action_name:, **_keywords)
             self.work = work
             self.processing_action_name = processing_action_name
-            # A concession regard
+            # TODO: Remove dependency on WorkDecorator, this is needed for the
+            # HTML sanitization/rendering.
             super(decorated_work)
           end
 
