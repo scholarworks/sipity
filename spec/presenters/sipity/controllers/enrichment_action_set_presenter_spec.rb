2296e46326e125081d064be2f177c1ab7c24c86f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-11 13:32:46 -0400

    Adding EnrichmentActionSetPresenter
    I want to render a collection of actions by identifier

---
+++ spec/presenters/sipity/controllers/enrichment_action_set_presenter_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+# Because RSpec's described_class is getting confused
+require 'sipity/controllers/enrichment_action_set_presenter'
+
+module Sipity
+  module Controllers
+    RSpec.describe EnrichmentActionSetPresenter, type: :presenter do
+      let(:context) { PresenterHelper::Context.new(current_user: current_user) }
+      let(:current_user) { double('Current User') }
+      let(:enrichment_action_set) { Parameters::ActionSet.new(identifier: 'optional', collection: [double]) }
+      subject { described_class.new(context, enrichment_action_set: enrichment_action_set) }
+
+      its(:enrichment_actions) { should eq(enrichment_action_set.collection) }
+      its(:identifier) { should eq(enrichment_action_set.identifier) }
+    end
+  end
+end
