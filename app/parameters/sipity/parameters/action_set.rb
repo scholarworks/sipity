3d9b4e554ec363951e10a3d81a0ae809a6786eb3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-11 15:05:38 -0400

    Adding additional methods to ActionSet
    I'm exposing Entity as a matter of collaboration. And auto-wrapping
    the collection as an array; That way I can expose other methods for
    the action set to improve its rendering.

--- app/parameters/sipity/parameters/action_set.rb
+++ app/parameters/sipity/parameters/action_set.rb
@@ -2,16 +2,23 @@ module Sipity
   module Parameters
     # Responsible for providing an identified collection of actions.
     class ActionSet
-      def initialize(identifier:, collection:)
+      def initialize(identifier:, collection:, entity:)
         self.identifier = identifier
         self.collection = collection
+        self.entity = entity
       end
 
-      attr_reader :identifier, :collection
+      delegate :any?, :present?, :empty?, to: :collection
+
+      attr_reader :identifier, :collection, :entity
 
       private
 
-      attr_writer :identifier, :collection
+      attr_writer :identifier, :entity
+
+      def collection=(input)
+        @collection = Array.wrap(input)
+      end
     end
   end
 end
--- spec/parameters/sipity/parameters/action_set_spec.rb
+++ spec/parameters/sipity/parameters/action_set_spec.rb
@@ -4,10 +4,14 @@ module Sipity
   module Parameters
     RSpec.describe ActionSet do
       let(:identifier) { double }
-      let(:collection) { double }
-      subject { described_class.new(identifier: identifier, collection: collection) }
+      let(:collection) { double(any?: true, present?: true) }
+      let(:entity) { double }
+      subject { described_class.new(identifier: identifier, collection: collection, entity: entity) }
       its(:identifier) { should eq identifier }
-      its(:collection) { should eq collection }
+      its(:collection) { should eq [collection] }
+      its(:entity) { should eq entity }
+      its(:any?) { should eq collection.any? }
+      its(:present?) { should eq collection.present? }
     end
   end
 end
