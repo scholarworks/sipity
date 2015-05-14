fcb192108aaa74cffec3e7254144fbd5f27b640c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-14 11:13:32 -0400

    Adding ComparableDelegateClass function
    The rubocop settings were adjusted because these comparison operators
    are not-advised by default Rubocop. However, with PowerConverter and
    case statements, I need to leverage these behaviors.

--- app/decorators/sipity/decorators.rb
+++ app/decorators/sipity/decorators.rb
@@ -7,5 +7,40 @@ module Sipity
   #   It does a great job of explaining their importance.
   # @note Establishing module name
   module Decorators
+    module_function
+
+    # A function to help you build a ComparableDecoratorClass
+    #
+    # @example
+    #   class DecoratorBook < Decorators::ComparableDelegateClass(Book)
+    #     # Further details of your class
+    #   end
+    #
+    # @todo Extract this to the hesburgh-lib
+    def ComparableDelegateClass(base_class)
+      klass = Class.new(ComparableSimpleDelegator)
+      klass.base_class = base_class
+      klass
+    end
+
+    # Provides a convenience wrapper of an object to assist in equality testing.
+    # This is key as it relates to PowerConverter and how it is used.
+    class ComparableSimpleDelegator < SimpleDelegator
+      class_attribute :base_class, instance_writer: false
+
+      class << self
+        def ===(other)
+          super || base_class === other
+        end
+      end
+
+      def is_a?(classification)
+        # REVIEW: Is base_class == classification a reasonable assumption?
+        #   Thinking in terms of Liskov's Substitution this may be necessary.
+        super || __getobj__.is_a?(classification) || base_class == classification
+      end
+
+      alias_method :kind_of?, :is_a?
+    end
   end
 end
