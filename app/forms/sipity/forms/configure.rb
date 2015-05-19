8d607058dd5acb8765e6cb64963eefbff2fa4f75
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-19 14:46:59 -0400

    Adding policy_enforcer to form configuration
    Because the policy enforcer comes hand in hand with the existing
    behavior.

--- app/forms/sipity/forms/configure.rb
+++ app/forms/sipity/forms/configure.rb
@@ -4,9 +4,12 @@ module Sipity
     module Configure
       module_function
 
-      def form_for_processing_entity(form_class:, base_class:)
+      def form_for_processing_entity(form_class:, base_class:, policy_enforcer: nil)
+        policy_enforcer ||= base_class.name.sub(/::(\w+)::(\w+)\Z/, '::Policies::\2Policy').constantize
         form_class.class_attribute :base_class unless form_class.respond_to?(:base_class=)
+        form_class.class_attribute :policy_enforcer unless form_class.respond_to?(:policy_enforcer=)
         form_class.base_class = base_class
+        form_class.policy_enforcer = policy_enforcer
         class << form_class
           delegate :model_name, :human_attribute_name, to: :base_class
         end
--- spec/forms/sipity/forms/configure_spec.rb
+++ spec/forms/sipity/forms/configure_spec.rb
@@ -5,12 +5,18 @@ module Sipity
     RSpec.describe Configure do
       context '.form_for_processing_entity' do
         let(:form_class) { Class.new }
-        let(:base_class) { double(model_name: true, human_attribute_name: true) }
+        let(:base_class) { double(model_name: true, human_attribute_name: true, name: 'Sipity::Models::Work') }
 
         it 'will add .base_class to the given .form_class' do
           expect { described_class.form_for_processing_entity(form_class: form_class, base_class: base_class) }.
             to change { form_class.respond_to?(:base_class) }.from(false).to(true)
           expect(form_class.base_class).to eq(base_class)
+        end
+
+        it 'will add .policy_enforcer to the given .form_class' do
+          expect { described_class.form_for_processing_entity(form_class: form_class, base_class: base_class) }.
+            to change { form_class.respond_to?(:policy_enforcer) }.from(false).to(true)
+          expect(form_class.policy_enforcer).to eq(Sipity::Policies::WorkPolicy)
         end
 
         it 'will delegate .model_name to the given .form_class' do
