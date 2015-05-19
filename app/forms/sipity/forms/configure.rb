3604e7c366bb9fc24ff2acd61a2bb22317847013
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-19 15:51:52 -0400

    Augmenting form_for_processing_entity
    I want even greater configuration as an option; Namely templates.

--- app/forms/sipity/forms/configure.rb
+++ app/forms/sipity/forms/configure.rb
@@ -4,12 +4,18 @@ module Sipity
     module Configure
       module_function
 
-      def form_for_processing_entity(form_class:, base_class:, policy_enforcer: nil)
-        policy_enforcer ||= base_class.name.sub(/::(\w+)::(\w+)\Z/, '::Policies::\2Policy').constantize
+      def form_for_processing_entity(form_class:, base_class:, **keywords)
         form_class.class_attribute :base_class unless form_class.respond_to?(:base_class=)
-        form_class.class_attribute :policy_enforcer unless form_class.respond_to?(:policy_enforcer=)
         form_class.base_class = base_class
-        form_class.policy_enforcer = policy_enforcer
+
+        form_class.class_attribute :policy_enforcer unless form_class.respond_to?(:policy_enforcer=)
+        form_class.policy_enforcer = keywords.fetch(:policy_enforcer) do
+          base_class.name.sub(/::(\w+)::(\w+)\Z/, '::Policies::\2Policy').constantize
+        end
+
+        form_class.class_attribute :template unless form_class.respond_to?(:template=)
+        form_class.template = keywords.fetch(:template) { form_class.name.demodulize.sub(/Form\Z/, '').underscore }
+
         class << form_class
           delegate :model_name, :human_attribute_name, to: :base_class
         end
--- spec/forms/sipity/forms/configure_spec.rb
+++ spec/forms/sipity/forms/configure_spec.rb
@@ -4,7 +4,15 @@ module Sipity
   module Forms
     RSpec.describe Configure do
       context '.form_for_processing_entity' do
-        let(:form_class) { Class.new }
+        let(:form_class) do
+          Class.new do
+            class << self
+              def name
+                'Sipity::Forms::Etd::HelloWorldForm'
+              end
+            end
+          end
+        end
         let(:base_class) { double(model_name: true, human_attribute_name: true, name: 'Sipity::Models::Work') }
 
         it 'will add .base_class to the given .form_class' do
@@ -17,6 +25,12 @@ module Sipity
           expect { described_class.form_for_processing_entity(form_class: form_class, base_class: base_class) }.
             to change { form_class.respond_to?(:policy_enforcer) }.from(false).to(true)
           expect(form_class.policy_enforcer).to eq(Sipity::Policies::WorkPolicy)
+        end
+
+        it 'will add .template to the given .form_class' do
+          expect { described_class.form_for_processing_entity(form_class: form_class, base_class: base_class) }.
+            to change { form_class.respond_to?(:template) }.from(false).to(true)
+          expect(form_class.template).to eq('hello_world')
         end
 
         it 'will delegate .model_name to the given .form_class' do
