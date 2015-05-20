ab08863935b31e0565bd3ef494d9e3d3adc0be68
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-19 21:29:27 -0400

    Compacting tests
    The other tests were getting rather chatty, with the shoulda-matchers
    things are much more compact.

--- spec/forms/sipity/forms/configure_spec.rb
+++ spec/forms/sipity/forms/configure_spec.rb
@@ -14,36 +14,15 @@ module Sipity
           end
         end
         let(:base_class) { double(model_name: true, human_attribute_name: true, name: 'Sipity::Models::Work') }
+        before { described_class.form_for_processing_entity(form_class: form_class, base_class: base_class) }
+        subject { form_class }
 
-        it 'will add .base_class to the given .form_class' do
-          expect { described_class.form_for_processing_entity(form_class: form_class, base_class: base_class) }.
-            to change { form_class.respond_to?(:base_class) }.from(false).to(true)
-          expect(form_class.base_class).to eq(base_class)
-        end
+        its(:policy_enforcer) { should eq(Sipity::Policies::WorkPolicy) }
+        its(:base_class) { should eq(base_class) }
+        its(:template) { should eq('hello_world') }
 
-        it 'will add .policy_enforcer to the given .form_class' do
-          expect { described_class.form_for_processing_entity(form_class: form_class, base_class: base_class) }.
-            to change { form_class.respond_to?(:policy_enforcer) }.from(false).to(true)
-          expect(form_class.policy_enforcer).to eq(Sipity::Policies::WorkPolicy)
-        end
-
-        it 'will add .template to the given .form_class' do
-          expect { described_class.form_for_processing_entity(form_class: form_class, base_class: base_class) }.
-            to change { form_class.respond_to?(:template) }.from(false).to(true)
-          expect(form_class.template).to eq('hello_world')
-        end
-
-        it 'will delegate .model_name to the given .form_class' do
-          described_class.form_for_processing_entity(form_class: form_class, base_class: base_class)
-          form_class.model_name
-          expect(base_class).to have_received(:model_name)
-        end
-
-        it 'will delegate .human_attribute_name to the given .form_class' do
-          described_class.form_for_processing_entity(form_class: form_class, base_class: base_class)
-          form_class.human_attribute_name
-          expect(base_class).to have_received(:human_attribute_name)
-        end
+        it { should delegate_method(:model_name).to(:base_class) }
+        it { should delegate_method(:human_attribute_name).to(:base_class) }
       end
     end
   end
