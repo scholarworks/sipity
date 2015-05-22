81df52b61507d4c04e1abc2d9e638b18bb8b0068
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-22 16:35:43 -0400

    Adding ProcessingForm object
    I want a form that can be used for composition; Something that
    highlights the complexity of the interaction through explicit
    contracts instead of through inheritance (and the gnarly structure
    that ensues).

---
+++ app/forms/sipity/forms/processing_form.rb
@@ -0,0 +1,67 @@
+module Sipity
+  module Forms
+    # This class is responsible for handling much of the form composition
+    # regarding processing actions.
+    #
+    # Its my effort to break the inheritance cycle.
+    class ProcessingForm
+      def initialize(form:, repository: default_repository, **keywords)
+        self.form = form
+        self.repository = repository
+        self.processing_action_name = keywords.fetch(:processing_action_name) { default_processing_action_name }
+      end
+
+      delegate :valid?, :entity, to: :form
+
+      def submit(requested_by:)
+        return false unless valid?
+        save(requested_by: requested_by)
+      end
+
+      attr_reader :repository, :processing_action_name, :registered_action
+      alias_method :to_registered_action, :registered_action
+      alias_method :enrichment_type, :processing_action_name
+      deprecate enrichment_type: "Use :processing_action_name instead"
+
+      def to_processing_entity
+        Conversions::ConvertToProcessingEntity.call(entity)
+      end
+
+      def to_work_area
+        PowerConverter.convert_to_work_area(entity)
+      end
+
+      private
+
+      def event_name
+        "#{processing_action_name}/submit"
+      end
+
+      attr_writer :repository, :processing_action_name
+      attr_reader :form
+
+      def save(requested_by:)
+        @registered_action = repository.register_processing_action_taken_on_entity(
+          entity: entity, action: processing_action_name, requested_by: requested_by
+        )
+        repository.log_event!(entity: entity, user: requested_by, event_name: event_name)
+
+        form.save(requested_by: requested_by)
+      end
+
+      def default_repository
+        CommandRepository.new
+      end
+
+      include GuardInterfaceExpectation
+      def form=(input)
+        guard_interface_expectation!(input, :valid?, :save, :base_class, :entity)
+        @form = input
+      end
+
+      def default_processing_action_name
+        form.class.name.demodulize.sub(/Form\Z/, '').underscore
+      end
+    end
+  end
+end
---
+++ spec/forms/sipity/forms/processing_form_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+module Sipity
+  module Forms
+    RSpec.describe ProcessingForm do
+      let(:entity) { double }
+      let(:form) { double(entity: entity, base_class: Models::Work, valid?: true, save: true, class: double(name: 'StartForm')) }
+      let(:repository) { CommandRepositoryInterface.new }
+      let(:user) { double }
+      subject { described_class.new(form: form, repository: repository) }
+
+      it { should delegate_method(:valid?).to(:form) }
+
+      its(:default_repository) { should respond_to :register_processing_action_taken_on_entity }
+      its(:default_repository) { should respond_to :log_event! }
+      it { should respond_to :to_registered_action }
+
+      it 'should convert the underlying entity to a processing entity' do
+        expect(Conversions::ConvertToProcessingEntity).to receive(:call).with(entity).and_return(:converted)
+        expect(subject.to_processing_entity).to eq(:converted)
+      end
+
+      it 'should convert the underlying entity to a work area' do
+        expect(PowerConverter).to receive(:convert_to_work_area).with(entity).and_return(:converted)
+        expect(subject.to_work_area).to eq(:converted)
+      end
+
+      context '#submit' do
+        context 'when invalid' do
+          before { allow(form).to receive(:valid?).and_return(false) }
+          it 'will return false' do
+            expect(subject).to_not receive(:save)
+            expect(subject.submit(requested_by: user)).to be_falsey
+          end
+        end
+
+        context 'when valid' do
+          before { allow(form).to receive(:valid?).and_return(true) }
+          it 'will return the results of saving the given form' do
+            expect(form).to receive(:save).with(requested_by: user).and_return(:thank_you)
+            expect(subject.submit(requested_by: user)).to eq(:thank_you)
+          end
+
+          it 'will register the action that was taken' do
+            expect(repository).to receive(:register_processing_action_taken_on_entity).and_call_original
+            subject.submit(requested_by: user)
+          end
+
+          it 'will log the event' do
+            expect(repository).to receive(:log_event!).and_call_original
+            subject.submit(requested_by: user)
+          end
+
+          it 'will set the registered action for future reference' do
+            expect(repository).to receive(:register_processing_action_taken_on_entity).and_return(:registered_action)
+            expect { subject.submit(requested_by: user) }.to change { subject.registered_action }.from(nil).to(:registered_action)
+          end
+        end
+      end
+    end
+  end
+end
