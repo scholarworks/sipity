13b7325ca136623a22ddadd8b188ff03ffa7ad2d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-26 11:47:51 -0400

    Normalizing DescribeForm
    Instead of using the Forms::Etd::DescribeForm and its
    inheritance, leveraging the new ProcessingForm.configure.

--- app/forms/sipity/forms/work_submissions/etd/describe_form.rb
+++ app/forms/sipity/forms/work_submissions/etd/describe_form.rb
@@ -3,9 +3,57 @@ module Sipity
     module WorkSubmissions
       module Etd
         # Responsible for capturing and validating information for describe.
-        class DescribeForm < WorkEnrichments::DescribeForm
-          def initialize(work:, repository:, attributes:, processing_action_name:)
-            super(attributes.merge(work: work, repository: repository, enrichment_type: processing_action_name))
+        class DescribeForm
+          ProcessingForm.configure(
+            form_class: self, base_class: Models::Work, processing_subject_name: :work,
+            attribute_names: [:title, :abstract, :alternate_title]
+          )
+
+          def initialize(work:, attributes: {}, **keywords)
+            self.work = work
+            self.processing_action_form = ProcessingForm.new(form: self, **keywords)
+            self.title = attributes.fetch(:title) { title_from_work }
+            self.abstract = attributes.fetch(:abstract) { abstract_from_work }
+            self.alternate_title = attributes.fetch(:alternate_title) { alternate_title_from_work }
+          end
+
+          include ActiveModel::Validations
+          validates :title, presence: true
+          validates :abstract, presence: true
+          validates :work, presence: true
+
+          private
+
+          def save(*)
+            repository.update_work_title!(work: work, title: title)
+            repository.update_work_attribute_values!(work: work, key: 'abstract', values: abstract)
+            repository.update_work_attribute_values!(work: work, key: 'alternate_title', values: alternate_title)
+          end
+
+          include Conversions::SanitizeHtml
+          def title=(value)
+            @title = sanitize_html(value)
+          end
+
+          def abstract=(value)
+            @abstract = sanitize_html(value)
+          end
+
+          def alternate_title=(value)
+            @alternate_title = sanitize_html(value) { nil }
+          end
+
+          def abstract_from_work
+            repository.work_attribute_values_for(work: work, key: 'abstract').first
+          end
+
+          def alternate_title_from_work
+            repository.work_attribute_values_for(work: work, key: 'alternate_title').first
+          end
+
+          def title_from_work
+            return '' unless work
+            work.title
           end
         end
       end
