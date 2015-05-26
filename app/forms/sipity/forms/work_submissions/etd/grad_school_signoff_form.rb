73db020e13eb171977243e9546e5ac1e9ae8685f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-26 13:50:43 -0400

    Normalizing GradSchoolSignoffForm
    Instead of using the Forms::Etd::GradSchoolSignoffForm and its
    inheritance, leveraging the new ProcessingForm.configure.

--- app/forms/sipity/forms/work_submissions/etd/grad_school_signoff_form.rb
+++ app/forms/sipity/forms/work_submissions/etd/grad_school_signoff_form.rb
@@ -3,9 +3,54 @@ module Sipity
     module WorkSubmissions
       module Etd
         # Responsible for submitting the final Grad School approval.
-        class GradSchoolSignoffForm < Forms::Etd::GradSchoolSignoffForm
-          def initialize(work:, repository:, attributes:, processing_action_name:)
-            super(attributes.merge(work: work, repository: repository, enrichment_type: processing_action_name))
+        class GradSchoolSignoffForm
+          ProcessingForm.configure(
+            form_class: self, base_class: Models::Work, attribute_names: :agree_to_signoff, processing_subject_name: :work
+          )
+
+          def initialize(work:, attributes: {}, **keywords)
+            self.work = work
+            self.processing_action_form = processing_action_form_builder.new(form: self, **keywords)
+            self.agree_to_signoff = attributes[:agree_to_signoff]
+          end
+
+          include ActiveModel::Validations
+          validates :agree_to_signoff, acceptance: { accept: true }
+
+          # @param f SimpleFormBuilder
+          #
+          # @return String
+          def render(f:)
+            markup = view_context.content_tag('legend', legend)
+            markup << f.input(
+              :agree_to_signoff,
+              as: :boolean,
+              inline_label:
+              signoff_agreement,
+              input_html: { required: 'required' }, # There is no way to add true boolean attributes to simle_form fields.
+              label: false,
+              wrapper_class: 'checkbox'
+            ).html_safe
+          end
+
+          def legend
+            view_context.t('etd/grad_school_signoff', scope: 'sipity/forms.state_advancing_actions.legend').html_safe
+          end
+
+          def signoff_agreement
+            view_context.t('i_agree', scope: 'sipity/forms.state_advancing_actions.verification.etd/grad_school_signoff').html_safe
+          end
+
+          delegate :submit, to: :processing_action_form
+
+          private
+
+          def agree_to_signoff=(value)
+            @agree_to_signoff = PowerConverter.convert_to_boolean(value)
+          end
+
+          def view_context
+            Draper::ViewContext.current
           end
         end
       end
