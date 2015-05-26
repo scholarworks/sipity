dd2d42475c086269880b21e94e5beded4ccd63f1
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-26 14:16:17 -0400

    Normalizing RespondToGradSchoolRequestForm
    Instead of using the Forms::Etd::RespondToGradSchoolRequestForm and
    its inheritance, leveraging the new ProcessingForm.configure.

---
+++ spec/forms/sipity/forms/work_submissions/etd/respond_to_grad_school_request_form_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+module Sipity
+  module Forms
+    module WorkSubmissions
+      module Etd
+        RSpec.describe RespondToGradSchoolRequestForm do
+          let(:work) { double('Work') }
+          let(:repository) { CommandRepositoryInterface.new }
+          let(:user) { User.new(id: 1) }
+          subject { described_class.new(work: work, repository: repository) }
+
+          context '#render' do
+            let(:f) { double }
+            it 'will return an input text area' do
+              expect(f).to receive(:input).with(:comment, hash_including(as: :text))
+              subject.render(f: f)
+            end
+          end
+
+          its(:input_legend) { should be_html_safe }
+
+          context 'with valid data' do
+            let(:an_action) { double }
+            let(:a_processing_comment) { double }
+            before do
+              allow(repository).to receive(:record_processing_comment).and_return(a_processing_comment)
+              allow(subject).to receive(:convert_to_processing_action).and_return(an_action)
+              allow(subject.send(:processing_action_form)).to receive(:submit).and_yield
+            end
+
+            it 'will send creating user a note that the advisor has requested changes' do
+              expect(repository).to receive(:deliver_notification_for).
+                with(scope: an_action, the_thing: a_processing_comment, requested_by: user).
+                and_call_original
+              subject.submit(requested_by: user)
+            end
+
+            it 'will record the processing comment' do
+              expect(repository).to receive(:record_processing_comment).and_return(a_processing_comment)
+              subject.submit(requested_by: user)
+            end
+          end
+        end
+      end
+    end
+  end
+end
