953d736fcd7298c75781bf88db64975b8d8111ea
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-26 12:27:59 -0400

    Normalizing GradSchoolRequestsChangeForm
    Instead of using the Forms::Etd::GradSchoolRequestsChangeForm and its
    inheritance, leveraging the new ProcessingForm.configure.

--- app/forms/sipity/forms/work_submissions/etd/grad_school_requests_change_form.rb
+++ app/forms/sipity/forms/work_submissions/etd/grad_school_requests_change_form.rb
@@ -4,9 +4,45 @@ module Sipity
       module WorkSubmissions
         # Responsible for capturing comments and forwarding them on to the
         # student.
-        class GradSchoolRequestsChangeForm < Forms::Etd::GradSchoolRequestsChangeForm
-          def initialize(work:, repository:, attributes:, processing_action_name:)
-            super(attributes.merge(work: work, repository: repository, enrichment_type: processing_action_name))
+        class GradSchoolRequestsChangeForm
+          ProcessingForm.configure(form_class: self, base_class: Models::Work, attribute_names: :comment, processing_subject_name: :work)
+
+          def initialize(work:, attributes: {}, **keywords)
+            self.work = work
+            self.processing_action_form = ProcessingForm.new(form: self, **keywords)
+            self.comment = attributes[:comment]
+          end
+
+          include ActiveModel::Validations
+          validates :comment, presence: true
+
+          # @param f SimpleFormBuilder
+          #
+          # @return String
+          def render(f:)
+            markup = view_context.content_tag('legend', grad_school_requests_change_legend)
+            markup << f.input(:comment, as: :text, autofocus: true, input_html: { class: 'form-control', required: 'required' })
+          end
+
+          def grad_school_requests_change_legend
+            view_context.t('etd/grad_school_requests_change', scope: 'sipity/forms.state_advancing_actions.legend').html_safe
+          end
+
+          private
+
+          def view_context
+            Draper::ViewContext.current
+          end
+
+          include Conversions::ConvertToProcessingAction
+          def save(requested_by:)
+            # Who knows what the heck this thing is
+            action = convert_to_processing_action(processing_action_form, scope: work)
+
+            processing_comment = repository.record_processing_comment(
+              entity: work, commenter: requested_by, comment: comment, action: action
+            )
+            repository.deliver_notification_for(the_thing: processing_comment, scope: action, requested_by: requested_by)
           end
         end
       end
---
+++ spec/forms/sipity/forms/work_submissions/etd/grad_school_requests_change_form_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+module Sipity
+  module Forms
+    module Etd
+      module WorkSubmissions
+        RSpec.describe GradSchoolRequestsChangeForm do
+          let(:work) { double('Work') }
+          let(:repository) { CommandRepositoryInterface.new }
+          let(:user) { User.new(id: 1) }
+          subject { described_class.new(work: work, repository: repository) }
+
+          its(:processing_action_name) { should eq('grad_school_requests_change') }
+          it { should_not be_persisted }
+
+          it 'will validate the presence of the :comment' do
+            subject.valid?
+            expect(subject.errors[:comment]).to be_present
+          end
+
+          context '#render' do
+            let(:f) { double }
+            it 'will return an input text area' do
+              expect(f).to receive(:input).with(:comment, hash_including(as: :text))
+              subject.render(f: f)
+            end
+            its(:grad_school_requests_change_legend) { should be_html_safe }
+          end
+
+          context 'with valid data' do
+            let(:processing_comment) { double('Processing Comment') }
+            let(:an_action) { double }
+            before do
+              expect(subject).to receive(:valid?).and_return(true)
+              allow(repository).to receive(:record_processing_comment).and_return(processing_comment)
+              allow(subject).to receive(:convert_to_processing_action).and_return(an_action)
+            end
+
+            it 'will send creating user a note that the advisor has requested changes' do
+              expect(repository).to receive(:deliver_notification_for).
+                with(scope: an_action, the_thing: processing_comment, requested_by: user).
+                and_call_original
+              subject.submit(requested_by: user)
+            end
+
+            it 'will record the processing comment' do
+              expect(repository).to receive(:record_processing_comment).and_call_original
+              subject.submit(requested_by: user)
+            end
+          end
+        end
+      end
+    end
+  end
+end
