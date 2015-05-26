3428a059df9112a3ecee04de30440cd098be2e30
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-26 14:08:51 -0400

    Normalizing RespondToAdvisorRequestForm
    Instead of using the Forms::Etd::RespondToAdvisorRequestForm and its
    inheritance, leveraging the new ProcessingForm.configure.

--- app/forms/sipity/forms/work_submissions/etd/respond_to_advisor_request_form.rb
+++ app/forms/sipity/forms/work_submissions/etd/respond_to_advisor_request_form.rb
@@ -4,9 +4,48 @@ module Sipity
       module Etd
         # Responsible for capturing a student's comment and forwarding them on to
         # the advisors.
-        class RespondToAdvisorRequestForm < Forms::Etd::RespondToAdvisorRequestForm
-          def initialize(work:, repository:, attributes:, processing_action_name:)
-            super(attributes.merge(work: work, repository: repository, enrichment_type: processing_action_name))
+        class RespondToAdvisorRequestForm
+          ProcessingForm.configure(
+            form_class: self, base_class: Models::Work, attribute_names: :comment, processing_subject_name: :work
+          )
+
+          def initialize(work:, attributes: {}, **keywords)
+            self.work = work
+            self.processing_action_form = processing_action_form_builder.new(form: self, **keywords)
+            self.comment = attributes[:comment]
+          end
+
+          include ActiveModel::Validations
+          validates :comment, presence: true
+
+          # @param f SimpleFormBuilder
+          #
+          # @return String
+          def render(f:)
+            markup = view_context.content_tag('legend', input_legend)
+            markup << f.input(:comment, as: :text, autofocus: true, input_html: { class: 'form-control', required: 'required' })
+          end
+
+          include Conversions::ConvertToProcessingAction
+          def submit(requested_by:)
+            processing_action_form.submit(requested_by: requested_by) do
+              action = convert_to_processing_action(processing_action_form, scope: work)
+
+              processing_comment = repository.record_processing_comment(
+                entity: work, commenter: requested_by, comment: comment, action: action
+              )
+              repository.deliver_notification_for(the_thing: processing_comment, scope: action, requested_by: requested_by)
+            end
+          end
+
+          private
+
+          def input_legend
+            view_context.t('etd/respond_to_advisor_request_form', scope: 'sipity/forms.state_advancing_actions.legend').html_safe
+          end
+
+          def view_context
+            Draper::ViewContext.current
           end
         end
       end
---
+++ spec/forms/sipity/forms/work_submissions/etd/respond_to_advisor_request_form_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+module Sipity
+  module Forms
+    module WorkSubmissions
+      module Etd
+        RSpec.describe RespondToAdvisorRequestForm do
+          let(:processing_entity) { Models::Processing::Entity.new(strategy_id: 1) }
+          let(:work) { double('Work', to_processing_entity: processing_entity) }
+          let(:repository) { CommandRepositoryInterface.new }
+          let(:action) do
+            Models::Processing::StrategyAction.new(strategy_id: processing_entity.strategy_id, name: 'respond_to_advisor_request')
+          end
+          let(:user) { User.new(id: 1) }
+          subject { described_class.new(work: work, processing_action_name: action, repository: repository) }
+
+          context '#render' do
+            let(:f) { double }
+            it 'will return an input text area' do
+              expect(f).to receive(:input).with(:comment, hash_including(as: :text))
+              subject.render(f: f)
+            end
+          end
+
+          its(:input_legend) { should be_html_safe }
+
+          context 'with valid data' do
+            let(:a_processing_comment) { double }
+            let(:an_action) { double(resulting_strategy_state: double) }
+            before do
+              allow(subject).to receive(:convert_to_processing_action).and_return(an_action)
+              allow(subject.send(:processing_action_form)).to receive(:submit).and_yield
+              allow(repository).to receive(:record_processing_comment).and_return(a_processing_comment)
+            end
+
+            it 'will send creating user a note that the advisor has requested changes' do
+              expect(repository).to receive(:deliver_notification_for).
+                with(the_thing: a_processing_comment, scope: an_action, requested_by: user).
+                and_call_original
+              subject.submit(requested_by: user)
+            end
+
+            it 'will record the processing comment' do
+              expect(repository).to receive(:record_processing_comment).and_return(a_processing_comment)
+              subject.submit(requested_by: user)
+            end
+          end
+        end
+      end
+    end
+  end
+end
