87a548d80edaa6c55094b881d71758d473694626
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-26 11:30:51 -0400

    Normalizing DefenseDate form
    Instead of using the Forms::Etd::DefenseDate and its
    inheritance, leveraging the new ProcessingForm.configure.

--- app/forms/sipity/forms/work_submissions/etd/defense_date_form.rb
+++ app/forms/sipity/forms/work_submissions/etd/defense_date_form.rb
@@ -3,9 +3,35 @@ module Sipity
     module WorkSubmissions
       module Etd
         # Exposes a means defining the thesis defense date todo item.
-        class DefenseDateForm < WorkEnrichments::DefenseDateForm
-          def initialize(work:, repository:, attributes:, processing_action_name:)
-            super(attributes.merge(work: work, repository: repository, enrichment_type: processing_action_name))
+        class DefenseDateForm
+          ProcessingForm.configure(
+            form_class: self, base_class: Models::Work, processing_subject_name: :work, attribute_names: [:defense_date]
+          )
+
+          include Conversions::ExtractInputDateFromInput
+          def initialize(work:, attributes: {}, **keywords)
+            self.work = work
+            self.processing_action_form = ProcessingForm.new(form: self, **keywords)
+            self.defense_date = extract_input_date_from_input(:defense_date, attributes) { defense_date_from_work }
+          end
+
+          include ActiveModel::Validations
+          validates :defense_date, presence: true
+
+          private
+
+          def save(requested_by:)
+            repository.update_work_attribute_values!(work: work, key: 'defense_date', values: defense_date)
+          end
+
+          def defense_date_from_work
+            return nil unless work
+            Array.wrap(repository.work_attribute_values_for(work: work, key: 'defense_date')).first
+          end
+
+          include Conversions::ConvertToDate
+          def defense_date=(value)
+            @defense_date = convert_to_date(value) { nil }
           end
         end
       end
