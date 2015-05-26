ed874b8578eb2c722fecea510124417ed1a9fb61
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-26 15:28:05 -0400

    Removing lookups for replaced forms

--- spec/forms/sipity/forms/work_event_triggers_spec.rb
+++ spec/forms/sipity/forms/work_event_triggers_spec.rb
@@ -4,17 +4,6 @@ module Sipity
   module Forms
     RSpec.describe WorkEventTriggers do
       context '#find_event_trigger_form_builder' do
-        [
-          ['submit_for_review', Etd::SubmitForReviewForm],
-          ['advisor_signoff', Etd::AdvisorSignoffForm],
-          ['advisor_requests_changes', Etd::AdvisorRequestsChangeForm]
-        ].each_with_index do |(processing_action_name, form_class), index|
-          it "will return #{form_class} for processing_action_name: #{processing_action_name} (Scenario ##{index}" do
-            expect(described_class.find_event_trigger_form_builder(processing_action_name: processing_action_name)).
-              to eq(form_class)
-          end
-        end
-
         it 'will raise an exception if no trigger can be inferred' do
           expect { described_class.find_event_trigger_form_builder(processing_action_name: 'bilbo_baggins') }.
             to raise_error(Exceptions::EventTriggerFormNotFoundError)
