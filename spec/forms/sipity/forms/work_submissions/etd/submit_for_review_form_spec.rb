5428f3c50c040828a80bafb3e2e33b13e5899076
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-26 14:38:37 -0400

    Normalizing SubmitForReviewForm
    Instead of using the Forms::Etd::SubmitForReviewForm and its
    inheritance, leveraging the new ProcessingForm.configure.

---
+++ spec/forms/sipity/forms/work_submissions/etd/submit_for_review_form_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+module Sipity
+  module Forms
+    module WorkSubmissions
+      module Etd
+        RSpec.describe SubmitForReviewForm do
+          let(:processing_entity) { Models::Processing::Entity.new(strategy_id: 1) }
+          let(:work) { double('Work', to_processing_entity: processing_entity) }
+          let(:repository) { CommandRepositoryInterface.new }
+          let(:action) { Models::Processing::StrategyAction.new(strategy_id: processing_entity.strategy_id) }
+          let(:user) { User.new(id: 1) }
+          subject { described_class.new(work: work, processing_action_name: action, repository: repository) }
+
+          it 'validates the aggreement to the submission terms' do
+            subject = described_class.new(work: work, processing_action_name: action, repository: repository)
+            subject.valid?
+            expect(subject.errors[:agree_to_terms_of_deposit]).to be_present
+          end
+
+          context '#render' do
+            it 'will render HTML safe submission terms and confirmation' do
+              form_object = double('Form Object')
+              expect(form_object).to receive(:input).with(:agree_to_terms_of_deposit, hash_including(as: :boolean)).and_return("<input />")
+              expect(subject.render(f: form_object)).to be_html_safe
+            end
+          end
+
+          it { should delegate_method(:submit).to(:processing_action_form) }
+        end
+      end
+    end
+  end
+end
