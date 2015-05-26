598ca8ab1b679e0292bae93db1b0d96a05a7b720
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-26 11:00:55 -0400

    Normalizing AdvisorSignoffForm form
    Instead of using the Forms::Etd::AdvisorSignoffForm and its
    inheritance, leveraging the new ProcessingForm.configure.

--- app/forms/sipity/forms/work_submissions/etd/advisor_signoff_form.rb
+++ app/forms/sipity/forms/work_submissions/etd/advisor_signoff_form.rb
@@ -4,9 +4,62 @@ module Sipity
       module Etd
         # Responsible for submitting the associated entity to the advisor
         # for signoff.
-        class AdvisorSignoffForm < Forms::Etd::AdvisorSignoffForm
-          def initialize(work:, repository:, attributes:, processing_action_name:)
-            super(attributes.merge(work: work, repository: repository, enrichment_type: processing_action_name))
+        class AdvisorSignoffForm
+          ProcessingForm.configure(form_class: self, base_class: Models::Work, attribute_names: [:agree_to_signoff])
+
+          def initialize(work:, attributes: {}, signoff_service: default_signoff_service, **keywords)
+            self.work = work
+            self.processing_action_form = ProcessingForm.new(form: self, **keywords)
+            self.signoff_service = signoff_service
+            self.agree_to_signoff = attributes[:agree_to_signoff]
+          end
+
+          include ActiveModel::Validations
+          validates :agree_to_signoff, acceptance: { accept: true }
+
+          # @param f SimpleFormBuilder
+          #
+          # @return String
+          def render(f:)
+            markup = view_context.content_tag('legend', advisor_signoff_legend)
+            markup << f.input(
+              :agree_to_signoff,
+              as: :boolean,
+              inline_label:
+              signoff_agreement,
+              input_html: { required: 'required' }, # There is no way to add true boolean attributes to simle_form fields.
+              label: false,
+              wrapper_class: 'checkbox'
+            ).html_safe
+          end
+
+          # TODO: Normalize translation
+          def advisor_signoff_legend
+            view_context.t('etd/advisor_signoff', scope: 'sipity/forms.state_advancing_actions.legend').html_safe
+          end
+
+          # TODO: Normalize translation
+          def signoff_agreement
+            view_context.t('i_agree', scope: 'sipity/forms.state_advancing_actions.verification.etd/advisor_signoff').html_safe
+          end
+
+          private
+
+          def agree_to_signoff=(value)
+            @agree_to_signoff = PowerConverter.convert_to_boolean(value)
+          end
+
+          def view_context
+            Draper::ViewContext.current
+          end
+
+          attr_accessor :signoff_service
+          def default_signoff_service
+            Services::AdvisorSignsOff
+          end
+
+          def save(requested_by:)
+            signoff_service.call(form: self, requested_by: requested_by, repository: repository)
           end
         end
       end
---
+++ spec/forms/sipity/forms/work_submissions/etd/advisor_signoff_form_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+
+module Sipity
+  module Forms
+    module WorkSubmissions
+      module Etd
+        RSpec.describe AdvisorSignoffForm do
+          let(:processing_entity) { Models::Processing::Entity.new(strategy_id: 1) }
+          let(:work) { double('Work', to_processing_entity: processing_entity) }
+          let(:repository) { CommandRepositoryInterface.new }
+          let(:action) { Models::Processing::StrategyAction.new(strategy_id: processing_entity.strategy_id, name: 'advisor_signoff') }
+          let(:user) { User.new(id: 1) }
+          let(:signoff_service) { double('Signoff Service', call: true) }
+          subject do
+            described_class.new(work: work, processing_action_name: action, signoff_service: signoff_service, repository: repository)
+          end
+
+          context 'the default signoff service' do
+            it 'will respond to :call' do
+              expect(described_class.new(work: work, processing_action_name: action).send(:default_signoff_service)).to respond_to(:call)
+            end
+          end
+
+          its(:work) { should eq work }
+
+          context '#render' do
+            it 'will render HTML safe submission terms and confirmation' do
+              form_object = double('Form Object')
+              expect(form_object).to receive(:input).with(:agree_to_signoff, hash_including(as: :boolean)).and_return("<input />")
+              expect(subject.render(f: form_object)).to be_html_safe
+            end
+          end
+
+          its(:advisor_signoff_legend) { should be_html_safe }
+          its(:signoff_agreement) { should be_html_safe }
+
+          context 'validation' do
+            it 'will require agreement to the signoff' do
+              subject = described_class.new(work: work, processing_action_name: action, repository: repository)
+              subject.valid?
+              expect(subject.errors[:agree_to_signoff]).to be_present
+            end
+          end
+
+          context 'when not valid, #submit' do
+            before do
+              expect(subject).to receive(:valid?).and_return(false)
+              expect(signoff_service).to_not receive(:call)
+            end
+            it 'will return the false' do
+              expect(subject.submit(requested_by: user)).to eq(false)
+            end
+          end
+
+          context 'when valid, #submit' do
+            before do
+              expect(subject).to receive(:valid?).and_return(true)
+            end
+
+            it 'will return the given work' do
+              expect(subject.submit(requested_by: user)).to eq(work)
+            end
+
+            it 'will call the AdvisorSignsOff service' do
+              expect(signoff_service).to receive(:call)
+              subject.submit(requested_by: user)
+            end
+          end
+        end
+      end
+    end
+  end
+end
