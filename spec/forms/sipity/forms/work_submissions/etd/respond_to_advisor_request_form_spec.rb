8fe10e71d155fc96ce973e872b60a7f6778ca530
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-28 09:58:21 -0400

    Leveraging RequestChangesViaCommentService
    The comment service is more applicable

--- spec/forms/sipity/forms/work_submissions/etd/respond_to_advisor_request_form_spec.rb
+++ spec/forms/sipity/forms/work_submissions/etd/respond_to_advisor_request_form_spec.rb
@@ -28,22 +28,19 @@ module Sipity
           context 'with valid data' do
             let(:a_processing_comment) { double }
             let(:an_action) { double(resulting_strategy_state: double) }
+
             before do
-              allow(subject).to receive(:convert_to_processing_action).and_return(an_action)
-              allow(subject.send(:processing_action_form)).to receive(:submit).and_yield
-              allow(repository).to receive(:record_processing_comment).and_return(a_processing_comment)
+              expect(subject).to receive(:valid?).and_return(true)
+              allow(Services::RequestChangesViaCommentService).to receive(:call)
             end
 
-            it 'will send creating user a note that the advisor has requested changes' do
-              expect(repository).to receive(:deliver_notification_for).
-                with(the_thing: a_processing_comment, scope: an_action, requested_by: user).
-                and_call_original
+            it 'will delegate to Services::RequestChangesViaCommentService' do
+              expect(Services::RequestChangesViaCommentService).to receive(:call)
               subject.submit(requested_by: user)
             end
 
-            it 'will record the processing comment' do
-              expect(repository).to receive(:record_processing_comment).and_return(a_processing_comment)
-              subject.submit(requested_by: user)
+            it 'will return the work' do
+              expect(subject.submit(requested_by: user)).to eq(work)
             end
           end
         end
