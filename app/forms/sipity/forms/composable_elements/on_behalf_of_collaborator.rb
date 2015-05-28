61a5e00e189a416de243b87e9f51e8187c6210d2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-05-28 10:27:05 -0400

    Enforcing contract for OnBehalfOfCollaborator
    Instead of the loose delegation, without contract enforcement, I'm
    opting to guard the input for a form.

--- app/forms/sipity/forms/composable_elements/on_behalf_of_collaborator.rb
+++ app/forms/sipity/forms/composable_elements/on_behalf_of_collaborator.rb
@@ -8,25 +8,34 @@ module Sipity
           self.form = form
           self.repository = repository
         end
-        attr_accessor :form, :repository
-        private(:form=, :repository=)
+
+        private
+
+        attr_accessor :repository
+        attr_reader :form
+
+        public
 
         attr_accessor :on_behalf_of_collaborator_id
 
-        delegate :action, :work, to: :form
-
         def valid_on_behalf_of_collaborators
           # TODO: This can be consolidated into a singular query
-          repository.collaborators_that_can_advance_the_current_state_of(work: work) -
-            repository.collaborators_that_have_taken_the_action_on_the_entity(entity: work, actions: action)
+          repository.collaborators_that_can_advance_the_current_state_of(work: form.entity) -
+            repository.collaborators_that_have_taken_the_action_on_the_entity(entity: form.entity, actions: form.to_processing_action)
         end
 
         def on_behalf_of_collaborator
-          repository.collaborators_that_can_advance_the_current_state_of(work: work, id: on_behalf_of_collaborator_id).first
+          repository.collaborators_that_can_advance_the_current_state_of(work: form.entity, id: on_behalf_of_collaborator_id).first
         end
 
         def valid_on_behalf_of_collaborator_ids
           valid_on_behalf_of_collaborators.map { |collaborator| collaborator.id.to_s }
+        end
+
+        include GuardInterfaceExpectation
+        def form=(input)
+          guard_interface_expectation!(input, :entity, :to_processing_action)
+          @form = input
         end
       end
     end
