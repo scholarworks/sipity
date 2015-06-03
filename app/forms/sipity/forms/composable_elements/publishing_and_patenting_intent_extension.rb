fd656d895b0918fd248aa3b04e6ddeefb0d5a8a2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-03 10:50:46 -0400

    Adding PublishingAndPatentingIntentExtension
    Extracting common behavior to improve composition of the publishing
    and patenting intent.

---
+++ app/forms/sipity/forms/composable_elements/publishing_and_patenting_intent_extension.rb
@@ -0,0 +1,64 @@
+module Sipity
+  module Forms
+    module ComposableElements
+      # Responsible for coordinating the Publishing and Patenting Intent; Useful
+      # as an extension
+      class PublishingAndPatentingIntentExtension
+        def initialize(form:, repository: default_repository)
+          self.form = form
+          self.repository = repository
+        end
+
+        private
+
+        attr_accessor :repository
+        attr_reader :form
+
+        include GuardInterfaceExpectation
+        def form=(input)
+          guard_interface_expectation!(input, :work)
+          @form = input
+        end
+
+        public
+
+        [
+          [:work_patent_strategy, :work_patent_strategies],
+          [:work_publication_strategy, :work_publication_strategies]
+        ].each do |singular_name, plural_name|
+          module_exec do
+            attr_accessor singular_name, plural_name
+
+            define_method "#{plural_name}_for_select" do
+              send("possible_#{plural_name}").map(&:to_sym)
+            end
+
+            define_method "possible_#{plural_name}" do
+              repository.get_controlled_vocabulary_values_for_predicate_name(name: send("#{singular_name}_predicate_name"))
+            end
+
+            define_method "#{singular_name}_from_work" do
+              Array.wrap(repository.work_attribute_values_for(work: form.work, key: send("#{singular_name}_predicate_name"))).first
+            end
+
+            define_method "persist_#{singular_name}" do
+              repository.update_work_attribute_values!(
+                work: form.work, key: send("#{singular_name}_predicate_name"), values: send(singular_name)
+              )
+            end
+
+            define_method "#{singular_name}_predicate_name" do
+              Models::AdditionalAttribute.const_get(singular_name.to_s.upcase)
+            end
+          end
+        end
+
+        private
+
+        def default_repository
+          CommandRepository.new
+        end
+      end
+    end
+  end
+end
