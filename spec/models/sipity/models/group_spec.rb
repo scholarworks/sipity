2089892d2f3b0ab476deb15059e66bf040a91f00
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-04 15:43:41 -0400

    Adding Group#to_s

--- spec/models/sipity/models/group_spec.rb
+++ spec/models/sipity/models/group_spec.rb
@@ -6,6 +6,8 @@ module Sipity
       subject { described_class.new }
       its(:valid?) { should be false }
 
+      it { should delegate_method(:to_s).to(:name) }
+
       it 'will have a Group.all_registered_users' do
         expect(described_class.all_registered_users).to be_persisted
       end
