37bf3cc1e28392a2723d6d8cc0808cd45b6e3eed
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-05 14:26:38 -0400

    Adding convenience conversion methods
    I want paths and URLs and normalized behavior

--- spec/config/initializers/power_converters_spec.rb
+++ spec/config/initializers/power_converters_spec.rb
@@ -28,6 +28,31 @@ RSpec.describe 'power converters' do
     end
   end
 
+  context 'access_url' do
+    [
+      {
+        to_convert: Sipity::Models::WorkArea.new(slug: 'wa-slug'),
+        expected: "/areas/wa-slug"
+      }, {
+        to_convert: Sipity::Models::SubmissionWindow.new(slug: 'sw-slug', work_area: Sipity::Models::WorkArea.new(slug: 'wa-slug')),
+        expected: "/areas/wa-slug/sw-slug"
+      }, {
+        to_convert: Sipity::Models::Work.new(id: 'w-id'),
+        expected: "/work_submissions/w-id"
+      }
+    ].each do |scenario|
+      it "will convert #{scenario.fetch(:to_convert).inspect} to '#{scenario.fetch(:expected)}'" do
+        expect(PowerConverter.convert(scenario.fetch(:to_convert), to: :access_url)).to match(scenario.fetch(:expected))
+      end
+    end
+
+    it 'will not convert an attachment' do
+      object = Sipity::Models::Attachment.create!(file: File.new(__FILE__), work_id: 1, pid: 2, predicate_name: 'attachment')
+      expect { PowerConverter.convert(object, to: :access_path) }.
+        to raise_error(PowerConverter::ConversionError)
+    end
+  end
+
   context 'boolean' do
     [
       ['1', true],
