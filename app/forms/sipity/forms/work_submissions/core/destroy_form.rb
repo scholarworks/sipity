e7bec732f966cb637a2f19f916d12cae54e2b0c3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-05 13:50:09 -0400

    Adding DestroyForm
    We do want a way to remove the object.

---
+++ app/forms/sipity/forms/work_submissions/core/destroy_form.rb
@@ -0,0 +1,43 @@
+module Sipity
+  module Forms
+    module WorkSubmissions
+      module Core
+        # Responsible for "deleting" a Work.
+        class DestroyForm
+          ProcessingForm.configure(
+            form_class: self, base_class: Models::Work, processing_subject_name: :work,
+            attribute_names: [:confirm_destroy]
+          )
+
+          def initialize(work:, attributes: {}, **keywords)
+            self.work = work
+            self.processing_action_form = processing_action_form_builder.new(form: self, **keywords)
+            self.confirm_destroy = attributes[:confirm_destroy]
+            initialize_submission_window!
+          end
+
+          include ActiveModel::Validations
+          validates :confirm_destroy, acceptance: { accept: true }
+
+          def submit(requested_by:)
+            return false unless valid?
+            repository.destroy_a_work(work: work)
+            submission_window # Because we won't have a work
+          end
+
+          private
+
+          def confirm_destroy=(value)
+            @confirm_destroy = PowerConverter.convert_to_boolean(value)
+          end
+
+          attr_reader :submission_window
+
+          def initialize_submission_window!
+            @submission_window = PowerConverter.convert_to_submission_window(work)
+          end
+        end
+      end
+    end
+  end
+end
