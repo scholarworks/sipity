859a36d04fdd93a65067b9c00213b3564f8d8947
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-08 16:21:50 -0400

    Adding UnauthenticatedConstraint
    I want to add custom constraint that handles non-authenticated users.
    The case that I'm working towards is ensuring that when we deploy
    Sipity and decommission the ETD system, the ETD url resolves to an
    unauthenticated page.

---
+++ spec/constraints/sipity/constraints/unauthenticated_constraint_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+module Sipity
+  module Constraints
+    RSpec.describe UnauthenticatedConstraint do
+      context '.matches?' do
+        it 'will be true if there is no warden environment' do
+          request = double(env: {})
+          expect(subject.matches?(request)).to eq(true)
+        end
+
+        it 'will be true warden does not have a user' do
+          request = double(env: { 'warden' => nil })
+          expect(subject.matches?(request)).to eq(true)
+        end
+
+        it 'will be true warden user is nil' do
+          request = double(env: { 'warden' => double(user: nil) })
+          expect(subject.matches?(request)).to eq(true)
+        end
+
+        it 'will be false if warden has a user (that is someone is authenticated)' do
+          request = double(env: { 'warden' => double(user: 'Hello') })
+          expect(subject.matches?(request)).to eq(false)
+        end
+      end
+    end
+  end
+end
