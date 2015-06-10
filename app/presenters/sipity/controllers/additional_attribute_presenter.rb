25b2b98791ab462d2bb55d0d5ca1057650b08301
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-10 08:46:21 -0400

    Enabling alternate attribute render strategies
    This is the first step towards polymorphic rendering of values.

--- app/presenters/sipity/controllers/additional_attribute_presenter.rb
+++ app/presenters/sipity/controllers/additional_attribute_presenter.rb
@@ -11,7 +11,7 @@ module Sipity
       def render_list_of_values
         text = "<ul class='occurrences'>\n"
         Array.wrap(values).each_with_object(text) do |value, mem|
-          mem << "\n" << content_tag('li', value.html_safe, class: "occurrence #{key}")
+          mem << "\n" << content_tag('li', __send__(method_name_for_render_key_value, value), class: "occurrence #{key}")
         end
         text << "\n</ul>"
         text.html_safe
@@ -22,6 +22,22 @@ module Sipity
       private
 
       attr_reader :additional_attribute
+
+      RENDER_METHOD_PREFIX = 'render_value_for_'.freeze
+      RENDER_METHOD_REGEXP = /\A#{RENDER_METHOD_PREFIX}/.freeze
+
+      def method_name_for_render_key_value
+        "#{RENDER_METHOD_PREFIX}#{PowerConverter.convert_to_safe_for_method_name(key)}"
+      end
+
+      def method_missing(method_name, *args, &block)
+        match = RENDER_METHOD_REGEXP.match(method_name)
+        if match
+          args.first.to_s.html_safe
+        else
+          super
+        end
+      end
     end
   end
 end
