3d72d4b71b350b1d98d2d9e505a714ea7e06beef
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-10 16:08:04 -0400

    Removing transient answer
    This is no longer needed as it is encoded in the AccessRight. I
    thought we would have more of this, but I'm not thinking its a
    requirement. So out goes the code.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610170005) do
+ActiveRecord::Schema.define(version: 20150610193426) do
 
   create_table "sipity_access_rights", force: :cascade do |t|
     t.string   "entity_id",         limit: 32,  null: false
