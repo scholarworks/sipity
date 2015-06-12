426646c1947b1aaf7562f0b2393ca14299fc77a0
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-12 11:24:57 -0400

    Refactoring to allow repository injection
    By removing a direct reference to a constant, I'm able to have a more
    robust composition.
    
    Also, in looking at Policies::WorkPolicy::Scope.resolve, it was only
    used via this single method. That means, I am able to remove a layer
    of indirection (and ultimately remove a class).

--- app/repositories/sipity/queries/work_queries.rb
+++ app/repositories/sipity/queries/work_queries.rb
@@ -10,24 +10,27 @@ module Sipity
         Models::Work.includes(:processing_entity, work_submission: [:work_area, :submission_window]).find(id)
       end
 
-      def build_dashboard_view(user:, filter: {})
-        Decorators::DashboardView.new(repository: self, user: user, filter: filter)
+      def build_dashboard_view(user:, filter: {}, repository: self)
+        Decorators::DashboardView.new(repository: repository, user: user, filter: filter)
       end
 
       # TODO: Rename this method; Keeping it separate for now.
-      def build_work_submission_processing_action_form(work:, processing_action_name:, attributes: {})
+      def build_work_submission_processing_action_form(work:, processing_action_name:, attributes: {}, repository: self)
         # Leveraging an obvious inflection point, namely each work area may well
         # have its own form module.
         Forms::WorkSubmissions.build_the_form(
           work: work,
           processing_action_name: processing_action_name,
           attributes: attributes,
-          repository: self
+          repository: repository
         )
       end
 
-      def find_works_for(user:, processing_state: nil)
-        Policies::WorkPolicy::Scope.resolve(user: user, scope: Models::Work, processing_state: processing_state)
+      # @todo: Is there a Parameter Object that makes more sense?
+      def find_works_for(user:, processing_state: nil, repository: self)
+        repository.scope_proxied_objects_for_the_user_and_proxy_for_type(
+          user: user, proxy_for_type: Models::Work, filter: { processing_state: processing_state }
+        )
       end
 
       def work_access_right_codes(work:)
