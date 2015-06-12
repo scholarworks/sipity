d860fc251dce42334da541043a7bc29d2044807f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-12 11:44:10 -0400

    Removing module function declarations
    These are not needed and create undue points of potention chaos.

--- app/repositories/sipity/commands/additional_attribute_commands.rb
+++ app/repositories/sipity/commands/additional_attribute_commands.rb
@@ -9,25 +9,18 @@ module Sipity
         create_work_attribute_values!(work: work, key: key, values: (input_values - existing_values))
         destroy_work_attribute_values!(work: work, key: key, values: (existing_values - input_values))
       end
-      module_function :update_work_attribute_values!
-      public :update_work_attribute_values!
 
       def create_work_attribute_values!(work:, key:, values:)
         Array.wrap(values).each do |value|
           Models::AdditionalAttribute.create!(work: work, key: key, value: value) if value.present?
         end
       end
-      module_function :create_work_attribute_values!
-      public :create_work_attribute_values!
 
       def destroy_work_attribute_values!(work:, key:, values:)
         values_to_destroy = Array.wrap(values)
         return true unless values_to_destroy.present?
         Models::AdditionalAttribute.where(work: work, key: key, value: values_to_destroy).destroy_all
       end
-
-      module_function :destroy_work_attribute_values!
-      public :destroy_work_attribute_values!
     end
   end
 end
