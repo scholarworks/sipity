2c776d74426bbba09a520d165c2e51f29af54f4d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-15 09:28:22 -0400

    Translating AdditionalAttributePresenter labels

--- spec/presenters/sipity/controllers/additional_attribute_presenter_spec.rb
+++ spec/presenters/sipity/controllers/additional_attribute_presenter_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Sipity
   module Controllers
     RSpec.describe AdditionalAttributePresenter do
-      let(:additional_attribute) { double(key: 'the_key', values: ['a', 'b']) }
+      let(:additional_attribute) { double(key: 'the_key', values: ['a', 'b'], entity: double) }
       let(:context) { PresenterHelper::Context.new }
       subject { described_class.new(context, additional_attribute: additional_attribute) }
 
@@ -12,13 +12,18 @@ module Sipity
       its(:label) { should be_a(String) }
       its(:render_list_of_values) { should be_html_safe }
 
+      it 'will translate the key into a label' do
+        expect(TranslationAssistant).to receive(:call)
+        subject.label
+      end
+
       [
         { key: 'work_patent_strategy', value: 'hello_world', expected: 'Hello World' },
         { key: 'work_publication_strategy', value: 'hello_world', expected: 'Hello World' },
         { key: 'chicken_nugget', value: 'hello_world', expected: 'hello_world' }
       ].each do |the_test|
         it "will render '#{the_test[:key]}' with value: #{the_test.fetch(:value)}" do
-          additional_attribute = double(key: the_test[:key], values: the_test.fetch(:value))
+          additional_attribute = double(key: the_test[:key], values: the_test.fetch(:value), entity: double)
           subject = described_class.new(context, additional_attribute: additional_attribute)
           expect(subject.render_list_of_values).to include(the_test[:expected])
         end
