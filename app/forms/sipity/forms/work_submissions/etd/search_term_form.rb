be2a66ced5f83ea7a71400fbfa67d1573b2335b9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-16 13:59:44 -0400

    Appeasing Rubocop with simplification
    ```console
    Assignment Branch Condition size for initialize is too high. [17/16]
    ```

--- app/forms/sipity/forms/work_submissions/etd/search_term_form.rb
+++ app/forms/sipity/forms/work_submissions/etd/search_term_form.rb
@@ -13,10 +13,7 @@ module Sipity
             self.work = work
             self.requested_by = requested_by
             self.processing_action_form = processing_action_form_builder.new(form: self, **keywords)
-            self.subject = attributes.fetch(:subject) { subject_from_work }
-            self.language = attributes.fetch(:language) { language_from_work }
-            self.temporal_coverage = attributes.fetch(:temporal_coverage) { temporal_coverage_from_work }
-            self.spatial_coverage = attributes.fetch(:spatial_coverage) { spatial_coverage_from_work }
+            initialize_attributes(attributes)
           end
 
           def submit
@@ -31,6 +28,13 @@ module Sipity
           include ActiveModel::Validations
 
           private
+
+          def initialize_attributes(attributes)
+            self.subject = attributes.fetch(:subject) { subject_from_work }
+            self.language = attributes.fetch(:language) { language_from_work }
+            self.temporal_coverage = attributes.fetch(:temporal_coverage) { temporal_coverage_from_work }
+            self.spatial_coverage = attributes.fetch(:spatial_coverage) { spatial_coverage_from_work }
+          end
 
           def update_attribute_values(key)
             repository.update_work_attribute_values!(work: work, key: key, values: send(key))
