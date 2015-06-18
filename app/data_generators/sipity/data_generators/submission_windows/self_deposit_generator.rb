b940b61e054bc22bcfec3548f2acb8547588e59f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-18 15:37:28 -0400

    Adding SelfDeposit start submission window

---
+++ app/data_generators/sipity/data_generators/submission_windows/self_deposit_generator.rb
@@ -0,0 +1,9 @@
+module Sipity
+  module DataGenerators
+    module SubmissionWindows
+      # Responsible for generating the submission window for the SelfDeposit work area.
+      class SelfDepositGenerator < BaseGenerator
+      end
+    end
+  end
+end
