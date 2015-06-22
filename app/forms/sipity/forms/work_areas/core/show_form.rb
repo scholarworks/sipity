43cc4a7455040f02b4d5c4fd85bb3500759db6bc
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-22 12:13:30 -0400

    Generalzing WorkAreas show form

--- app/forms/sipity/forms/work_areas/core/show_form.rb
+++ app/forms/sipity/forms/work_areas/core/show_form.rb
@@ -2,27 +2,56 @@ module Sipity
   module Forms
     module WorkAreas
       module Core
-        # Responsible for "showing" a Work Area.
-        #
-        # @note This is the result of changing the WorkAreas controller
-        #   to be two actions.
-        class ShowForm < Decorators::ComparableSimpleDelegator
-          self.base_class = Models::WorkArea
-          class_attribute :policy_enforcer
-          self.policy_enforcer = Sipity::Policies::WorkAreaPolicy
+        # Responsible for "showing" an ETD Work Area.
+        class ShowForm
+          ProcessingForm.configure(
+            form_class: self, base_class: Models::WorkArea, attribute_names: [:processing_state, :order, :page]
+          )
 
-          def initialize(work_area:, processing_action_name:, **_keywords)
+          def initialize(work_area:, requested_by:, attributes: {}, **keywords)
             self.work_area = work_area
-            self.processing_action_name = processing_action_name
-            super(work_area)
+            self.requested_by = requested_by
+            self.processing_action_form = processing_action_form_builder.new(form: self, **keywords)
+            self.search_criteria_config = keywords.fetch(:search_criteria_config) { default_search_criteria_config }
+            self.processing_state = attributes[:processing_state]
+            self.order = attributes.fetch(:order) { default_order }
+            self.page = attributes.fetch(:page) { default_page }
           end
 
-          attr_reader :processing_action_name
+          # @note There is a correlation to the Parameters::SearchCriteriaForWorksParameter
+          #   object
+          def input_name_for_select_processing_state
+            "#{model_name.param_key}[processing_state]"
+          end
+
+          # @note There is a correlation to the Parameters::SearchCriteriaForWorksParameter
+          #   object
+          def input_name_for_select_sort_order
+            "#{model_name.param_key}[order]"
+          end
+
+          def processing_states_for_select
+            repository.processing_state_names_for_select_within_work_area(work_area: work_area)
+          end
+
+          include ActiveModel::Validations
+
+          delegate :name, :slug, to: :work_area
 
           private
 
-          attr_writer :processing_action_name
-          attr_accessor :work_area
+          delegate :default_order, :default_page, :order_options_for_select, to: :search_criteria_config
+          attr_accessor :search_criteria_config
+
+          include GuardInterfaceExpectation
+          def work_area=(input)
+            guard_interface_expectation!(input, :name, :slug)
+            @work_area = input
+          end
+
+          def default_search_criteria_config
+            Parameters::SearchCriteriaForWorksParameter
+          end
         end
       end
     end
--- spec/forms/sipity/forms/work_areas/core/show_form_spec.rb
+++ spec/forms/sipity/forms/work_areas/core/show_form_spec.rb
@@ -6,14 +6,30 @@ module Sipity
     module WorkAreas
       module Core
         RSpec.describe ShowForm do
-          let(:processing_action_name) { 'show' }
-          let(:work_area) { double }
-          subject { described_class.new(work_area: work_area, processing_action_name: processing_action_name) }
+          let(:work_area) { double(name: 'Hello Name', slug: 'hello-world') }
+          let(:repository) { QueryRepositoryInterface.new }
+          let(:user) { double }
+          subject { described_class.new(work_area: work_area, requested_by: user, repository: repository) }
 
           its(:policy_enforcer) { should eq Sipity::Policies::WorkAreaPolicy }
-          its(:processing_action_name) { should eq processing_action_name }
+          its(:processing_action_name) { should eq('show') }
+          it { should implement_processing_form_interface }
+          it { should delegate_method(:name).to(:work_area) }
+          it { should delegate_method(:slug).to(:work_area) }
 
-          it { expect(subject.is_a?(Models::WorkArea)).to be_truthy }
+          its(:order_options_for_select) { should be_a(Array) }
+          its(:input_name_for_select_processing_state) { should eq('work_area[processing_state]') }
+          its(:input_name_for_select_sort_order) { should eq('work_area[order]') }
+          its(:page) { should eq(subject.send(:default_page)) }
+
+          it { should delegate_method(:default_order).to(:search_criteria_config) }
+          it { should delegate_method(:default_page).to(:search_criteria_config) }
+          it { should delegate_method(:order_options_for_select).to(:search_criteria_config) }
+
+          it 'will expose #processing_states_for_select' do
+            expect(repository).to receive(:processing_state_names_for_select_within_work_area).with(work_area: work_area).and_call_original
+            subject.processing_states_for_select
+          end
         end
       end
     end
