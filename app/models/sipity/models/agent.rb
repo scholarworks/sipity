cf2b041b7047a6d48ed27be438046f048ac74fe7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-06-29 13:58:45 -0400

    Adding Agent.create_a_named_agent!

--- app/models/sipity/models/agent.rb
+++ app/models/sipity/models/agent.rb
@@ -7,6 +7,13 @@ module Sipity
 
       has_one :processing_actor, as: :proxy_for, class_name: 'Sipity::Models::Processing::Actor'
       has_many :event_logs, class_name: 'Sipity::Models::EventLog', as: :requested_by
+
+      # @note - This is not a service function because its isolated in its
+      #   behavior and its not something that is going to be used all that
+      #   often.
+      def self.create_a_named_agent!(name:, authentication_token: self.authentication_token)
+        self.create!(name: name, authentication_token: authentication_token)
+      end
     end
   end
 end
--- spec/models/sipity/models/agent_spec.rb
+++ spec/models/sipity/models/agent_spec.rb
@@ -12,6 +12,12 @@ module Sipity
 
       it { should have_one(:processing_actor) }
       it { should have_many(:event_logs) }
+
+      context '.create_a_named_agent!' do
+        it 'creates a named agent and assigns an authentication_token' do
+          expect { described_class.create_a_named_agent!(name: 'Hello') }.to change(described_class, :count).by(1)
+        end
+      end
     end
   end
 end
