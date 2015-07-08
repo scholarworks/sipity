53a6344306a57e62556724827b61c2b27142eaeb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-07-08 12:40:03 -0400

    Adding ProcessingHook for SubmitForReview
    As part of the Grad School's business logic, whenever a student
    submits their work for review, they want to capture the submission
    date.
    
    The submission date provides an audit trail, and is something that
    may be applicable for the metadata of the scholarly object.

---
+++ app/processing_hooks/sipity/processing_hooks/etd/works/submit_for_review_processing_hook.rb
@@ -0,0 +1,37 @@
+module Sipity
+  module ProcessingHooks
+    module Etd
+      module Works
+        # Responsible for performing any additional behavior as part of a user
+        # taking the SubmitForReview action on an ETD Work.
+        module SubmitForReviewProcessingHook
+          module_function
+
+          # When the SubmitForReview action is taken on a given ETD Work,
+          # this method should be called.
+          #
+          # @api private
+          # @see Sipity::ProcessingHooks.call for how this is called
+          def call(entity:, repository: default_repository, as_of_date: default_as_of_date, **_keywords)
+            work = entity.proxy_for
+            repository.update_work_attribute_values!(
+              work: work,
+              values: as_of_date.strftime(Models::AdditionalAttribute::DATE_FORMAT),
+              key: Models::AdditionalAttribute::ETD_SUBMISSION_DATE
+            )
+          end
+
+          def default_repository
+            CommandRepository.new
+          end
+          private_class_method :default_repository
+
+          def default_as_of_date
+            Time.zone.today
+          end
+          private_class_method :default_as_of_date
+        end
+      end
+    end
+  end
+end
