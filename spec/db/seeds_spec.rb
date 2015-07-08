bbb3e7b7509e2a6e1a199e23d5e9510639d10fea
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-07-08 14:39:12 -0400

    Ensuring seeds are idempotent

---
+++ spec/db/seeds_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+RSpec.describe 'database seeds' do
+  let(:path_to_seeds) { Rails.root.join('db/seeds.rb') }
+  it 'allows for seeds to be run repeatedly without updating the data a second time' do
+    instance_eval(path_to_seeds.read)
+    [:update_attribute, :update_attributes, :update_attributes!, :save, :save!, :update, :update!].each do |method_names|
+      expect_any_instance_of(ActiveRecord::Base).to_not receive(method_names)
+    end
+    instance_eval(path_to_seeds.read)
+  end
+end
