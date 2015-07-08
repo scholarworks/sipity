5b4da1da3d792b16f40d443c2380e7d90836adeb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-07-08 15:41:26 -0400

    Moving logic out of seeds
    The logic that has been moved was a one time migration; As such
    it need not always be run.
    
    Also, teasing apart the behavior as I was assuming the OnUserCreate was
    doing only group management (but it could've been doing so much more).

---
+++ db/data/20150708193800_apply_all_user_group_to_users.rb
@@ -0,0 +1,13 @@
+class ApplyAllUserGroupToUsers < ActiveRecord::Migration
+  def self.up
+    #add existing users to that group (unless they are already there)
+    User.find_each do |user|
+      registered_users_group = Sipity::Models::Group.all_registered_users
+      registered_users_group.group_memberships.create(user: user) if registered_users_group.group_memberships.where(user: user).empty?
+    end
+  end
+
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
