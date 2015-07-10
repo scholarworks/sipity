953fc86e9a1951afa9daea82240fe1146f94a308
Rajesh Balekai <rbalekai@nd.edu>
2015-07-10 15:32:39 -0400

    Changes in email for Grad School Signoff
    Closing issue 820
    
    Closes #820
    
    $ ./script/close-issue 820

--- spec/decorators/sipity/decorators/emails/work_email_decorator_spec.rb
+++ spec/decorators/sipity/decorators/emails/work_email_decorator_spec.rb
@@ -4,12 +4,30 @@ module Sipity
   module Decorators
     module Emails
       RSpec.describe WorkEmailDecorator do
-        let(:creators) { [double(name: 'John'), double(name: 'Ringo')] }
+        let(:creators) { [double(name: 'John', username: 'john123'), double(name: 'Ringo', username: 'ringo123')] }
         let(:accessible_objects) { [double(name: 'Work'), double(name: 'Attachment')] }
         let(:work) { Models::Work.new(id: 'abc', work_type: 'doctoral_dissertation', title: 'My Title') }
         let(:collaborators) { [double, double] }
         let(:reviewers) { [double, double] }
         let(:repository) { QueryRepositoryInterface.new }
+        let(:work_access) { Models::AccessRightFacade.new(work, work: work) }
+        let(:program_names) { [double, double] }
+        let(:degree) { [double] }
+        let(:creator_netids) { ['john123', 'ringo123'] }
+        let(:accessible_file) { double(name: 'Attachment') }
+        let(:accessible_files) { [accessible_file] }
+        let(:publishing_intent) { [double] }
+        let(:patent_intent) { [double] }
+        let(:submission_date) { double }
+        let(:additional_attributes) do
+          {
+            "Sipity::Models::AdditionalAttribute::PROGRAM_NAME_PREDICATE_NAME" => program_names,
+            "Sipity::Models::AdditionalAttribute::DEGREE_PREDICATE_NAME" => degree,
+            "Sipity::Models::AdditionalAttribute::WORK_PUBLICATION_STRATEGY" => publishing_intent,
+            "Sipity::Models::AdditionalAttribute::WORK_PATENT_STRATEGY" => patent_intent,
+            "Sipity::Models::AdditionalAttribute::ETD_SUBMISSION_DATE" => submission_date
+          }
+        end
 
         subject { described_class.new(work, repository: repository) }
 
@@ -22,6 +40,11 @@ module Sipity
             with(work: work).and_return(collaborators)
           allow(repository).to receive(:work_collaborators_responsible_for_review).
             with(work: work).and_return(reviewers)
+          additional_attributes.each do |key, value|
+            allow(repository).to receive(:work_attribute_values_for).
+              with(work: work, key: key.constantize).
+              and_return(value)
+          end
         end
 
         its(:work_type) { should eq('Doctoral dissertation') }
@@ -31,9 +54,36 @@ module Sipity
         its(:email_message_action_url) { should match(%r{/#{work.to_param}\Z}) }
         its(:collaborators) { should eq(collaborators) }
         its(:reviewers) { should eq(reviewers) }
-
         its(:creator_names) { should eq(['John', 'Ringo']) }
         its(:accessible_objects) { should eq(accessible_objects) }
+        its(:program_names) { should eq(program_names) }
+        its(:degree) { should eq(degree) }
+        its(:creator_netids) { should eq(creator_netids) }
+        its(:publishing_intent) { should eq(publishing_intent) }
+        its(:patent_intent) { should eq(patent_intent) }
+        its(:submission_date) { should eq(submission_date) }
+
+        its(:additional_committe_members) do
+          allow(subject).to receive(:collaborators).and_return(['John', 'Ringo'])
+          allow(subject).to receive(:reviewers).and_return(['Ringo'])
+          additional_committe_members = ['John']
+          should eq(additional_committe_members)
+        end
+
+        its(:work_access) do
+          allow(Models::AccessRightFacade).to receive(:new).
+            with(work, work: work).and_return(work_access)
+          should eq(work_access)
+        end
+
+        its(:accessible_files) do
+          allow(repository).to receive(:work_attachments).
+            with(work: work).and_return(accessible_files)
+          allow(Models::AccessRightFacade).to receive(:new).
+            and_return(accessible_file)
+          should eq(accessible_files)
+        end
+
       end
     end
   end
