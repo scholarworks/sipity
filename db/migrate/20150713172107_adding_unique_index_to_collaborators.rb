051682abea3a9116884441f1783aee2c19fc52fe
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-07-14 14:59:49 -0400

    Adding forgiveness regarding index already existing

--- db/migrate/20150713172107_adding_unique_index_to_collaborators.rb
+++ db/migrate/20150713172107_adding_unique_index_to_collaborators.rb
@@ -2,7 +2,7 @@ class AddingUniqueIndexToCollaborators < ActiveRecord::Migration
   def change
     Sipity::Models::Collaborator.connection.execute("UPDATE `sipity_collaborators` SET email = NULL WHERE email = '';")
     Sipity::Models::Collaborator.connection.execute("UPDATE `sipity_collaborators` SET netid = NULL WHERE netid = '';")
-    add_index :sipity_collaborators, [:work_id, :email], unique: true
-    add_index :sipity_collaborators, [:work_id, :netid], unique: true
+    add_index(:sipity_collaborators, [:work_id, :email], unique: true) rescue true
+    add_index(:sipity_collaborators, [:work_id, :netid], unique: true) rescue true
   end
 end
