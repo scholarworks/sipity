d0a5bf53a91f3dd60f88fb103a3185cf32a9b87a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-08-25 15:30:09 -0400

    Adding identifier to event log

--- app/repositories/sipity/commands/event_log_commands.rb
+++ app/repositories/sipity/commands/event_log_commands.rb
@@ -16,7 +16,10 @@ module Sipity
       # @note This is both a module function and an instance function.
       # @see The underlying spec defines the behavior; Do not access
       def log_event!(entity:, requested_by:, event_name:)
-        Models::EventLog.create!(entity: entity, user_id: requested_by.id, requested_by: requested_by, event_name: event_name)
+        Models::EventLog.create!(
+          entity: entity, user_id: requested_by.id, requested_by: requested_by, event_name: event_name,
+          identifier_id: PowerConverter.convert_to_identifier_id(requested_by)
+        )
       end
     end
   end
