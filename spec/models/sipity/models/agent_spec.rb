8fe16ae2825e59488458660bc0453c37cd57ffa5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-08-28 09:32:51 -0400

    Compacting Agent into a module
    With each of the data structures extracted, the module can be
    a builder mechanism.

--- spec/models/sipity/models/agent_spec.rb
+++ spec/models/sipity/models/agent_spec.rb
@@ -7,11 +7,10 @@ module Sipity
       context 'class configuration' do
         subject { described_class }
         its(:default_token_decoder) { should respond_to(:call) }
-        its(:public_methods) { should_not include(:new) }
-        its(:private_methods) { should include(:new) }
       end
 
       context '.new_from_cogitate_token' do
+        # TODO: This test has overly complicated setup; Simplify this
         let(:token) { 'A "Token" because relying on a Token generated via Cogitate and its configuration may be painful' }
         let(:token_decoder) { double('Token Decoder', call: agent) }
         let(:agent) do
@@ -19,18 +18,9 @@ module Sipity
             the_agent.add_email('hworld@nd.edu')
           end
         end
-
         subject { Agent.new_from_cogitate_token(token: token, token_decoder: token_decoder) }
-        its(:email) { should eq('hworld@nd.edu') }
-        its(:default_ids_decoder) { should respond_to(:call) }
-        it { should delegate_method(:ids).to(:cogitate_agent) }
-        it { should delegate_method(:name).to(:cogitate_agent) }
-        its(:user_signed_in?) { should eq(true) }
-
-        context '#netid' do
-          it 'will decode the ids' do
-            expect(subject.netid).to eq('hworld')
-          end
+        it 'will honor the AgentInterface' do
+          expect(Contract.valid?(subject, Sipity::Interfaces::AgentInterface)).to eq(true)
         end
       end
 
@@ -38,18 +28,16 @@ module Sipity
         before { allow(User).to receive(:find).with(user.id).and_return(user) }
         let(:user) { double(id: 123, email: 'hello@world.com', to_s: 'The user', username: 'hello') }
         subject { Agent.new_from_user_id(user_id: user.id) }
-        its(:email) { should eq(user.email) }
-        its(:name) { should eq(user.to_s) }
-        its(:ids) { should eq([Cogitate::Client.encoded_identifier_for(strategy: 'netid', identifying_value: user.username)]) }
-        its(:user_id) { should eq(user.id) }
-        its(:id) { should eq(user.id) }
-        its(:user_signed_in?) { should eq(true) }
-        its(:to_polymorphic_type) { should eq('User') }
+        it 'will honor the AgentInterface' do
+          expect(Contract.valid?(subject, Sipity::Interfaces::AgentInterface)).to eq(true)
+        end
       end
 
       context '.new_null_agent' do
         subject { Agent.new_null_agent }
-        its(:user_signed_in?) { should eq(false) }
+        it 'will honor the AgentInterface' do
+          expect(Contract.valid?(subject, Sipity::Interfaces::AgentInterface)).to eq(true)
+        end
       end
     end
   end
