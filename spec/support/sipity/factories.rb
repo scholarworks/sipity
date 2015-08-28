7b059e11dd4224155967e1d99d5a601b1d477c33
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-08-28 09:39:24 -0400

    Assuming a test user has agreed to ToS
    Since Devise and Warden were being short-circuited in tests
    the ToS question never came up. However with a change in the
    authentication layer ToS comes up.

--- spec/support/sipity/factories.rb
+++ spec/support/sipity/factories.rb
@@ -1,9 +1,15 @@
 module Sipity
   module Factories
     def create_user(overrides = {})
+      skip_agreed_to_tos = overrides.delete(:skip_agreed_to_tos) || false
       attributes = { name: 'Test User', email: 'test@example.com' }.merge(overrides)
       attributes[:username] ||= attributes[:email]
-      User.create!(attributes)
+      User.create!(attributes).tap do |user|
+        break if skip_agreed_to_tos
+        require 'cogitate/client'
+        identifier_id = Cogitate::Client.encoded_identifier_for(strategy: 'netid', identifying_value: user.username)
+        Sipity::Models::AgreedToTermsOfService.create!(identifier_id: identifier_id, agreed_at: Time.zone.now)
+      end
     end
     module_function :create_user
   end
