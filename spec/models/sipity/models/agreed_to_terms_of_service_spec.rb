05762b6076983763c43ff272a4e7aa301677bef2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-09-29 13:44:31 -0400

    Adding some "missing" tests

--- spec/models/sipity/models/agreed_to_terms_of_service_spec.rb
+++ spec/models/sipity/models/agreed_to_terms_of_service_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Sipity::Models::AgreedToTermsOfService, type: :model do
-
+  context 'database configuration' do
+    subject { described_class }
+    its(:column_names) { should include('identifier_id') }
+    its(:column_names) { should include('agreed_at') }
+  end
 end
