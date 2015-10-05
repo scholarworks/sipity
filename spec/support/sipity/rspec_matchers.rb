d4856b4895baf5b5fb82af293a806d8145082c5e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-10-05 15:57:17 -0400

    Adding RSpec matcher for contractually_honors
    Why:
    
    * Because it is helpful to quickly assert a contract has been adhered
      to.
    
    This change addresses the need by:
    
    * Writing a custom contracts matcher

--- spec/support/sipity/rspec_matchers.rb
+++ spec/support/sipity/rspec_matchers.rb
@@ -1,3 +1,4 @@
+require 'contracts'
 module Sipity
   module RSpecMatchers
     class ImplementProcessingFormInterface
@@ -19,6 +20,30 @@ module Sipity
 
     def implement_processing_form_interface
       ImplementProcessingFormInterface.new
+    end
+
+    # Does the licensee adhear to the given contract?
+    class ContractuallyHonorMatcher
+      def initialize(contract)
+        @contract = contract
+      end
+
+      def matches?(licensee)
+        @licensee = licensee
+        Contract.valid?(@licensee, @contract)
+      end
+
+      def description
+        "expected to honor the #{@contract.inspect} contract"
+      end
+
+      def failure_message
+        "expected #{@licensee} to honor #{@contract.inspect} contract"
+      end
+    end
+
+    def contractually_honor(contract)
+      ContractuallyHonorMatcher.new(contract)
     end
   end
 end
