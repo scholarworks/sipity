bb1e793cf1e5df971c2a6fd8e9bec33d9ec316d9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-10-19 10:14:22 -0400

    Removing some more code smells
    ```console
    [21]:Sipity::Services::Notifier#deliver_email refers to options more
         than self (FeatureEnvy)
    ```

--- app/services/sipity/services/notifier.rb
+++ app/services/sipity/services/notifier.rb
@@ -19,23 +19,23 @@ module Sipity
       private_class_method :build_notification_container
 
       def deliver_email(notificaton_container:, notification:, options:)
-        to = options.fetch(:to)
-        cc = options.fetch(:cc) { [] }
-        bcc = options.fetch(:bcc) { [] }
+        to, cc, bcc = options.fetch(:to), options.fetch(:cc, []), options.fetch(:bcc, [])
         entity = options.fetch(:entity)
-        if to.empty?
-          Airbrake.notify_or_ignore(
-            error_class: Exceptions::SenderNotFoundError,
-            error_message: "#{Exceptions::SenderNotFoundError}: Return without sending message.",
-            parameters: {}
-          )
-          return
-        end
+        return notify_aibrake_of_no_sender if to.empty?
         email_notifier = notificaton_container.public_send(notification, entity: entity, to: to, cc: cc, bcc: bcc)
         email_notifier.deliver_now
       end
-
       private_class_method :deliver_email
+
+      def notify_aibrake_of_no_sender
+        Airbrake.notify_or_ignore(
+          error_class: Exceptions::SenderNotFoundError,
+          error_message: "#{Exceptions::SenderNotFoundError}: Return without sending message.",
+          parameters: {}
+        )
+        false
+      end
+      private_class_method :notify_aibrake_of_no_sender
 
       class << self
         alias_method :call, :deliver
