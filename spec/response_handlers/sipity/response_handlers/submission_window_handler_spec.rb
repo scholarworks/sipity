af3e029617a179dd01ba30f77f3cbe466ef13020
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-10-29 10:43:58 -0400

    Refactoring testing to account for upcoming tests
    I'm preparing to add command line responders; So I want to make sure
    the specs include space for those.

--- spec/response_handlers/sipity/response_handlers/submission_window_handler_spec.rb
+++ spec/response_handlers/sipity/response_handlers/submission_window_handler_spec.rb
@@ -7,28 +7,32 @@ module Sipity
     module SubmissionWindowHandler
       RSpec.describe SuccessResponder do
         let(:handler) { double(render: 'rendered', template: 'show') }
-
-        it 'will coordinate the rendering of the template' do
-          described_class.call(handler: handler)
-          expect(handler).to have_received(:render).with(template: handler.template)
+        context '.call' do
+          it 'will coordinate the rendering of the template' do
+            described_class.call(handler: handler)
+            expect(handler).to have_received(:render).with(template: handler.template)
+          end
         end
       end
 
       RSpec.describe SubmitSuccessResponder do
         let(:handler) { double(redirect_to: true, response_object: double(id: '123')) }
-
-        it 'will coordinate the rendering of the template' do
-          expect(PowerConverter).to receive(:convert_to_access_path).and_return('/hello/world')
-          described_class.call(handler: handler)
-          expect(handler).to have_received(:redirect_to).with('/hello/world')
+        context '.call' do
+          it 'will coordinate the rendering of the template' do
+            expect(PowerConverter).to receive(:convert_to_access_path).and_return('/hello/world')
+            described_class.call(handler: handler)
+            expect(handler).to have_received(:redirect_to).with('/hello/world')
+          end
         end
       end
 
       RSpec.describe SubmitFailureResponder do
         let(:handler) { double(render: 'rendered', template: 'show') }
-        it 'will coordinate the rendering of the template' do
-          described_class.call(handler: handler)
-          expect(handler).to have_received(:render).with(template: handler.template, status: :unprocessable_entity)
+        context '.call' do
+          it 'will coordinate the rendering of the template' do
+            described_class.call(handler: handler)
+            expect(handler).to have_received(:render).with(template: handler.template, status: :unprocessable_entity)
+          end
         end
       end
     end
