884957d7fdadb93d2323d1f7fdedb973605b4be5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-10-30 10:19:21 -0400

    Renaming variable to be more generic
    Why:
    
    * Because :controller assumed what the external collaborator was. I
      want a more narrow interface exposed.
    
    This change addresses the need by:
    
    * Renaming :controller -> :context within the given class.

--- app/controllers/sipity/controllers/processing_action_composer.rb
+++ app/controllers/sipity/controllers/processing_action_composer.rb
@@ -12,15 +12,15 @@ module Sipity
       # @see ProcessingActionComposer#processing_action_name
       def self.build_for_controller(controller:, **keywords)
         new(
-          controller: controller,
+          context: controller,
           response_handler_container: controller.response_handler_container,
           processing_action_name: keywords.fetch(:processing_action_name) { -> { controller.params.fetch(:processing_action_name) } },
           **keywords
         )
       end
 
-      def initialize(controller:, processing_action_name:, response_handler_container:, response_handler: default_response_handler)
-        self.controller = controller
+      def initialize(context:, processing_action_name:, response_handler_container:, response_handler: default_response_handler)
+        self.context = context
         self.processing_action_name = processing_action_name
         self.response_handler_container = response_handler_container
         self.response_handler = response_handler
@@ -28,7 +28,7 @@ module Sipity
 
       def prepend_processing_action_view_path_with(slug:)
         path = build_processing_action_view_path_for(slug: slug)
-        controller.prepend_view_path(path)
+        context.prepend_view_path(path)
       end
 
       def run_and_respond_with_processing_action(**keywords)
@@ -40,13 +40,13 @@ module Sipity
       attr_accessor :response_handler_container
 
       def run_processing_action(**keywords)
-        status, object = controller.run(processing_action_name: processing_action_name, **keywords)
+        status, object = context.run(processing_action_name: processing_action_name, **keywords)
         Parameters::HandledResponseParameter.new(status: status, object: object, template: processing_action_name)
       end
 
       def handle_response(handled_response)
         response_handler.handle_response(
-          context: controller,
+          context: context,
           handled_response: handled_response,
           container: response_handler_container # Note: Controller is passed twice, tighten this up?
         )
@@ -54,7 +54,7 @@ module Sipity
 
       attr_writer :processing_action_name
 
-      # Why these antics? Because when this object is created using a controller as the context, the params object is not entirely set (at
+      # Why these antics? Because when this object is created using a context as the context, the params object is not entirely set (at
       # least not in test). So I'm adding the ability to provide a callable value.
       #
       # @see ProcessingActionComposer.build_for_controller
@@ -63,18 +63,18 @@ module Sipity
         @processing_action_name
       end
 
-      attr_reader :controller
+      attr_reader :context
 
       include GuardInterfaceExpectation
-      def controller=(input)
+      def context=(input)
         guard_interface_expectation!(input, :prepend_view_path, :params, :run, :response_handler_container, :controller_path)
-        @controller = input
+        @context = input
       end
 
       ROOT_VIEW_PATH = Rails.root.join('app/views')
 
       def build_processing_action_view_path_for(slug:)
-        File.join(ROOT_VIEW_PATH, controller.controller_path, PowerConverter.convert_to_file_system_safe_file_name(slug))
+        File.join(ROOT_VIEW_PATH, context.controller_path, PowerConverter.convert_to_file_system_safe_file_name(slug))
       end
 
       attr_reader :response_handler
