6baf4066366fe4ae5c892708c0fcb9885c81b7e6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-11-11 15:31:40 -0500

    Updating #work_attribute_values_for method docs
    [skip ci]

--- app/repositories/sipity/queries/additional_attribute_queries.rb
+++ app/repositories/sipity/queries/additional_attribute_queries.rb
@@ -7,12 +7,23 @@ module Sipity
     # TODO: These methods need to no longer be module_functions; I believe the
     #   direction is to look towards creating service objects.
     module AdditionalAttributeQueries
+      # @api public
+      #
+      # Responsible for returning the values associated with the given :work and :key.
+      #
+      # @param work [#to_work] The containing work
+      # @param key [#to_s] The named predicate (yes it should be :predicate_name and not :key) to retrieve values for
+      # @param cardinality [:many, :one, #to_i] The maximum number to include
+      #
+      # @return Array or Object depending on cardinality
+      #
+      # @review Consider not using cardinality and instead preferring specific methods? This could mean an explosion.
       def work_attribute_values_for(work:, key:, cardinality: :many)
         work = Conversions::ConvertToWork.call(work)
         scope = Models::AdditionalAttribute.where(work_id: work.id, key: key)
         scope = scope.limit(cardinality) unless cardinality == :many
         returning_value = scope.pluck(:value)
-        returning_value = returning_value.first if cardinality == 1 || cardinality == :one
+        returning_value = returning_value.first if cardinality == :one || cardinality == 1
         returning_value
       end
 
