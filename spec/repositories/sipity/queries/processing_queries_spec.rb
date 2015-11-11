1751114f0fbc8f6bdee68ea05dd952a101f3d14e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-11-11 11:31:02 -0500

    Adding sequestering state for ETD workflow
    Because at this point in time, we have marked objects as ready for
    ingest; However they are not ready as they need to either go through
    cataloging or be marked as ready for immediate ingest with delayed
    cataloging.
    
    The plan is to change the data migration, which has yet to run, to
    move items from Ready for Ingest to the temporary, though more
    appropriate 'grad_school_approved_but_waiting_for_routing'.

--- spec/repositories/sipity/queries/processing_queries_spec.rb
+++ spec/repositories/sipity/queries/processing_queries_spec.rb
@@ -72,8 +72,9 @@ module Sipity
           # demonstrates what's working
           expect(subject).to eq(
             [
-              "advisor_changes_requested", "back_from_cataloging", "grad_school_changes_requested", "ingested", "ingesting",
-              "new", "ready_for_cataloging", "ready_for_ingest", "under_advisor_review", "under_grad_school_review"
+              "advisor_changes_requested", "back_from_cataloging", "grad_school_approved_but_waiting_for_routing",
+              "grad_school_changes_requested", "ingested", "ingesting", "new", "ready_for_cataloging", "ready_for_ingest",
+              "under_advisor_review", "under_grad_school_review"
             ]
           )
         end
