0d7d7ff4fa737a9c1f8db6efa6a7f0eb74c2c746
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-11-20 13:37:26 -0500

    Adding an ETD::AuthorForm
    I want to capture the preferred author name as we don't expose an
    obvious method for the user to change their name from what they have as
    a preferred name.

---
+++ spec/forms/sipity/forms/work_submissions/etd/author_form_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+require 'support/sipity/command_repository_interface'
+require 'sipity/forms/work_submissions/etd/author_form'
+
+module Sipity
+  module Forms
+    module WorkSubmissions
+      module Etd
+        RSpec.describe AuthorForm do
+          let(:work) { Models::Work.new(id: '1234') }
+          let(:repository) { CommandRepositoryInterface.new }
+          let(:keywords) { { work: work, requested_by: double, repository: repository } }
+          subject { described_class.new(keywords) }
+
+          before do
+            allow(repository).to receive(:work_attribute_values_for).
+              with(work: work, key: 'author_name', cardinality: 1).and_return(nil)
+          end
+
+          its(:processing_action_name) { should eq('author') }
+          its(:policy_enforcer) { should eq Policies::WorkPolicy }
+
+          it { should respond_to :work }
+          it { should respond_to :author_name }
+          it { should respond_to :requested_by }
+
+          include Shoulda::Matchers::ActiveModel
+          it { should validate_presence_of(:author_name) }
+
+          context '#submit' do
+            context 'with invalid data' do
+              before do
+                expect(subject).to receive(:valid?).and_return(false)
+              end
+              it 'will return false if not valid' do
+                expect(subject.submit).to eq(false)
+              end
+            end
+
+            context 'with valid data' do
+              subject { described_class.new(keywords.merge(attributes: { author_name: 'Hello Dolly' })) }
+              before do
+                allow(subject).to receive(:valid?).and_return(true)
+                allow(subject.send(:processing_action_form)).to receive(:submit).and_yield
+              end
+
+              it 'will update the author of the work' do
+                expect(repository).to receive(:update_work_attribute_values!).with(
+                  work: work, values: 'Hello Dolly', key: 'author_name'
+                )
+                subject.submit
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
