f825eac4ee4047939e4d8ad3e36373a91b9d934a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-12-01 13:11:12 -0500

    Updating schema for data migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130142312) do
+ActiveRecord::Schema.define(version: 20151201155313) do
 
   create_table "data_migrations", id: false, force: :cascade do |t|
     t.string "version", limit: 255, null: false
