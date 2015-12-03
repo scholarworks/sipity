bd8ebdab7bf5872580893cbaa5d967837ca9e960
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-12-03 10:17:28 -0500

    Creating a lookup for emailers by work area
    Given that I'm wanting to introduce multiple work areas, and have them
    be configurable/pluggable, I need a lookup mechanism that adhears to
    a convention.

---
+++ app/mailers/sipity/mailers.rb
@@ -0,0 +1,27 @@
+require 'sipity/exceptions'
+
+module Sipity
+  # The name space for mailers
+  module Mailers
+    # @api public
+    #
+    # Responsible for finding the correct mailer for the given entity and notification
+    #
+    # @param entity [#to_work_area]
+    # @param notification [String,Symbol]
+    #
+    # @return subclass of ActionMailer::Base
+    def self.find_mailer_for(entity:, notification:)
+      mailer = mailer_for(entity: entity)
+      return mailer if mailer.respond_to?(notification)
+      fail Exceptions::NotificationNotFoundError, name: notification, container: mailer
+    end
+
+    def self.mailer_for(entity:)
+      work_area = PowerConverter.convert(entity, to: :work_area)
+      mailer_name_as_constant = "#{work_area.demodulized_class_prefix_name}Mailer"
+      return "#{self}::#{mailer_name_as_constant}".constantize
+    end
+    private_class_method :mailer_for
+  end
+end
