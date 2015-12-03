6244f6c931917947f6d20c10e9f4b890ad91b19f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-12-03 14:39:57 -0500

    Extracting a common email class builder method
    Prior to this commit the EtdMailer had a heavy load of duplicate logic.
    It wasn't as declarative as it could be.
    
    This commit extracts a builder method that allows for the predictable
    generation of the EtdMailer and an upcoming UlraMailer object. The
    predictability is desirable from an validation and proper composition
    stand point.
    
    It should be noted that a few oddities are introduced with the builder,
    namely the view paths in partials gets rather weird (i.e. see comments
    in the builder for more details).
    
    By extracting a function responsible for building the emailer in a
    consistent manner, I can swap out the details of the actual email
    subsystem. This could've been done by creating a subclass of the
    ActionMailer::Base and adding the configuration options there. But then
    we have an explicit 3 levels of inheritance, which becomes nasty.
    
    It should also be noted that by moving towards this composition, it would
    be possible to express the email notifications in a configuration file.
    One thing I've been thinking about is whether the entire workflow should
    be articulated in some kind of localized data structure instead of the
    database. But that is another thought for later.

--- spec/mailers/sipity/mailers/etd_mailer_spec.rb
+++ spec/mailers/sipity/mailers/etd_mailer_spec.rb
@@ -18,35 +18,16 @@ module Sipity
       let(:user) { User.new(name: 'User') }
       let(:actor) { Models::Processing::Actor.new(proxy_for: user) }
       let(:to) { 'test@example.com' }
-
-      described_class::NOTIFCATION_METHOD_NAMES_FOR_WORK.each do |work_notification_method_name|
-        context "##{work_notification_method_name}" do
-          it 'should send an email' do
-            processing_entity # making sure its declared
-            described_class.send(work_notification_method_name, entity: work, to: to).deliver_now
-            expect(ActionMailer::Base.deliveries.count).to eq(1)
-          end
-        end
+      let(:comment) { Models::Processing::Comment.new(actor: actor, entity: processing_entity) }
+      let(:action) do
+        Models::Processing::EntityActionRegister.new(entity: processing_entity, on_behalf_of_actor: actor, created_at: Time.zone.now)
       end
 
-      described_class::NOTIFCATION_METHOD_NAMES_FOR_PROCESSING_COMMENTS.each do |email_method|
-        context "##{email_method}" do
-          let(:processing_comment) { Models::Processing::Comment.new(actor: actor, entity: processing_entity) }
+      described_class.emails.each do |email|
+        context "##{email.method_name}" do
           it 'should send an email' do
-            described_class.send(email_method, entity: processing_comment, to: to).deliver_now
-            expect(ActionMailer::Base.deliveries.count).to eq(1)
-          end
-        end
-      end
-
-      described_class::NOTIFCATION_METHOD_NAMES_FOR_REGISTERED_ACTION.each do |email_method|
-        context "##{email_method}" do
-          # YOWZA! This is a lot of collaborators!
-          let(:registered_action) do
-            Models::Processing::EntityActionRegister.new(entity: processing_entity, on_behalf_of_actor: actor, created_at: Time.zone.now)
-          end
-          it 'should send an email' do
-            described_class.send(email_method, entity: registered_action, to: to).deliver_now
+            processing_entity
+            described_class.send(email.method_name, entity: send(email.as), to: to).deliver_now
             expect(ActionMailer::Base.deliveries.count).to eq(1)
           end
         end
