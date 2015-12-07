8af96ef25786035204ebec6c51bb7eb1fa56511b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-12-07 08:38:06 -0500

    Correcting for data migration edge case
    I don't want to obliterate any existing dates for submission windows.

--- db/data/20151204140143_add_open_date_to_current_submission_windows.rb
+++ db/data/20151204140143_add_open_date_to_current_submission_windows.rb
@@ -1,7 +1,7 @@
 class AddOpenDateToCurrentSubmissionWindows < ActiveRecord::Migration
   def self.up
     # I'm sure there is a better date mechanism for this but its a quick fix
-    Sipity::Models::SubmissionWindow.find_each do |submission_window|
+    Sipity::Models::SubmissionWindow.where(open_for_starting_submissions_at: nil).find_each do |submission_window|
       submission_window.update_column(:open_for_starting_submissions_at, submission_window.created_at)
     end
   end
