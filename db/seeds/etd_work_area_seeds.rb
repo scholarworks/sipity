3fb7371400b834daf3f7c2223cbe727f97e14448
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-12-16 09:10:24 -0500

    Ensuring that ETD Start window is open
    There was a data migration setup to open the start window, but the seed
    data obliterated that date by passing a nil value. This sets the seed
    data with an explicit opening time.

--- db/seeds/etd_work_area_seeds.rb
+++ db/seeds/etd_work_area_seeds.rb
@@ -1,3 +1,5 @@
 Sipity::DataGenerators::FindOrCreateWorkArea.call(name: 'Electronic Thesis and Dissertation', slug: 'etd') do |work_area|
-  Sipity::DataGenerators::FindOrCreateSubmissionWindow.call(slug: 'start', work_area: work_area)
+  Sipity::DataGenerators::FindOrCreateSubmissionWindow.call(
+    slug: 'start', work_area: work_area, open_for_starting_submissions_at: Date.new(2015,6,12).to_time.in_time_zone
+  )
 end
