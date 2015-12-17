02743c364f74898895f94a4ec64dec36da8ade68
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-12-17 12:34:34 -0500

    Adding the Library Project Submit to Directors

---
+++ app/forms/sipity/forms/work_submissions/library_project/submit_for_director_review_form.rb
@@ -0,0 +1,33 @@
+require 'sipity/forms/processing_form'
+require 'active_model/validations'
+require 'sipity/forms'
+module Sipity
+  module Forms
+    module WorkSubmissions
+      module LibraryProject
+        # Responsible for submitting the associated entity to the advisor
+        # for signoff.
+        class SubmitForDirectorReviewForm
+          ProcessingForm.configure(
+            form_class: self, base_class: Models::Work, processing_subject_name: :work,
+            attribute_names: [], template: Forms::STATE_ADVANCING_ACTION_CONFIRMATION_TEMPLATE_NAME
+          )
+
+          def initialize(work:, requested_by:, attributes: {}, **keywords)
+            self.work = work
+            self.requested_by = requested_by
+            self.processing_action_form = processing_action_form_builder.new(form: self, **keywords)
+          end
+
+          include ActiveModel::Validations
+
+          def render(*)
+            %(<legend>Submit to Program Directors for review</legend>).html_safe
+          end
+
+          delegate :submit, to: :processing_action_form
+        end
+      end
+    end
+  end
+end
