d4f27d55aad13e0798e65a9101668c8897bf8c77
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-01-12 15:03:00 -0500

    Adding validation for WorkType schemas
    I want to move towards driving this information based on JSON schemas.
    The whole goal is to build much of Sipity via configuration files.

---
+++ app/data_generators/sipity/data_generators/work_type_schema.rb
@@ -0,0 +1,74 @@
+require 'dry/validation/schema'
+
+module Sipity
+  module DataGenerators
+    # Responsible for defining the schema for building work types.
+    class WorkTypeSchema < Dry::Validation::Schema
+      filled_string = proc { |value| value.str? & value.filled? }
+      string_or_array_of_strings_config = proc { |receiver| receiver.str? | receiver.array? { receiver.each { |a_receiver| a_receiver.str? } } }
+      email_config =  proc do |states|
+        states.array? do
+          states.each do |state|
+            state.hash? do
+              state.key(:name) { |name| name.str? }
+              state.key(:to, &string_or_array_of_strings_config)
+              state.optional(:cc, &string_or_array_of_strings_config)
+              state.optional(:bcc, &string_or_array_of_strings_config)
+            end
+          end
+        end
+      end
+      key(:work_types) do |work_types|
+        work_types.array? do
+          work_types.each do |work_type|
+            work_type.key(:name, &filled_string)
+            work_type.key(:actions) do |actions|
+              actions.array? do
+                actions.each do |action|
+                  action.hash? do
+                    action.key(:name, &filled_string)
+                    action.optional(:transition_to, &filled_string)
+                    action.optional(:required_actions, &string_or_array_of_strings_config)
+                    action.optional(:states) do |states|
+                      states.array? do
+                        states.each do |state|
+                          state.hash? do
+                            state.key(:name, &filled_string)
+                            state.key(:roles, &string_or_array_of_strings_config)
+                          end
+                        end
+                      end
+                    end
+                    action.optional(:emails, &email_config)
+                  end
+                end
+              end
+            end
+            work_type.key(:strategy_permissions) do |strategy_permissions|
+              strategy_permissions.array? do
+                strategy_permissions.each do |strategy_permission|
+                  strategy_permission.hash? do
+                    strategy_permission.key(:group, &filled_string)
+                    strategy_permission.key(:role, &filled_string)
+                  end
+                end
+              end
+            end
+
+            work_type.optional(:processing_hooks) do |processing_hooks|
+              processing_hooks.array? do
+                processing_hooks.each do |processing_hook|
+                  processing_hook.hash? do
+                    processing_hook.key(:state, &filled_string)
+                    processing_hook.key(:emails, &email_config)
+                    processing_hook.key(:reason, &filled_string)
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/data_generators/sipity/data_generators/work_type_schema_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+RSpec.describe Sipity::DataGenerators::WorkTypeSchema do
+  subject { described_class.new }
+
+  context 'with valid data' do
+    let(:data) do
+      {
+        work_types: [
+          {
+            name: 'ulra_submission',
+            strategy_permissions: [
+              { group: 'ULRA Review Committee', role: 'ulra_reviewer' }
+            ],
+            actions: [
+              {
+                name: 'start_a_submission',
+                transition_to: 'new',
+                emails: [
+                  { name: 'confirmation_of_ulra_submission_started', to: ['creating_user'] },
+                  { name: 'faculty_assigned_for_ulra_submission', to: ['advisor'] }
+                ],
+                states: [
+                  { name: 'under_review', roles: 'ulra_reviewer' },
+                  { name: 'something_else', roles: ['ulra_reviewer'] }
+                ],
+                attributes: { presentation_sequence: 1 },
+                required_actions: ['something']
+              }, {
+                name: 'something',
+                transition_to: 'new',
+                states: [
+                  { name: 'something_else', roles: ['ulra_reviewer'] }
+                ]
+              }
+            ],
+            processing_hooks: [
+              {
+                state: 'under_review',
+                reason: 'processing_hook_triggered',
+                emails: [
+                  { name: 'student_has_indicated_attachments_are_complete', to: 'ulra_reviewer' }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    end
+
+    it 'validates good data' do
+      expect(subject.call(data).messages).to be_empty
+    end
+  end
+end
