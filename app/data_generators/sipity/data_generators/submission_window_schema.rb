86b511f151d11c23e5818a6e26c5dcb9b50d1ede
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-01-14 14:47:03 -0500

    Adding the ULRA submission window and schema
    I want to move towards submission windows described as schema. This
    will greatly ease some of the database related tests that go along
    with permissions.
    
    Also note, that I'm adding a few states to the ULRA configuration to
    reflect that I should be managing state changes with open and close
    functionality.

---
+++ app/data_generators/sipity/data_generators/submission_window_schema.rb
@@ -0,0 +1,26 @@
+require 'dry/validation/schema'
+require 'sipity/data_generators/schema_rules_for_processing_entity'
+
+module Sipity
+  module DataGenerators
+    # Responsible for defining the schema for building work types.
+    class SubmissionWindowSchema < Dry::Validation::Schema
+      key(:submission_windows) do |submission_windows|
+        submission_windows.array? do
+          submission_windows.each do |submission_window|
+            submission_window.hash? do
+              submission_window.key(:attributes) do |attributes|
+                attributes.hash? do
+                  attributes.key(:slug, &SchemaRulesForProcessingEntity.filled_string)
+                  attributes.optional(:open_for_starting_submissions_at) { |value| value.format?(/\A\d{4}-\d{2}-\d{2}\Z/) }
+                end
+              end
+              submission_window.key(:actions, &SchemaRulesForProcessingEntity.actions_config)
+              submission_window.optional(:strategy_permissions, &SchemaRulesForProcessingEntity.strategy_permissions_config)
+            end
+          end
+        end
+      end
+    end
+  end
+end
