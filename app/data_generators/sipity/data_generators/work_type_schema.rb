45eae57b59bb7457d8722ddca9587f4a4ae12199
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-01-15 11:52:06 -0500

    Leveraging reusable config for WorkType schema

--- app/data_generators/sipity/data_generators/work_type_schema.rb
+++ app/data_generators/sipity/data_generators/work_type_schema.rb
@@ -10,34 +10,7 @@ module Sipity
           work_types.each do |work_type|
             work_type.hash? do
               work_type.key(:name, &SchemaRulesForProcessingEntity.filled_string)
-              work_type.key(:actions) do |actions|
-                actions.array? do
-                  actions.each do |action|
-                    action.hash? do
-                      action.key(:name, &SchemaRulesForProcessingEntity.filled_string)
-                      action.optional(:transition_to, &SchemaRulesForProcessingEntity.filled_string)
-                      action.optional(:required_actions, &SchemaRulesForProcessingEntity.string_or_array_of_strings_config)
-                      action.optional(:states) do |states|
-                        states.array? do
-                          states.each do |state|
-                            state.hash? do
-                              state.key(:name, &SchemaRulesForProcessingEntity.string_or_array_of_strings_config)
-                              state.key(:roles, &SchemaRulesForProcessingEntity.string_or_array_of_strings_config)
-                            end
-                          end
-                        end
-                      end
-                      action.optional(:emails, &SchemaRulesForProcessingEntity.email_config)
-                      action.optional(:attributes) do |attributes|
-                        attributes.hash? do
-                          attributes.optional(:presentation_sequence) { |seq| seq.int? & seq.gteq?(0) }
-                          attributes.optional(:allow_repeat_within_current_state, &:bool?)
-                        end
-                      end
-                    end
-                  end
-                end
-              end
+              work_type.key(:actions, &SchemaRulesForProcessingEntity.actions_config)
               work_type.optional(:strategy_permissions, &SchemaRulesForProcessingEntity.strategy_permissions_config)
               work_type.optional(:action_analogues) do |analogues|
                 analogues.array? do
