a7c090318e93948d553aaba3b83a9ca8cfb84e49
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-01-21 14:21:49 -0500

    Linting JavaScript

--- app/assets/javascripts/nd_ldap_lookup.js
+++ app/assets/javascripts/nd_ldap_lookup.js
@@ -4,6 +4,8 @@
 * Updated 2014-05-12
 */
 jQuery(function($){
+  'use strict';
+
   $('#people-results').on('click', '.person-details-show', function(){
     var $this = $(this);
     if($this.find('i').hasClass('icon-sort-down')){
@@ -32,7 +34,7 @@ jQuery(function($){
       .done(function(response) {
         $button.prop('disabled', false);
         $icon.removeClass('icon-arrows-cw animate-spin').addClass('icon-search');
-        if(response.response == 'success'){
+        if(response.response === 'success'){
           showResults(response.data);
         } else {
           $results.html(error);
@@ -55,22 +57,22 @@ jQuery(function($){
       var item = this;
       var name = (item.formal_name) ? item.formal_name : item.fullname;
       output += '<li><b class="person-name">'+ name +'</b> ';
-      if(item.affiliation_primary.length > 0) output += '<span class="person-primary-affiliation">('+ item.affiliation_primary +')</span> ';
+      if(item.affiliation_primary.length > 0){ output += '<span class="person-primary-affiliation">('+ item.affiliation_primary +')</span> '; }
       output += '<span class="person-details-show btn btn-mini">More&nbsp;<i class="icon-sort-down"></i></span><ul class="person-details">';
       output += itemList(item.title, 'person-title');
       output += itemList(item.dept, 'person-dept');
       output += itemList(item.telephone, 'person-phone');
-      if(item.email) output += '<li class="person-email"><a href="mailto:' + item.email + '">' + item.email + '</a></li>';
-      if(item.address_postal) {
+      if(item.email){ output += '<li class="person-email"><a href="mailto:' + item.email + '">' + item.email + '</a></li>'; }
+      if(item.address_postal){
         output += '<li class="person-address">' + item.address_postal + '</li>';
-      } else if(item.address) {
+      } else if(item.address){
         output += '<li class="person-address">' + item.address + '</li>';
       }
       output += '<li class="person-more"><a class="btn btn-mini" href="http://eds.nd.edu/cgi-bin/nd_ldap_search.pl?ldapfilter=uid='+ item.uid +'">Details&hellip;</a></li>';
       output += '</ul></li>';
     });
     output += '</ul>';
-    if(data.length == 0) output = '<p>No results found.</p>';
+    if(data.length === 0){ output = '<p>No results found.</p>'; }
     $results.html(output);
   }
 
@@ -79,7 +81,7 @@ jQuery(function($){
     if(item && item.length > 0){
       output = '<li><ul class="'+ listClass +'">';
       $.each(item, function(){
-        if(listClass == 'person-phone'){
+        if(listClass === 'person-phone'){
           output += '<li><a class="tel" href="tel:1-'+ this +'">'+ this +'</a></li>';
         } else {
           output += '<li>'+ this +'</li>';
