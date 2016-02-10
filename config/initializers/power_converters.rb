36af46b59d8be258d6c7c81bb6d090827d407ad1
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-02-10 09:59:19 -0500

    Improved solution to Octal string issue
    See @062e1450ad70dbd97d7a10106c78784739524be0
    See @52c692d156514ad62ce5bc731de3741b21d6f402

--- config/initializers/power_converters.rb
+++ config/initializers/power_converters.rb
@@ -32,15 +32,13 @@ PowerConverter.define_conversion_for(:catalog_system_number) do |input|
   case input
   when Float
     nil
+  when Numeric
+    format("%#9.09d", Integer(input))
   else
-    if input.to_s =~ /\A\d{9}\Z/
-      input
-    else
-      begin
-        format("%#9.09d", input)
-      rescue ArgumentError, TypeError
-        nil
-      end
+    begin
+      format("%#9.09d", Integer(input, 10))
+    rescue ArgumentError, TypeError
+      nil
     end
   end
 end
