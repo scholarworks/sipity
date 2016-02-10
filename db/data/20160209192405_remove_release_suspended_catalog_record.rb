ba4f02d63981408296b0b93837e9369716ae9f24
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-02-10 11:13:29 -0500

    Fixing error on data migration
    I had a misnamed constant and did not apply the change against my local
    machine. The error was caught as part of the deploy process.

--- db/data/20160209192405_remove_release_suspended_catalog_record.rb
+++ db/data/20160209192405_remove_release_suspended_catalog_record.rb
@@ -1,6 +1,6 @@
 class RemoveReleaseSuspendedCatalogRecord < ActiveRecord::Migration
   def self.up
-    Sipity::Models::Notifications::Email.where(method_name: 'release_suspended_catalog_record').destroy_all
+    Sipity::Models::Notification::Email.where(method_name: 'release_suspended_catalog_record').destroy_all
   end
 
   def self.down
