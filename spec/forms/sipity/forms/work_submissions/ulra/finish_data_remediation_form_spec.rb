8d6b05485ea87e3c2e0ac4009de705f65251372f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-03-02 16:16:00 -0500

    Adding the data remediation forms for ULRA

---
+++ spec/forms/sipity/forms/work_submissions/ulra/finish_data_remediation_form_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+module Sipity
+  module Forms
+    module WorkSubmissions
+      module Ulra
+        RSpec.describe FinishDataRemediationForm do
+          let(:work) { Models::Work.new(id: '1234') }
+          let(:repository) { CommandRepositoryInterface.new }
+          let(:attributes) { {} }
+          let(:keywords) { { work: work, repository: repository, requested_by: double, attributes: attributes } }
+          subject { described_class.new(keywords) }
+
+          its(:policy_enforcer) { should eq Policies::WorkPolicy }
+
+          it { should respond_to :work }
+
+          it { should delegate_method(:submit).to(:processing_action_form) }
+
+          its(:render) { should be_html_safe }
+        end
+      end
+    end
+  end
+end
