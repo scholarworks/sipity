d77ddf077222810149dc3212c02b4d0742b05c74
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-03-03 15:25:04 -0500

    Adding spec for PowerConverter::boolean

--- spec/conversions/power_converters/boolean_spec.rb
+++ spec/conversions/power_converters/boolean_spec.rb
@@ -17,10 +17,19 @@ RSpec.describe 'PowerConverter' do
       ['ashdkfjahskdfadsf', true],
       ['9876543210', true],
       [nil, false],
+      [:hello, true],
       [Object.new, true]
     ].each_with_index do |(to_convert, expected), index|
       it "will convert #{to_convert.inspect} to #{expected} (Scenario ##{index}" do
         expect(PowerConverter.convert_to_boolean(to_convert)).to eq(expected)
+      end
+    end
+
+    [
+      '', ' ', "\t", "\n"
+    ].each do |to_convert|
+      it "will raise an exception for #{to_convert.inspect} an empty string" do
+        expect { PowerConverter.convert(to_convert, to: :boolean) }.to raise_error(PowerConverter::ConversionError)
       end
     end
   end
