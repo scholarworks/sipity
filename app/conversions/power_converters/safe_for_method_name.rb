47cfb58d6ca630bf976d8e7191cc712b3ac81da2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-03-10 09:16:53 -0500

    Appeasing Rubocop and improving specs

--- app/conversions/power_converters/safe_for_method_name.rb
+++ app/conversions/power_converters/safe_for_method_name.rb
@@ -1,10 +1,6 @@
 PowerConverter.define_conversion_for(:safe_for_method_name) do |input|
   case input
   when Symbol, String
-    if input.present?
-      input.to_s.gsub(/\W+/, '_').underscore
-    else
-      nil
-    end
+    input.to_s.gsub(/\W+/, '_').underscore if input.present?
   end
 end
--- spec/conversions/power_converters/safe_for_method_name_spec.rb
+++ spec/conversions/power_converters/safe_for_method_name_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe 'PowerConverter' do
   context 'safe_for_method_name' do
     [
       { to_convert: 'Hello World', expected: 'hello_world' },
-      { to_convert: 'HelloWorld', expected: 'hello_world' }
+      { to_convert: 'HelloWorld', expected: 'hello_world' },
+      { to_convert: 'Hello!!World', expected: 'hello_world' }
     ].each do |scenario|
       it "will convert #{scenario.fetch(:to_convert)} to #{scenario.fetch(:expected)}" do
         expect(PowerConverter.convert(scenario.fetch(:to_convert), to: :safe_for_method_name)).to eq(scenario.fetch(:expected))
@@ -12,7 +13,7 @@ RSpec.describe 'PowerConverter' do
     end
 
     [
-      ''
+      '', nil, :''
     ].each do |to_convert_but_will_fail|
       it "will fail to convert #{to_convert_but_will_fail.inspect}" do
         expect { PowerConverter.convert(to_convert_but_will_fail, to: :safe_for_method_name) }.to(
