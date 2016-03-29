ba644d1223c72d272419718e177801080f02e198
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-03-29 15:54:37 -0400

    Working on WebhookWriter interface and test coverage
    I spent a lot of time trying to figuring out how to test writing to
    files. The method I ultimately decided upon separates the creation of
    the output buffer from the action that writes the data. This lets you
    use a StringIO object as a stand-in for a file in your test cases.

--- app/exporters/sipity/exporters/batch_ingest_exporter.rb
+++ app/exporters/sipity/exporters/batch_ingest_exporter.rb
@@ -4,9 +4,17 @@ module Sipity
   module Exporters
     # Responsible for coordinating sending a work through the batch ingest.
     class BatchIngestExporter
+      DATA_PATH = Figaro.env.curate_batch_data_mount_path!
+
       def self.call(work:)
         new(work: work).call
       end
+
+      def initialize(work:)
+        @work = work
+      end
+
+      attr_accessor :work
 
       def call
         AttachmentWriter.call(exporter: self)
@@ -16,13 +24,61 @@ module Sipity
         DirectoryMover.call(exporter: self)
       end
 
+      def data_directory
+        @data_directory ||= File.join(DATA_PATH, "/sipity-#{work.id}")
+      end
+
+      def make_data_directory
+        FileUtils.mkdir_p(data_directory)
+      end
+
+      def work_id
+        work.to_param
+      end
+
       class AttachmentWriter
       end
 
       class MetadataWriter
       end
 
-      class WebhookWriter
+      # Adds a list of URLs to the WEBHOOK file that are called during the bacth
+      # ingest process.
+      module WebhookWriter
+        module_function
+
+        def call(exporter:)
+          exporter.make_data_directory
+          write_contents(
+            target: output_buffer(filename: target_path(exporter: exporter)),
+            content: callback_url(exporter: exporter)
+          )
+        end
+
+        def write_contents(target:, content:)
+          target.write(content)
+          target.close_write
+        end
+
+        def output_buffer(filename:)
+          file_descriptor = IO.sysopen(filename, 'w+')
+          IO.new(file_descriptor)
+        end
+
+        def target_path(exporter:)
+          File.join(exporter.data_directory, 'WEBHOOK')
+        end
+
+        def callback_url(exporter:)
+          File.join(
+            "#{Figaro.env.protocol!}://#{authorization_credentials}@#{Figaro.env.domain_name!}",
+            "/work_submissions/#{exporter.work_id}/callback/ingest_completed.json"
+          )
+        end
+
+        def authorization_credentials
+          "#{Sipity::Models::Group::BATCH_INGESTORS}:#{Figaro.env.sipity_batch_ingester_access_key!}"
+        end
       end
 
       class DirectoryMover
--- spec/exporters/sipity/exporters/batch_ingest_exporter/webhook_writer_spec.rb
+++ spec/exporters/sipity/exporters/batch_ingest_exporter/webhook_writer_spec.rb
@@ -5,7 +5,62 @@ module Sipity
   module Exporters
     class BatchIngestExporter
       RSpec.describe WebhookWriter do
-        xit('will respond to #call')
+
+        let(:exporter) { double('BatchIngestExporter', work_id: 1661, data_directory: '/tmp/sipity-1661', make_data_directory: true) }
+        let(:mock_file) { StringIO.new }
+
+        describe '#call' do
+          # TODO confirm that Exporter recieves #make_data_directory
+          # TODO confirm that write_webook_file is called
+
+          before { allow(described_class).to receive(:output_buffer).and_return(mock_file) }
+
+          it 'bootstraps the data directory' do
+            described_class.call(exporter: exporter)
+            # expect(exporter).to receive(:make_data_directory)
+          end
+        end
+
+        describe '#output_buffer' do
+          let(:placeholder_file) { File.join(Dir.pwd, 'spec/exporters/sipity/exporters/batch_ingest_exporter/PLACEHOLDER_FILE') }
+          subject { described_class.output_buffer(filename: placeholder_file) }
+          it { is_expected.to respond_to(:write) }
+        end
+
+        describe '#write_contents' do
+          let(:example_webhook_url) { 'http://webhook.url.technology' }
+
+          it 'writes the content to the specified file' do
+            described_class.write_contents(target: mock_file, content: example_webhook_url)
+            mock_file.rewind
+            expect(mock_file.read).to eq(example_webhook_url)
+          end
+        end
+
+        describe '#target_path' do
+          subject { described_class.target_path(exporter: exporter) }
+          it { is_expected.to eq('/tmp/sipity-1661/WEBHOOK') }
+        end
+
+        describe '#callback_url' do
+          subject { described_class.callback_url(exporter: exporter) }
+          # Figaro.env.protocol! == 'http'
+          # Figaro.env.domain_name! == 'localhost:3000'
+
+          it { is_expected.to eq('http://Batch Ingestors:1234@localhost:3000/work_submissions/1661/callback/ingest_completed.json') }
+
+          it 'returns a valid URL' do
+            url_pattern = URI::regexp
+            expect(subject).to match(url_pattern)
+          end
+        end
+
+        describe '#authorization_credentials' do
+          subject { described_class.authorization_credentials }
+          # Figaro.env.sipity_batch_ingester_access_key! == '1234'
+
+          it { is_expected.to eq('Batch Ingestors:1234') }
+        end
       end
     end
   end
