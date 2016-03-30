e7e9a789c95a7c6f6614abeb29a924d9d8eba815
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-03-30 12:54:31 -0400

    Stubbing out ULRA mapping to ROF

--- app/conversions/sipity/conversions/to_rof_hash/work_type_converters_for_work.rb
+++ app/conversions/sipity/conversions/to_rof_hash/work_type_converters_for_work.rb
@@ -5,16 +5,25 @@ module Sipity
       # Responsible for allowing for different work types to be exported with custom metadata
       class WorkTypeConvertersForWork
         def self.build(work:, base_converter:, repository:)
-          instantiate_a_converter(work: work, base_converter: base_converter, repository: repository) ||
-          (raise Exceptions::FailedToInitializeWorkConverterError, work: work)
+          converter = instantiate_a_converter(work: work, base_converter: base_converter, repository: repository)
+          raise Exceptions::FailedToInitializeWorkConverterError, work: work unless converter
+          converter.new(work: work, base_converter: base_converter, repository: repository)
         end
 
+        # NOTE: Hear there be dragons. This is a prime location for plugin architecture to come along and expose a means for new work types
+        # to register a conversion to attempt. But at least its isolated.
         def self.instantiate_a_converter(work:, base_converter:, repository:)
           case work.work_type
           when Models::WorkType::DOCTORAL_DISSERTATION, Models::WorkType::MASTER_THESIS
-            EtdConverter.new(work: work, base_converter: base_converter, repository: repository)
-          else
-            false
+            EtdConverter
+          when Models::WorkType::ULRA_SUBMISSION
+            # NOTE: Locabulary gem for valid values
+            case repository.work_attribute_values_for(work: work, key: Models::AdditionalAttribute::AWARD_CATEGORY, cardinality: 1)
+            when 'Senior Thesis'
+              UlraSeniorThesisConverter
+            when '10000 Level', "20000–40000 Level", "Honors Thesis", "Capstone Project"
+              UlraDocumentConverter
+            end
           end
         end
         private_class_method :instantiate_a_converter
@@ -44,6 +53,13 @@ module Sipity
           # def af_model
           #   raise NotImplementedError, "Expected #{self.class} to implement ##{__method__}"
           # end
+
+          private
+
+          # @todo Optimize round trips to the database concerning the additional attributes
+          def fetch_attribute_values(key:)
+            repository.work_attribute_values_for(work: work, key: key)
+          end
         end
         private_constant :AbstractConverter
 
@@ -108,11 +124,6 @@ module Sipity
             end
           end
 
-          # @todo Optimize round trips to the database concerning the additional attributes
-          def fetch_attribute_values(key:)
-            repository.work_attribute_values_for(work: work, key: key)
-          end
-
           def etd_degree_level_from_work_type
             # I could be using the translations, but under test this falls apart as I don't load the translations.
             # Loading translations adds a significant chunk of time to the test suite.
@@ -121,6 +132,12 @@ module Sipity
               Models::WorkType::MASTER_THESIS => "Master’s Thesis"
             }.fetch(work.work_type)
           end
+        end
+
+        class UlraSeniorThesisConverter < AbstractConverter
+        end
+
+        class UlraDocumentConverter < AbstractConverter
         end
       end
     end
--- spec/conversions/sipity/conversions/to_rof_hash/work_type_converters_for_work_spec.rb
+++ spec/conversions/sipity/conversions/to_rof_hash/work_type_converters_for_work_spec.rb
@@ -15,6 +15,28 @@ module Sipity
         let(:work) { Sipity::Models::Work.new(work_type: Models::WorkType::MASTER_THESIS) }
         it { is_expected.to be_a(described_class::EtdConverter) }
       end
+
+      context 'for a Models::WorkType::ULRA_SUBMISSION' do
+        let(:work) { Sipity::Models::Work.new(work_type: Models::WorkType::ULRA_SUBMISSION) }
+        {
+          '10000 Level' => described_class::UlraDocumentConverter,
+          "20000–40000 Level" => described_class::UlraDocumentConverter,
+          "Honors Thesis" => described_class::UlraDocumentConverter,
+          "Capstone Project" => described_class::UlraDocumentConverter,
+          'Senior Thesis' => described_class::UlraSeniorThesisConverter
+        }.each_pair do |award_category, converter_class|
+          context "with award category #{award_category.inspect}" do
+            before { allow(repository).to receive(:work_attribute_values_for).and_return(award_category) }
+            it { is_expected.to be_a(converter_class) }
+          end
+        end
+        context "with an unknown award category" do
+          before { allow(repository).to receive(:work_attribute_values_for).and_return('Best Haircut') }
+          it 'should raise an Exceptions::FailedToInitializeWorkConverterError' do
+            expect { subject }.to raise_error(Exceptions::FailedToInitializeWorkConverterError)
+          end
+        end
+      end
       context 'for an unhandled work type' do
         let(:work) { double(work_type: 'coalmine') }
         it 'should raise an Exceptions::FailedToInitializeWorkConverterError' do
