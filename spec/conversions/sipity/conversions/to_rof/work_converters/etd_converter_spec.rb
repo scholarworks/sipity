1444e4292a4bcb4f6fc68b518ac95af49b23aa40
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-04-12 09:33:31 -0400

    Extracting a shared example for work to ROF
    I have for avoided using shared examples, in part because I thought
    they were less necessary given the mechanisms for Sipity's development.
    However, in looking again at them, I see a clear mechanism for defining
    interfaces in Ruby. So I'm bringing them about again.

--- spec/conversions/sipity/conversions/to_rof/work_converters/etd_converter_spec.rb
+++ spec/conversions/sipity/conversions/to_rof/work_converters/etd_converter_spec.rb
@@ -1,39 +1,13 @@
 require 'spec_helper'
 require 'sipity/conversions/to_rof/work_converters/etd_converter'
+require 'support/shared_examples/a_work_to_rof_converter'
 
 module Sipity
   module Conversions
     module ToRof
       module WorkConverters
         RSpec.describe EtdConverter do
-          let(:work) do
-            Sipity::Models::Work.new(
-              id: 'abcd-ef',
-              work_type: 'doctoral_dissertation',
-              collaborators: [collaborator],
-              access_right: access_right,
-              created_at: Time.zone.today
-            )
-          end
-          let(:access_right) { Sipity::Models::AccessRight.new(access_right_code: Sipity::Models::AccessRight::OPEN_ACCESS) }
-          let(:collaborator) { Sipity::Models::Collaborator.new(role: 'Research Director', name: 'Alexander Hamilton') }
-          let(:repository) { Sipity::QueryRepositoryInterface.new }
-
-          subject { described_class.new(work: work, repository: repository) }
-
-          its(:default_repository) { is_expected.to respond_to :work_attachments }
-          its(:af_model) { is_expected.to eq('Etd') }
-          its(:edit_groups) { is_expected.to be_a(Array) }
-          its(:metadata) { is_expected.to be_a(Hash) }
-          its(:rels_ext) { is_expected.to be_a(Hash) }
-          its(:to_hash) { is_expected.to be_a(Hash) }
-
-          context '#attachments' do
-            it 'should retrieve all attachments' do
-              expect(repository).to receive(:work_attachments).with(work: work, predicate_name: :all).and_return(:returned_value)
-              expect(subject.attachments).to be_a(Array)
-            end
-          end
+          it_behaves_like 'a work to rof converter', af_model: 'Etd', attachment_predicate_name: :all
         end
       end
     end
